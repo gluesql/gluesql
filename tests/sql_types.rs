de20197e9b596b57d158b00bb609e8b088eb22bb
Taehoon Moon <panarch@kaist.ac.kr>
2020-02-29 16:16:21 +0900

    Add sql_types test case

---
+++ tests/sql_types.rs
@@ -0,0 +1,45 @@
+mod helper;
+
+use helper::{Helper, SledHelper};
+
+#[test]
+fn sql_types() {
+    let helper = SledHelper::new("data.db");
+
+    let create_sql = "
+        CREATE TABLE Item (
+            id INTEGER,
+            content TEXT,
+        );
+    ";
+
+    helper.run_and_print(create_sql);
+
+    let delete_sql = "DELETE FROM Item";
+
+    helper.run_and_print(delete_sql);
+
+    let insert_sqls: [&str; 2] = [
+        "INSERT INTO Item (id, content) VALUES (1, \"Hello\");",
+        "INSERT INTO Item (id, content) VALUES (1, \"World\");",
+    ];
+
+    for insert_sql in insert_sqls.iter() {
+        helper.run(insert_sql).unwrap();
+    }
+ 
+    let test_sqls = vec![
+        (2, "SELECT * FROM Item;"),
+        (1, "UPDATE Item SET content=\"Foo\" WHERE content=\"World\";"),
+        (0, "SELECT * FROM Item WHERE content=\"World\";"),
+        (1, "SELECT * FROM Item WHERE content=\"Foo\";"),
+        (1, "UPDATE Item SET id = 11 WHERE content=\"Foo\";"),
+        (2, "SELECT * FROM Item;"),
+    ];
+
+    for (num, sql) in test_sqls.into_iter() {
+        helper.test_rows(sql, num);
+    }
+
+    helper.run_and_print(delete_sql);
+}
