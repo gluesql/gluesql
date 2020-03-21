6372055902d530afb82e2101129ca1ff8979b980
Jihyun Yu <yjh0502@gmail.com>
2020-03-21 21:05:28 +0900

    first error type

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,21 @@ pub use data::*;
 pub use execute::*;
 pub use executor::*;
 pub use storage::*;
+
+pub mod error {
+    use thiserror::Error;
+
+    #[derive(Error, Debug)]
+    pub enum Error {
+        #[error(transparent)]
+        Sled(#[from] sled::Error),
+        #[error(transparent)]
+        Bincode(#[from] bincode::Error),
+
+        // storage
+        #[error("not found")]
+        NotFound,
+    }
+
+    pub type Result<T> = std::result::Result<T, Error>;
+}
