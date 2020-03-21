77ad7aa1bbfd577b5f4d09511d510f2d58e69f95
Jihyun Yu <yjh0502@gmail.com>
2020-03-21 22:05:47 +0900

    use Vec::get

--- src/data/row.rs
+++ src/data/row.rs
@@ -9,7 +9,7 @@ pub struct Row(pub Vec<Value>);
 
 impl Row {
     pub fn get_value(&self, index: usize) -> Option<&Value> {
-        self.0.iter().nth(index)
+        self.0.get(index)
     }
 
     pub fn take_first_value(self) -> Option<Value> {
