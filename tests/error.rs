298507283800fcd26c2af62ef4661121695b3b18
Taehoon Moon <panarch@kaist.ac.kr>
2020-03-31 21:26:54 +0900

    Change error.rs code style, little bit

--- tests/error.rs
+++ tests/error.rs
@@ -13,32 +13,30 @@ fn error() {
     helper.run_and_print("CREATE TABLE TableA (id INTEGER);");
     helper.run_and_print("INSERT INTO TableA (id) VALUES (1);");
 
-    let test_cases = {
-        vec![
-            (StoreError::SchemaNotFound.into(), "SELECT * FROM Nothing;"),
-            (SelectError::TableNotFound.into(), "SELECT * FROM;"),
-            (
-                SelectError::TooManyTables.into(),
-                "SELECT * FROM TableA, TableB",
-            ),
-            (
-                SelectError::JoinRightSideNotSupported.into(),
-                "SELECT * FROM TableA JOIN (SELECT * FROM TableB) as TableC ON 1 = 1",
-            ),
-            (
-                JoinError::UsingOnJoinNotSupported.into(),
-                "SELECT * FROM TableA JOIN TableA USING (id)",
-            ),
-            (
-                JoinError::JoinTypeNotSupported.into(),
-                "SELECT * FROM TableA CROSS JOIN TableA as A ON 1 = 2;",
-            ),
-        ]
-    };
+    let test_cases = vec![
+        (StoreError::SchemaNotFound.into(), "SELECT * FROM Nothing;"),
+        (SelectError::TableNotFound.into(), "SELECT * FROM;"),
+        (
+            SelectError::TooManyTables.into(),
+            "SELECT * FROM TableA, TableB",
+        ),
+        (
+            SelectError::JoinRightSideNotSupported.into(),
+            "SELECT * FROM TableA JOIN (SELECT * FROM TableB) as TableC ON 1 = 1",
+        ),
+        (
+            JoinError::UsingOnJoinNotSupported.into(),
+            "SELECT * FROM TableA JOIN TableA USING (id)",
+        ),
+        (
+            JoinError::JoinTypeNotSupported.into(),
+            "SELECT * FROM TableA CROSS JOIN TableA as A ON 1 = 2;",
+        ),
+    ];
 
-    for (error, sql) in test_cases.into_iter() {
-        helper.test_error(sql, error);
-    }
+    test_cases
+        .into_iter()
+        .for_each(|(error, sql)| helper.test_error(sql, error));
 
     helper.test_error(
         "SELECT TableA.* FROM TableA;",
