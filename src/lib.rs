6d963e208791e8e6ed8aec049d99f93749f7f0d7
Taehoon Moon <panarch@kaist.ac.kr>
2020-08-20 16:54:45 +0900

    Bump version to v0.1.10, write comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
+//! # GlueSQL
+//!
+//! `gluesql` is a SQL database engine fully written in Rust.
+//! You can simply use `gluesql` as an embedded SQL database using default storage
+//! [sled](https://crates.io/crates/sled).
+//! Or you can make your own SQL database using `gluesql`, it provides parser & execution layer as
+//! a library.
+//!
+//! `gluesql` uses [sqlparser-rs](https://crates.io/crates/sqlparser) as a parser, and has own implementation of execution layer.
+//! And the entire codes of execution layer are pure functional!
+//!
+//! ## Examples
+//!
+//! ```
+//! use gluesql::*;
+//!
+//! #[cfg(feature = "sled-storage")]
+//! fn main() {
+//!     let storage = SledStorage::new("data/doc-db").unwrap();
+//!     let mut glue = Glue::new(storage);
+//!
+//!     let sqls = "
+//!         CREATE TABLE Glue (id INTEGER);
+//!         INSERT INTO Glue VALUES (100);
+//!         INSERT INTO Glue VALUES (200);
+//!         SELECT * FROM Glue WHERE id > 100;
+//!         DROP TABLE Glue;
+//!     ";
+//!
+//!     for query in parse(sqls).unwrap() {
+//!         glue.execute(&query).unwrap();
+//!     }
+//! }
+//!
+//! #[cfg(not(feature = "sled-storage"))]
+//! fn main() {}
+//! ```
+//!
+//! ## Custom Storage
+//! All you need to implement for `gluesql` is implementing 3 traits (2 for functions, 1 for
+//! running tests).
+//!
+//! * [Store](store/trait.Store.html)
+//! * [StoreMut](store/trait.StoreMut.html)
+//! * [Tester](tests/trait.Tester.html)
+//!
+//! Custom storage examples to see,
+//! * [GlueSQL-js](https://github.com/gluesql/gluesql-js)
+//!
+//! ## Tests
+//! For making easy for developers to implement custom storages, `gluesql` also provides integration
+//! tests as a module.
+//!
+//! So, in `/tests/`, it looks quite empty, but actual test cases exist in `src/tests/`.
+//!
+//! Example code to see,
+//! * [tests/sled_storage.rs](https://github.com/gluesql/gluesql/blob/main/tests/sled_storage.rs)
+//!
+//! After you implement `Tester` trait, the only thing you need to do is calling `generate_tests!` macro.
+
 mod executor;
 mod glue;
 mod parse;
