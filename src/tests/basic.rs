33c7addc8bbbbe02254eb2a6497bca032cabdecd
Willy Svenningsson <willy@fager.st>
2020-09-21 22:00:32 +0200

    Issues #76 SingleQuotedString
    INSERT INTO ... VALUES(value,,,)
    
    Value 'value' was not accpeted. Only "value".
    
    But 'value' in other expressions then in VALUE() do work.
    
    Single quote inside strings should be typed as 2 single quotw signs.
    
    There might be further problems when importing from other database as the escape domain is not standardized and I don't now what sqlparser accepts

--- src/tests/basic.rs
+++ src/tests/basic.rs
@@ -14,6 +14,9 @@ CREATE TABLE Test (
     tester.run_and_print(
         "INSERT INTO Test (id, num, name) VALUES (3, 4, \"Great\"), (4, 7, \"Job\")",
     );
+    tester.run_and_print(
+      "INSERT INTO Test (id, num, name) VALUES (5, 6, 'Poor'), (7, 8, ''),(9,10,'It''s me, Bob.')",
+);
 
     use Value::*;
 
@@ -25,17 +28,20 @@ CREATE TABLE Test (
         1   2   "Hello".to_owned();
         1   9   "World".to_owned();
         3   4   "Great".to_owned();
-        4   7   "Job".to_owned()
+        4   7   "Job".to_owned();
+        5   6   "Poor".to_owned();
+        7   8   "".to_owned();
+        9   10   "It's me, Bob.".to_owned()
     );
     assert_eq!(expected, found);
 
     tester.run_and_print("UPDATE Test SET id = 2");
 
     let found = tester.run("SELECT id FROM Test").expect("select");
-    let expected = select!(I64; 2; 2; 2; 2);
+    let expected = select!(I64; 2; 2; 2; 2; 2; 2; 2);
     assert_eq!(expected, found);
 
     let found = tester.run("SELECT id, num FROM Test").expect("select");
-    let expected = select!(I64 I64; 2 2; 2 9; 2 4; 2 7);
+    let expected = select!(I64 I64; 2 2; 2 9; 2 4; 2 7; 2 6;2 8;2 10);
     assert_eq!(expected, found);
 }
