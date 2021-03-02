27a98a99732ea01501607d94418cb95280c1307e
Kyran <kyran@gostelow.me>
2021-03-02 20:12:33 +1100

    Better float eq

--- src/data/value/mod.rs
+++ src/data/value/mod.rs
@@ -189,10 +189,9 @@ impl Value {
             }?),
             (DataType::Boolean, Value::F64(value)) =>
             {
-                #[allow(clippy::float_cmp)]
-                if *value == 1.0 {
+                if value.eq(&1.0) {
                     Ok(Value::Bool(true))
-                } else if *value == 0.0 {
+                } else if value.eq(&0.0) {
                     Ok(Value::Bool(false))
                 } else {
                     Err(ValueError::ImpossibleCast.into())
