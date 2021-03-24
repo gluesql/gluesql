2a81d9ecc2e198e5d4a77f4196b87fc1e1a903ca
Kyran <kyran@gostelow.me>
2021-03-25 09:49:12 +1100

    Improve testing a bit

--- src/glue.rs
+++ src/glue.rs
@@ -57,42 +57,75 @@ impl Glue {
 #[cfg(feature = "sled-storage")]
 #[cfg(test)]
 mod tests {
-    use crate::{parse_sql::parse, Glue, Payload, Row, SledStorage, Value};
+    use {
+        crate::{parse_sql::parse, Glue, Payload, Row, SledStorage, Value},
+        std::convert::TryFrom,
+    };
     #[test]
     fn eq() {
-        let path = "test";
-        std::fs::remove_dir_all(&path);
-        let sled = SledStorage::new(path).unwrap();
+        let config = sled::Config::default()
+            .path("data/using_config")
+            .temporary(true);
+
+        let sled = SledStorage::try_from(config).unwrap();
         let mut glue = Glue::new(sled);
-        glue.execute(
-            &parse("CREATE TABLE api_test (id INTEGER PRIMARY KEY, name TEXT)").unwrap()[0],
+        assert_eq!(
+            glue.execute(
+                &parse(
+                    "CREATE TABLE api_test (id INTEGER PRIMARY KEY, name TEXT, nullable TEXT NULL, is BOOLEAN)"
+                )
+                .unwrap()[0],
+            ),
+            Ok(Payload::Create)
         );
         assert_eq!(
             glue.execute(
-                &parse("INSERT INTO api_test (id, name) VALUES (1, 'test1'), (2, 'test2')")
+                &parse("INSERT INTO api_test (id, name, nullable, is) VALUES (1, 'test1', 'not null', TRUE), (2, 'test2', NULL, FALSE)")
                     .unwrap()[0]
             ),
             Ok(Payload::Insert(2))
         );
 
         assert_eq!(
-            glue.execute(&parse("SELECT * FROM api_test").unwrap()[0]),
+            glue.execute(&parse("SELECT id, name, is FROM api_test").unwrap()[0]), // Not selecting NULL because NULL != NULL. TODO: Expand this test so that NULL == NULL
             Ok(Payload::Select {
-                labels: vec![String::from("id"), String::from("name")],
+                labels: vec![String::from("id"), String::from("name"), String::from("is")],
                 rows: vec![
-                    Row(vec![Value::I64(1), Value::Str(String::from("test1"))]),
-                    Row(vec![Value::I64(2), Value::Str(String::from("test2"))])
+                    Row(vec![
+                        Value::I64(1),
+                        Value::Str(String::from("test1")),
+                        Value::Bool(true)
+                    ]),
+                    Row(vec![
+                        Value::I64(2),
+                        Value::Str(String::from("test2")),
+                        Value::Bool(false)
+                    ])
                 ]
             })
         );
         assert_eq!(
             glue.select_as_string(&parse("SELECT * FROM api_test").unwrap()[0]),
             Ok(vec![
-                vec![String::from("id"), String::from("name")],
-                vec![String::from("1"), String::from("test1")],
-                vec![String::from("2"), String::from("test2")]
+                vec![
+                    String::from("id"),
+                    String::from("name"),
+                    String::from("nullable"),
+                    String::from("is")
+                ],
+                vec![
+                    String::from("1"),
+                    String::from("test1"),
+                    String::from("not null"),
+                    String::from("TRUE")
+                ],
+                vec![
+                    String::from("2"),
+                    String::from("test2"),
+                    String::from("NULL"),
+                    String::from("FALSE")
+                ]
             ])
         );
-        std::fs::remove_dir_all(&path);
     }
 }
