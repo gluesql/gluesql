da0ff39366557ac7c8031392d36e0ee64687949a
Kyran Gostelow <kyran@gostelow.me>
2021-04-02 12:10:51 +1100

    Another test case (commented out)

--- src/tests/function/uuid.rs
+++ src/tests/function/uuid.rs
@@ -14,15 +14,29 @@ test_case!(uuid, async move {
                 Value::I64;
                 1
             )),
-        ), /* Cannot test as INSERT is behaving poorly at the moment
-           (
-               "INSERT INTO test (id) VALUES (UUID()), (UUID()), (UUID()), (UUID()), (UUID())",
-               Ok(Payload::Insert(5)), // Should error if UUID isn't working
-           ),
-           (
-               "INSERT INTO test (id) VALUES (UUID())",
-               Ok(Payload::Insert(1)), // Should error if UUID isn't working
-           ),*/
+        ),
+        /*( Cannot test as INSERT is behaving poorly at the moment
+            "INSERT INTO test (id) VALUES (UUID()), (UUID()), (UUID()), (UUID()), (UUID())",
+            Ok(Payload::Insert(5)), // Should error if UUID isn't working
+        ),
+        (
+            "INSERT INTO test (id) VALUES (UUID())",
+            Ok(Payload::Insert(1)), // Should error if UUID isn't working
+        ),*/
+
+        /*( Cannot test as INSERT is behaving poorly at the moment
+            "CREATE TABLE test_default (id INT UNIQUE NOT NULL DEFAULT UUID())",
+            Ok(Payload::Create),
+        ),
+        ("INSERT INTO test_default (id) VALUES (NULL)", Ok(Payload::Insert(1))),
+        (
+            "SELECT id FROM test_default", // Unreliable result
+            Ok(select!(
+                id
+                Value::I64;
+                1
+            )),
+        ),*/
     ];
 
     for (sql, expected) in test_cases.into_iter() {
