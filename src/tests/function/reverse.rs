08bfca3a8a4ad820addd111cf3c53b2ab24dd2ce
MeenSeek Kim <meenseek5929@naver.com>
2021-08-26 22:04:51 +0900

    [Update] - reverse test codes about null param

--- src/tests/function/reverse.rs
+++ src/tests/function/reverse.rs
@@ -4,9 +4,7 @@ test_case!(reverse, async move {
     let test_cases = vec![
         ("CREATE TABLE Item (name TEXT)", Ok(Payload::Create)),
         (
-            r#"INSERT INTO Item VALUES
-                ("Let's meet")
-            "#,
+            r#"INSERT INTO Item VALUES ("Let's meet")"#,
             Ok(Payload::Insert(1)),
         ),
         (
@@ -16,6 +14,22 @@ test_case!(reverse, async move {
                 Value::Str;
                 "teem s'teL".to_owned()
             )),
+        ),
+        (
+            r#"SELECT REVERSE(1) AS test FROM Item"#,
+            Err(EvaluateError::FunctionRequiresStringValue("REVERSE".to_owned()).into()),
+        ),
+        (
+            "CREATE TABLE NullTest (name TEXT null)",
+            Ok(Payload::Create),
+        ),
+        (
+            r#"INSERT INTO NullTest VALUES (null)"#,
+            Ok(Payload::Insert(1)),
+        ),
+        (
+            r#"SELECT REVERSE(name) AS test FROM NullTest"#,
+            Ok(select_with_null!(test; Value::Null)),
         ),
     ];
     for (sql, expected) in test_cases.into_iter() {
