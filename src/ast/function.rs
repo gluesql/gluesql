5e6610524b9018f5272d5ca0430f79f8c6506e99
inhwa1025 <inhwa9901@gmail.com>
2021-08-28 00:18:56 +0900

    Add REPEAT to Function enum in AST

--- src/ast/function.rs
+++ src/ast/function.rs
@@ -72,6 +72,8 @@ pub enum Function {
     Rtrim { expr: Expr, chars: Option<Expr> },
     #[strum(to_string = "REVERSE")]
     Reverse(Expr),
+    #[strum(to_string = "REPEAT")]
+    Repeat{ expr: Expr, num: Expr },
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
