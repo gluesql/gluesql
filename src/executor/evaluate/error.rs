5e31d34cd28946fede6bb8eb81bfdfb7c0937b7c
SeongHoon Wee <mrgravity817@gmail.com>
2021-10-10 00:56:09 +0900

    Remove unused  error

--- src/executor/evaluate/error.rs
+++ src/executor/evaluate/error.rs
@@ -57,7 +57,4 @@ pub enum EvaluateError {
 
     #[error("negative substring length not allowed")]
     NegativeSubstrLenNotAllowed,
-
-    #[error("the result types should be equal: {0}")]
-    UnequalResultTypes(String),
 }
