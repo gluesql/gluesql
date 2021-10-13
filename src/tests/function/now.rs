20f4c8cf997a4779dcc26c8e5ba5316cbf60353a
simple6192 <simple6192@naver.com>
2021-10-13 08:00:29 +0000

    add now test

---
+++ src/tests/function/now.rs
@@ -0,0 +1,29 @@
+use crate::*;
+
+test_case!(now, async move {
+    // use Value::*;
+    use crate::Value::Timestamp;
+    macro_rules! t {
+        ($timestamp: expr) => {
+            $timestamp.parse().unwrap()
+        };
+    }
+
+    let test_cases = vec![
+        ("CREATE TABLE Item (time TIMESTAMP)", Ok(Payload::Create)),
+        (
+            r#"INSERT INTO Item VALUES
+                ("2021-10-13T06:42:40.364832862"),
+                ("9999-12-31T23:59:40.364832862");"#,
+            Ok(Payload::Insert(2)),
+        ),
+        (
+            "SELECT time FROM Item WHERE time > NOW();",
+            Ok(select!("time" Timestamp; t!("9999-12-31T23:59:40.364832862"))),
+        ),
+    ];
+
+    for (sql, expected) in test_cases {
+        test!(expected, sql);
+    }
+});
