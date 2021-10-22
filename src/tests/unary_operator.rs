b19ffd68fb552bf6ae5be202dbd62aa2202443c2
devil-k <ehdgur0812@gmail.com>
2021-10-22 22:39:22 +0900

    add test factorial overflow

--- src/tests/unary_operator.rs
+++ src/tests/unary_operator.rs
@@ -4,17 +4,17 @@ test_case!(unary_operator, async move {
     use Value::*;
 
     let test_cases = vec![
-        ("CREATE TABLE Test (v1 INT, v2 FLOAT, v3 TEXT, v4 INT)", Ok(Payload::Create)),
+        ("CREATE TABLE Test (v1 INT, v2 FLOAT, v3 TEXT, v4 INT, v5 INT)", Ok(Payload::Create)),
         (
-            r#"INSERT INTO Test VALUES (10, 10.5, "hello", -5)"#,
+            r#"INSERT INTO Test VALUES (10, 10.5, "hello", -5, 1000)"#,
             Ok(Payload::Insert(1)),
         ),
         (
             "SELECT * FROM Test",
             Ok(select!(
-                v1  |   v2  |   v3  |               v4
-                I64 |   F64 |   Str |               I64;
-                10      10.5    "hello".to_owned()  i64::from(-5)
+                v1  |   v2  |   v3  |               v4  |           v5
+                I64 |   F64 |   Str |               I64 |           I64;
+                10      10.5    "hello".to_owned()  i64::from(-5)   1000
             )),
         ),
         (
@@ -66,14 +66,6 @@ test_case!(unary_operator, async move {
             )),
         ),
         (
-            "SELECT v2! as v2 FROM Test",
-            Err(ValueError::UnaryFactorialOnNonNumeric.into()),
-        ),
-        (
-            "SELECT v4! as v4 FROM Test",
-            Err(ValueError::UnaryFactorialOnNegativeNumeric.into()),
-        ),
-        (
             "SELECT 4! as v1 FROM Test",
             Ok(select!(
                 v1
@@ -82,16 +74,36 @@ test_case!(unary_operator, async move {
             )),
         ),
         (
+            "SELECT v2! as v2 FROM Test",
+            Err(ValueError::UnaryFactorialOnNonNumeric.into()),
+        ),
+        (
             "SELECT v3! as v1 FROM Test",
             Err(ValueError::UnaryFactorialOnNonNumeric.into()),
+        ),
+        (
+            "SELECT v4! as v4 FROM Test",
+            Err(ValueError::UnaryFactorialOnNegativeNumeric.into()),
+        ),
+        (
+            "SELECT v5! as v5 FROM Test",
+            Err(ValueError::UnaryFactorialOverflow.into()),
         ),
         (
             "SELECT (-5)! as v4 FROM Test",
             Err(LiteralError::UnaryFactorialOperationOnNegativeNumeric.into()),
         ),
         (
+            "SELECT (5.5)! as v4 FROM Test",
+            Err(LiteralError::UnreachableUnaryOperation.into()),
+        ),
+        (
             "SELECT 'errrr'! as v1 FROM Test",
             Err(LiteralError::UnaryOperationOnNonNumeric.into()),
+        ),
+        (
+            "SELECT 1000! as v4 FROM Test",
+            Err(LiteralError::UnaryFactorialOperationOverflow.into()),
         ),
     ];
 
