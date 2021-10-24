4b996a237a2c339a3f7de46f46f0046ba0b5b36b
Jiseok CHOI <jiseok.dev@gmail.com>
2021-10-24 15:40:41 +0900

    Remove `storage.clone()`, `Store` and `StoreMut`

--- src/storages/memory_storage/alter_table.rs
+++ src/storages/memory_storage/alter_table.rs
@@ -3,30 +3,39 @@
 use {
     super::MemoryStorage,
     crate::{
-        ast::ColumnDef,
-        result::MutResult,
-        schema::ColumnDefExt,
-        store::AlterTable,
-        store::{Store, StoreMut},
-        AlterTableError, Row, Value,
+        ast::ColumnDef, result::MutResult, schema::ColumnDefExt, store::AlterTable,
+        AlterTableError, Value,
     },
     async_trait::async_trait,
 };
+
+macro_rules! unwrap_or_return_err {
+    ($storage:expr, $expr:expr) => {
+        match $expr {
+            Ok(success) => success,
+            Err(err) => return Err(($storage, err.into())),
+        }
+    };
+}
 
 #[async_trait(?Send)]
 impl AlterTable for MemoryStorage {
     async fn rename_schema(self, table_name: &str, new_table_name: &str) -> MutResult<Self, ()> {
         let mut storage = self;
 
-        let mut item = storage
+        match storage
             .items
             .remove(table_name)
             .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-            .map_err(|err| err_into(&storage, err))?;
-        item.schema.table_name = new_table_name.to_owned();
-        storage.items.insert(new_table_name.to_owned(), item);
+        {
+            Ok(mut item) => {
+                item.schema.table_name = new_table_name.to_owned();
+                storage.items.insert(new_table_name.to_owned(), item);
 
-        Ok((storage, ()))
+                Ok((storage, ()))
+            }
+            Err(err) => Err((storage, err.into())),
+        }
     }
 
     async fn rename_column(
@@ -37,33 +46,37 @@ impl AlterTable for MemoryStorage {
     ) -> MutResult<Self, ()> {
         let mut storage = self;
 
-        let ms = storage.clone();
-        let item = storage
-            .items
-            .get_mut(table_name)
-            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-            .map_err(|err| err_into(&ms, err))?;
-        let column_def = item
-            .schema
-            .column_defs
-            .iter_mut()
-            .find(|column_def| column_def.name == old_column_name)
-            .ok_or(AlterTableError::RenamingColumnNotFound)
-            .map_err(|err| err_into(&ms, err))?;
+        let item = unwrap_or_return_err!(
+            storage,
+            storage
+                .items
+                .get_mut(table_name)
+                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
+        );
+
+        let column_def = unwrap_or_return_err!(
+            storage,
+            item.schema
+                .column_defs
+                .iter_mut()
+                .find(|column_def| column_def.name == old_column_name)
+                .ok_or(AlterTableError::RenamingColumnNotFound)
+        );
         column_def.name = new_column_name.to_owned();
 
         Ok((storage, ()))
     }
 
     async fn add_column(self, table_name: &str, column_def: &ColumnDef) -> MutResult<Self, ()> {
-        let storage = self;
+        let mut storage = self;
 
-        let ms = storage.clone();
-        let item = storage
-            .items
-            .get(table_name)
-            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-            .map_err(|err| err_into(&ms, err))?;
+        let item = unwrap_or_return_err!(
+            storage,
+            storage
+                .items
+                .get(table_name)
+                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
+        );
 
         if item
             .schema
@@ -74,7 +87,7 @@ impl AlterTable for MemoryStorage {
             let adding_column = column_def.name.to_owned();
 
             return Err((
-                ms.clone(),
+                storage,
                 AlterTableError::AddingColumnAlreadyExists(adding_column).into(),
             ));
         }
@@ -85,41 +98,29 @@ impl AlterTable for MemoryStorage {
         let value = match (default, nullable) {
             (Some(expr), _) => {
                 let evaluated =
-                    crate::evaluate_stateless(None, expr).map_err(|err| err_into(&ms, err))?;
+                    unwrap_or_return_err!(storage, crate::evaluate_stateless(None, expr));
 
-                evaluated
-                    .try_into_value(data_type, nullable)
-                    .map_err(|err| err_into(&ms, err))?
+                unwrap_or_return_err!(storage, evaluated.try_into_value(data_type, nullable))
             }
             (None, true) => Value::Null,
             (None, false) => {
                 return Err((
-                    ms.clone(),
+                    storage,
                     AlterTableError::DefaultValueRequired(column_def.clone()).into(),
                 ))
             }
         };
 
-        let rows = storage
-            .scan_data(table_name)
-            .await
-            .map_err(|err| err_into(&ms, err))?
-            .collect::<Result<Vec<_>, _>>()
-            .map_err(|err| err_into(&ms, err))?
-            .into_iter()
-            .map(|(key, mut row)| {
-                row.0.push(value.clone());
-                (key, row)
-            })
-            .collect::<Vec<_>>();
-
-        let mut storage = storage.update_data(table_name, rows).await?.0;
-
-        let item = storage
-            .items
-            .get_mut(table_name)
-            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-            .map_err(|err| err_into(&ms, err))?;
+        let item = unwrap_or_return_err!(
+            storage,
+            storage
+                .items
+                .get_mut(table_name)
+                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
+        );
+        item.rows.iter_mut().for_each(|(_, row)| {
+            row.0.push(value.clone());
+        });
         item.schema.column_defs.push(column_def.clone());
 
         Ok((storage, ()))
@@ -133,44 +134,30 @@ impl AlterTable for MemoryStorage {
     ) -> MutResult<Self, ()> {
         let mut storage = self;
 
-        let ms = storage.clone();
-        let item = storage
-            .items
-            .get_mut(table_name)
-            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-            .map_err(|err| (ms.clone(), err.into()))?;
+        let item = unwrap_or_return_err!(
+            storage,
+            storage
+                .items
+                .get_mut(table_name)
+                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
+        );
         let column_index = item
             .schema
             .column_defs
             .iter()
             .position(|column_def| column_def.name == column_name);
 
-        let rows = match column_index {
+        match column_index {
             Some(column_index) => {
                 item.schema.column_defs.remove(column_index);
 
-                storage
-                    .scan_data(table_name)
-                    .await
-                    .map_err(|err| err_into(&ms, err))?
-                    .collect::<Result<Vec<_>, _>>()
-                    .map_err(|err| err_into(&ms, err))?
-                    .into_iter()
-                    .map(|(key, row)| {
-                        let row = Row(row
-                            .0
-                            .into_iter()
-                            .enumerate()
-                            .filter_map(|(i, v)| (i != column_index).then(|| v))
-                            .collect());
-
-                        (key, row)
-                    })
-                    .collect::<Vec<_>>()
+                item.rows.iter_mut().for_each(|(_, row)| {
+                    if row.0.len() > column_index {
+                        row.0.remove(column_index);
+                    }
+                });
             }
-            None if if_exists => {
-                vec![]
-            }
+            None if if_exists => {}
             None => {
                 return Err((
                     storage,
@@ -179,12 +166,6 @@ impl AlterTable for MemoryStorage {
             }
         };
 
-        let storage = storage.update_data(table_name, rows).await?.0;
-
         Ok((storage, ()))
     }
-}
-
-fn err_into(ms: &MemoryStorage, err: impl Into<crate::Error>) -> (MemoryStorage, crate::Error) {
-    (ms.clone(), err.into())
 }
