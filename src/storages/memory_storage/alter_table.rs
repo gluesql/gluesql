479556374c2a2d32dc02f05cf89e69f2e53fdd22
Jiseok CHOI <jiseok.dev@gmail.com>
2021-11-02 00:37:08 +0900

    Replace `unwrap_or_return_err` macro to sync function

--- src/storages/memory_storage/alter_table.rs
+++ src/storages/memory_storage/alter_table.rs
@@ -4,18 +4,115 @@ use {
     super::MemoryStorage,
     crate::{
         ast::ColumnDef, result::MutResult, schema::ColumnDefExt, store::AlterTable,
-        AlterTableError, Value,
+        AlterTableError, Result, Value,
     },
     async_trait::async_trait,
 };
 
-macro_rules! unwrap_or_return_err {
-    ($storage:expr, $expr:expr) => {
-        match $expr {
-            Ok(success) => success,
-            Err(err) => return Err(($storage, err.into())),
+impl MemoryStorage {
+    fn rename_column_sync(
+        &mut self,
+        table_name: &str,
+        old_column_name: &str,
+        new_column_name: &str,
+    ) -> Result<()> {
+        let item = self
+            .items
+            .get_mut(table_name)
+            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))?;
+
+        let mut column_def = item
+            .schema
+            .column_defs
+            .iter_mut()
+            .find(|column_def| column_def.name == old_column_name)
+            .ok_or(AlterTableError::RenamingColumnNotFound)?;
+
+        column_def.name = new_column_name.to_owned();
+
+        Ok(())
+    }
+
+    fn add_column_sync(&mut self, table_name: &str, column_def: &ColumnDef) -> Result<()> {
+        let item = self
+            .items
+            .get(table_name)
+            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))?;
+
+        if item
+            .schema
+            .column_defs
+            .iter()
+            .any(|ColumnDef { name, .. }| name == &column_def.name)
+        {
+            let adding_column = column_def.name.to_owned();
+
+            return Err(AlterTableError::AddingColumnAlreadyExists(adding_column).into());
         }
-    };
+
+        let ColumnDef { data_type, .. } = column_def;
+        let nullable = column_def.is_nullable();
+        let default = column_def.get_default();
+        let value = match (default, nullable) {
+            (Some(expr), _) => {
+                let evaluated = crate::evaluate_stateless(None, expr)?;
+
+                evaluated.try_into_value(data_type, nullable)?
+            }
+            (None, true) => Value::Null,
+            (None, false) => {
+                return Err(AlterTableError::DefaultValueRequired(column_def.clone()).into())
+            }
+        };
+
+        let item = self
+            .items
+            .get_mut(table_name)
+            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))?;
+
+        item.rows.iter_mut().for_each(|(_, row)| {
+            row.0.push(value.clone());
+        });
+        item.schema.column_defs.push(column_def.clone());
+
+        Ok(())
+    }
+
+    fn drop_column_sync(
+        &mut self,
+        table_name: &str,
+        column_name: &str,
+        if_exists: bool,
+    ) -> Result<()> {
+        let item = self
+            .items
+            .get_mut(table_name)
+            .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))?;
+
+        let column_index = item
+            .schema
+            .column_defs
+            .iter()
+            .position(|column_def| column_def.name == column_name);
+
+        match column_index {
+            Some(column_index) => {
+                item.schema.column_defs.remove(column_index);
+
+                item.rows.iter_mut().for_each(|(_, row)| {
+                    if row.0.len() > column_index {
+                        row.0.remove(column_index);
+                    }
+                });
+            }
+            None if if_exists => {}
+            None => {
+                return Err(AlterTableError::DroppingColumnNotFound(column_name.to_owned()).into())
+            }
+        };
+
+        Ok(())
+    }
 }
 
 #[async_trait(?Send)]
@@ -46,84 +143,19 @@ impl AlterTable for MemoryStorage {
     ) -> MutResult<Self, ()> {
         let mut storage = self;
 
-        let item = unwrap_or_return_err!(
-            storage,
-            storage
-                .items
-                .get_mut(table_name)
-                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-        );
-
-        let column_def = unwrap_or_return_err!(
-            storage,
-            item.schema
-                .column_defs
-                .iter_mut()
-                .find(|column_def| column_def.name == old_column_name)
-                .ok_or(AlterTableError::RenamingColumnNotFound)
-        );
-        column_def.name = new_column_name.to_owned();
-
-        Ok((storage, ()))
+        match storage.rename_column_sync(table_name, old_column_name, new_column_name) {
+            Ok(()) => Ok((storage, ())),
+            Err(err) => Err((storage, err)),
+        }
     }
 
     async fn add_column(self, table_name: &str, column_def: &ColumnDef) -> MutResult<Self, ()> {
         let mut storage = self;
 
-        let item = unwrap_or_return_err!(
-            storage,
-            storage
-                .items
-                .get(table_name)
-                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-        );
-
-        if item
-            .schema
-            .column_defs
-            .iter()
-            .any(|ColumnDef { name, .. }| name == &column_def.name)
-        {
-            let adding_column = column_def.name.to_owned();
-
-            return Err((
-                storage,
-                AlterTableError::AddingColumnAlreadyExists(adding_column).into(),
-            ));
+        match storage.add_column_sync(table_name, column_def) {
+            Ok(()) => Ok((storage, ())),
+            Err(err) => Err((storage, err)),
         }
-
-        let ColumnDef { data_type, .. } = column_def;
-        let nullable = column_def.is_nullable();
-        let default = column_def.get_default();
-        let value = match (default, nullable) {
-            (Some(expr), _) => {
-                let evaluated =
-                    unwrap_or_return_err!(storage, crate::evaluate_stateless(None, expr));
-
-                unwrap_or_return_err!(storage, evaluated.try_into_value(data_type, nullable))
-            }
-            (None, true) => Value::Null,
-            (None, false) => {
-                return Err((
-                    storage,
-                    AlterTableError::DefaultValueRequired(column_def.clone()).into(),
-                ))
-            }
-        };
-
-        let item = unwrap_or_return_err!(
-            storage,
-            storage
-                .items
-                .get_mut(table_name)
-                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-        );
-        item.rows.iter_mut().for_each(|(_, row)| {
-            row.0.push(value.clone());
-        });
-        item.schema.column_defs.push(column_def.clone());
-
-        Ok((storage, ()))
     }
 
     async fn drop_column(
@@ -134,38 +166,9 @@ impl AlterTable for MemoryStorage {
     ) -> MutResult<Self, ()> {
         let mut storage = self;
 
-        let item = unwrap_or_return_err!(
-            storage,
-            storage
-                .items
-                .get_mut(table_name)
-                .ok_or_else(|| AlterTableError::TableNotFound(table_name.to_owned()))
-        );
-        let column_index = item
-            .schema
-            .column_defs
-            .iter()
-            .position(|column_def| column_def.name == column_name);
-
-        match column_index {
-            Some(column_index) => {
-                item.schema.column_defs.remove(column_index);
-
-                item.rows.iter_mut().for_each(|(_, row)| {
-                    if row.0.len() > column_index {
-                        row.0.remove(column_index);
-                    }
-                });
-            }
-            None if if_exists => {}
-            None => {
-                return Err((
-                    storage,
-                    AlterTableError::DroppingColumnNotFound(column_name.to_owned()).into(),
-                ))
-            }
-        };
-
-        Ok((storage, ()))
+        match storage.drop_column_sync(table_name, column_name, if_exists) {
+            Ok(()) => Ok((storage, ())),
+            Err(err) => Err((storage, err)),
+        }
     }
 }
