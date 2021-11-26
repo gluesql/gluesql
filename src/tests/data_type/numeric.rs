e72c8e77f3332426736ec7417aed89099609fd90
Jiseok CHOI <jiseok.dev@gmail.com>
2021-11-26 20:51:20 +0900

    Separate binary operator test cases

--- src/tests/data_type/numeric.rs
+++ src/tests/data_type/numeric.rs
@@ -7,7 +7,7 @@ test_case!(numeric, async move {
         field_two INT(8),
     );"
     );
-    run!("INSERT INTO Item VALUES (1, -1), (-2, 2), (0, 127), (0, -127);");
+    run!("INSERT INTO Item VALUES (1, -1), (-2, 2), (3, 3), (-4, -4);");
 
     let parse_i8 = |text: &str| -> i8 { text.parse().unwrap() };
 
@@ -26,8 +26,8 @@ test_case!(numeric, async move {
             I8               |    I8;
             1                   parse_i8("-1");
             parse_i8("-2")         2;
-            0                     127;
-            0                   parse_i8("-127")
+            3                      3;
+            parse_i8("-4")      parse_i8("-4")
         )),
         "SELECT field_one, field_two FROM Item"
     );
@@ -36,7 +36,8 @@ test_case!(numeric, async move {
         Ok(select!(
             field_one
             I8;
-            1
+            1;
+            3
         )),
         "SELECT field_one FROM Item WHERE field_one > 0"
     );
@@ -45,8 +46,7 @@ test_case!(numeric, async move {
             field_one
             I8;
             1;
-            0;
-            0
+            3
         )),
         "SELECT field_one FROM Item WHERE field_one >= 0"
     );
@@ -55,47 +55,89 @@ test_case!(numeric, async move {
         Ok(select!(
             field_one
             I8;
-            0;
-            0
+            -2
         )),
-        "SELECT field_one FROM Item WHERE field_one = 0"
+        "SELECT field_one FROM Item WHERE field_one = -2"
     );
 
     test!(
         Ok(select!(
             field_one
             I8;
-            -2
+            -2;
+            -4
         )),
         "SELECT field_one FROM Item WHERE field_one < 0"
     );
+
     test!(
         Ok(select!(
             field_one
             I8;
             -2;
-            0;
-            0
+            -4
         )),
         "SELECT field_one FROM Item WHERE field_one <= 0"
     );
 
     test!(
         Ok(select!(
-            plus             |      sub           |       mul      |       div       |   modulo
-            I8               |      I8            |        I8      |       I8        |     I8;
-            0                        2              parse_i8("-1")    parse_i8("-1")        0;
-            0                   parse_i8("-4")      parse_i8("-4")    parse_i8("-1")        0;
-            127                 parse_i8("-127")           0                0               0;
-            parse_i8("-127")        127                    0                0               0
+            plus
+            I8;
+            0;
+            0;
+            6;
+            -8
         )),
-        "SELECT
-            field_one + field_two AS plus,
-            field_one - field_two AS sub,
-            field_one * field_two AS mul,
-            field_one / field_two AS div,
-            field_one % field_two AS modulo
-            FROM Item;"
+        "SELECT field_one + field_two AS plus FROM Item;"
+    );
+
+    test!(
+        Ok(select!(
+            sub
+            I8;
+            2;
+            -4;
+            0;
+            0
+        )),
+        "SELECT field_one - field_two AS sub FROM Item;"
+    );
+
+    test!(
+        Ok(select!(
+            mul
+            I8;
+            -1;
+            -4;
+            9;
+            16
+        )),
+        "SELECT field_one * field_two AS mul FROM Item;"
+    );
+
+    test!(
+        Ok(select!(
+            div
+            I8;
+            -1;
+            -1;
+            1;
+            1
+        )),
+        "SELECT field_one / field_two AS div FROM Item;"
+    );
+
+    test!(
+        Ok(select!(
+            modulo
+            I8;
+            0;
+            0;
+            0;
+            0
+        )),
+        "SELECT field_one % field_two AS modulo FROM Item;"
     );
 
     run!("DELETE FROM Item");
