50d502d32d75fd54cfec9853a12a64595626128c
Taehoon Moon <taehoon.moon@outlook.com>
2021-12-15 22:28:22 +0900

    Replace replacen to strip_prefix in sled_storage/metadata, clean up codes

--- src/storages/sled_storage/metadata.rs
+++ src/storages/sled_storage/metadata.rs
@@ -11,6 +11,8 @@ use {
     std::str,
 };
 
+const SCHEMA_PREFIX: &str = "schema/";
+
 #[async_trait(?Send)]
 impl Metadata for SledStorage {
     async fn schema_names(&self) -> Result<Vec<String>> {
@@ -25,22 +27,26 @@ impl Metadata for SledStorage {
             }
         };
         let lock_txid = lock::fetch(&self.tree, txid, created_at, self.tx_timeout)?;
-        let prefix = "schema/";
 
         self.tree
-            .scan_prefix(prefix)
+            .scan_prefix(SCHEMA_PREFIX)
             .map(move |item| {
                 let (key, value) = item.map_err(err_into)?;
                 let snapshot: Snapshot<Schema> = bincode::deserialize(&value).map_err(err_into)?;
                 let schema = snapshot.extract(txid, lock_txid);
+                if schema.is_none() {
+                    return Ok(None);
+                }
 
-                schema
-                    .map(|_| {
-                        str::from_utf8(key.as_ref())
-                            .map(|s| s.replacen(prefix, "", 1))
-                            .map_err(err_into)
+                str::from_utf8(key.as_ref())
+                    .map_err(err_into)?
+                    .strip_prefix(SCHEMA_PREFIX)
+                    .map(|prefix| Some(prefix.to_owned()))
+                    .ok_or_else(|| {
+                        Error::StorageMsg(
+                            "conflict - schema_names failed, strip_prefix not matched".to_owned(),
+                        )
                     })
-                    .transpose()
             })
             .filter_map(|item| item.transpose())
             .collect::<Result<Vec<_>>>()
