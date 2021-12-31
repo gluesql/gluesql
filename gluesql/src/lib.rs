c5644842501aef84fd517f9537c99ede72f5d698
Jiseok CHOI <jiseok.dev@gmail.com>
2021-12-31 17:39:15 +0900

    Move core lib-doc to gluesql lib-doc

---
+++ gluesql/src/lib.rs
@@ -0,0 +1,70 @@
+//! # GlueSQL
+//!
+//! `gluesql` is a SQL database library written in Rust.  
+//! It provides a parser ([sqlparser-rs](https://github.com/ballista-compute/sqlparser-rs)), execution layer,
+//! and optional storages ([`sled`](https://github.com/spacejam/sled) or `memory`) packaged into a single library.  
+//! Developers can choose to use GlueSQL to build their own SQL database,
+//! or as an embedded SQL database using the default storage engine.
+//!
+//! ## Examples
+//!
+//! ```
+//! use gluesql::prelude::*;
+//!
+//! fn main() {
+//!     let storage = SledStorage::new("data/doc-db").unwrap();
+//!     let mut glue = Glue::new(storage);
+//!     
+//!     let sqls = vec![
+//!         "DROP TABLE IF EXISTS Glue;",
+//!         "CREATE TABLE Glue (id INTEGER);",
+//!         "INSERT INTO Glue VALUES (100);",
+//!         "INSERT INTO Glue VALUES (200);",
+//!         "SELECT * FROM Glue WHERE id > 100;",
+//!     ];
+//!
+//!     for sql in sqls {
+//!         let output = glue.execute(sql).unwrap();
+//!         println!("{:?}", output)
+//!     }
+//! }
+//! ```
+//!
+//! ## Custom Storage
+//! To get started, all you need to implement for `gluesql` is implementing three traits
+//! (two for functions, one for running tests).  
+//! There are also optional traits.
+//! Whether to implement it or not is all up to you.
+//!
+//! ### Store traits
+//! * [Store](store/trait.Store.html)
+//! * [StoreMut](store/trait.StoreMut.html)
+//!
+//! ### Optional Store traits
+//! * [AlterTable](store/trait.AlterTable.html)
+//! * [Index](store/trait.Index.html)
+//! * [IndexMut](store/trait.IndexMut.html)
+//! * [Transaction](store/trait.Transaction.html)
+//! * [Metadata](store/trait.Metadata.html)
+//!
+//! ### Trait to run integration tests
+//! * [Tester](tests/trait.Tester.html)
+//!
+//! ## Tests
+//! `gluesql` provides integration tests as a module.  
+//! Developers who wants to make their own custom storages can import and run those tests.  
+//! `/tests/` might look quite empty, but actual test cases exist in `src/tests/`.  
+//!
+//! Example code to see,
+//! * [tests/sled_storage.rs](https://github.com/gluesql/gluesql/blob/main/tests/sled_storage.rs)
+//! * [tests/memory_storage.rs](https://github.com/gluesql/gluesql/blob/main/tests/memory_storage.rs)
+//!
+//! After you implement `Tester` trait, the only thing you need to do is calling `generate_tests!` macro.
+
+pub use gluesql_core;
+pub use memory_storage;
+pub use sled_storage;
+
+pub mod prelude {
+    pub use {gluesql_core::prelude::*, memory_storage::MemoryStorage, sled_storage::SledStorage};
+}
