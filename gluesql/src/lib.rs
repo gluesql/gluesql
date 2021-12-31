cea107b50aa0198324071db4a3d1101e92f0be5e
Jiseok CHOI <jiseok.dev@gmail.com>
2021-12-31 17:43:24 +0900

    Remove needless doc-main

--- gluesql/src/lib.rs
+++ gluesql/src/lib.rs
@@ -11,22 +11,20 @@
 //! ```
 //! use gluesql::prelude::*;
 //!
-//! fn main() {
-//!     let storage = SledStorage::new("data/doc-db").unwrap();
-//!     let mut glue = Glue::new(storage);
+//! let storage = SledStorage::new("data/doc-db").unwrap();
+//! let mut glue = Glue::new(storage);
 //!     
-//!     let sqls = vec![
-//!         "DROP TABLE IF EXISTS Glue;",
-//!         "CREATE TABLE Glue (id INTEGER);",
-//!         "INSERT INTO Glue VALUES (100);",
-//!         "INSERT INTO Glue VALUES (200);",
-//!         "SELECT * FROM Glue WHERE id > 100;",
-//!     ];
+//! let sqls = vec![
+//!     "DROP TABLE IF EXISTS Glue;",
+//!     "CREATE TABLE Glue (id INTEGER);",
+//!     "INSERT INTO Glue VALUES (100);",
+//!     "INSERT INTO Glue VALUES (200);",
+//!     "SELECT * FROM Glue WHERE id > 100;",
+//! ];
 //!
-//!     for sql in sqls {
-//!         let output = glue.execute(sql).unwrap();
-//!         println!("{:?}", output)
-//!     }
+//! for sql in sqls {
+//!     let output = glue.execute(sql).unwrap();
+//!     println!("{:?}", output)
 //! }
 //! ```
 //!
