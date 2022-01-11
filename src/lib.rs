bd7c654669320de0cc73c7cf13454a3dadcc1363
Taehoon Moon <taehoon.moon@outlook.com>
2022-01-11 20:18:35 +0900

    Fix src/lib.rs doc comment, (#469)
    Rename gluesql_sled_storage to sled-storage.
    Fix it to work with or without sled-storage feature

--- src/lib.rs
+++ src/lib.rs
@@ -9,24 +9,29 @@
 //! ## Examples
 //!
 //! ```
-//! #![cfg(feature = "gluesql_sled_storage")]
-//! use gluesql::prelude::*;
+//! #[cfg(feature = "sled-storage")]
+//! fn main() {
+//!     use gluesql::prelude::*;
 //!
-//! let storage = SledStorage::new("data/doc-db").unwrap();
-//! let mut glue = Glue::new(storage);
+//!     let storage = SledStorage::new("data/doc-db").unwrap();
+//!     let mut glue = Glue::new(storage);
 //!     
-//! let sqls = vec![
-//!     "DROP TABLE IF EXISTS Glue;",
-//!     "CREATE TABLE Glue (id INTEGER);",
-//!     "INSERT INTO Glue VALUES (100);",
-//!     "INSERT INTO Glue VALUES (200);",
-//!     "SELECT * FROM Glue WHERE id > 100;",
-//! ];
+//!     let sqls = vec![
+//!         "DROP TABLE IF EXISTS Glue;",
+//!         "CREATE TABLE Glue (id INTEGER);",
+//!         "INSERT INTO Glue VALUES (100);",
+//!         "INSERT INTO Glue VALUES (200);",
+//!         "SELECT * FROM Glue WHERE id > 100;",
+//!     ];
 //!
-//! for sql in sqls {
-//!     let output = glue.execute(sql).unwrap();
-//!     println!("{:?}", output)
+//!     for sql in sqls {
+//!         let output = glue.execute(sql).unwrap();
+//!         println!("{:?}", output)
+//!     }
 //! }
+//!
+//! #[cfg(not(feature = "sled-storage"))]
+//! fn main() {}
 //! ```
 //!
 //! ## Custom Storage
