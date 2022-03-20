f3e27ad63473bfba6f5041b4f3c45ed0d1c90663
Jiseok CHOI <jiseok.dev@gmail.com>
2022-03-20 23:51:19 +0900

    Implement `schema_names` method

--- storages/multi-threaded-memory-storage/src/metadata.rs
+++ storages/multi-threaded-memory-storage/src/metadata.rs
@@ -2,15 +2,18 @@ use {
     super::MultiThreadedMemoryStorage,
     async_trait::async_trait,
     gluesql_core::{result::Result, store::Metadata},
+    std::sync::Arc,
 };
 
 #[async_trait(?Send)]
 impl Metadata for MultiThreadedMemoryStorage {
     async fn schema_names(&self) -> Result<Vec<String>> {
-        // let mut names: Vec<_> = self.items.keys().map(Clone::clone).collect();
-        // names.sort();
+        let items = Arc::clone(&self.items);
+        let items = items.read().await;
 
-        // Ok(names)
-        unimplemented!()
+        let mut names: Vec<_> = items.keys().map(Clone::clone).collect();
+        names.sort();
+
+        Ok(names)
     }
 }
