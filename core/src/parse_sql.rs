310565047f58cf7c2daeaf34c580a75105660c32
Jiseok CHOI <jiseok.dev@gmail.com>
2022-04-01 16:12:00 +0900

    Use `AsRef`

--- core/src/parse_sql.rs
+++ core/src/parse_sql.rs
@@ -8,15 +8,15 @@ use {
     },
 };
 
-pub fn parse(sql: &str) -> Result<Vec<SqlStatement>> {
+pub fn parse<Sql: AsRef<str>>(sql: Sql) -> Result<Vec<SqlStatement>> {
     let dialect = GenericDialect {};
 
-    Parser::parse_sql(&dialect, sql).map_err(|e| Error::Parser(format!("{:#?}", e)))
+    Parser::parse_sql(&dialect, sql.as_ref()).map_err(|e| Error::Parser(format!("{:#?}", e)))
 }
 
-pub fn parse_query(sql_expr: &str) -> Result<SqlQuery> {
+pub fn parse_query<Sql: AsRef<str>>(sql_expr: Sql) -> Result<SqlQuery> {
     let dialect = GenericDialect {};
-    let tokens = Tokenizer::new(&dialect, sql_expr)
+    let tokens = Tokenizer::new(&dialect, sql_expr.as_ref())
         .tokenize()
         .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
 
@@ -25,9 +25,9 @@ pub fn parse_query(sql_expr: &str) -> Result<SqlQuery> {
         .map_err(|e| Error::Parser(format!("{:#?}", e)))
 }
 
-pub fn parse_expr(sql_expr: &str) -> Result<SqlExpr> {
+pub fn parse_expr<Sql: AsRef<str>>(sql_expr: Sql) -> Result<SqlExpr> {
     let dialect = GenericDialect {};
-    let tokens = Tokenizer::new(&dialect, sql_expr)
+    let tokens = Tokenizer::new(&dialect, sql_expr.as_ref())
         .tokenize()
         .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
 
@@ -36,9 +36,9 @@ pub fn parse_expr(sql_expr: &str) -> Result<SqlExpr> {
         .map_err(|e| Error::Parser(format!("{:#?}", e)))
 }
 
-pub fn parse_interval(sql_interval: &str) -> Result<SqlExpr> {
+pub fn parse_interval<Sql: AsRef<str>>(sql_interval: Sql) -> Result<SqlExpr> {
     let dialect = GenericDialect {};
-    let tokens = Tokenizer::new(&dialect, sql_interval)
+    let tokens = Tokenizer::new(&dialect, sql_interval.as_ref())
         .tokenize()
         .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
 
