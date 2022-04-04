7f6af7c793303be37b467729153722357a289cfb
Kyran Gostelow <kyran@gostelow.me>
2022-04-04 12:07:06 +1000

    Really actually working

--- test-suite/benches/glue_benchmark.rs
+++ test-suite/benches/glue_benchmark.rs
@@ -1,89 +1,102 @@
-use {
-	criterion::*,
-	gluesql::prelude::*,
-	std::time::Duration,
-	sled::IVec,
-};
+use {criterion::*, gluesql::prelude::*, sled::IVec, std::time::Duration};
 
 fn setup_glue() -> Glue<IVec, SledStorage> {
-	let path = "data/sled_bench";
+    let path = "data/sled_bench";
 
-	match std::fs::remove_dir_all(&path) {
-		Ok(()) => (),
-		Err(e) => {
-			println!("fs::remove_file {:?}", e);
-		}
-	}
+    match std::fs::remove_dir_all(&path) {
+        Ok(()) => (),
+        Err(e) => {
+            println!("fs::remove_file {:?}", e);
+        }
+    }
 
-	let storage = SledStorage::new(&path)
-		.expect("Create Storage");
+    let storage = SledStorage::new(&path).expect("Create Storage");
 
-	Glue::new(storage)
+    Glue::new(storage)
 }
 
 fn setup_a(glue: &mut Glue<IVec, SledStorage>) {
-	glue.execute(
-		"
+    glue.execute(
+        "
 		CREATE TABLE A (
-			pk INTEGER PRIMARY KEY
+			pk INTEGER
 		)
 	",
-	)
-	.unwrap();
-	glue.execute(
-		"
+    )
+    .unwrap();
+    glue.execute(
+        "
 		CREATE INDEX primkey ON A (pk)
 	",
-	)
-	.unwrap();
-	(0..10_000).into_iter().for_each(|pk| {glue.execute(&format!("INSERT INTO A VALUES ({pk})", pk=pk)).unwrap();});
+    )
+    .unwrap();
+    (0..10_000).into_iter().for_each(|pk| {
+        glue.execute(&format!("INSERT INTO A VALUES ({pk})", pk = pk))
+            .unwrap();
+    });
 }
 
 fn setup_b(glue: &mut Glue<IVec, SledStorage>) {
-	glue.execute(
-		"
+    glue.execute(
+        "
 		CREATE TABLE B (
-			pk INTEGER AUTO_INCREMENT PRIMARY KEY,
+			pk INTEGER,
 			fk INTEGER,
 			val FLOAT
 		)
 	",
-	)
-	.unwrap();
-	glue.execute(
-		"
+    )
+    .unwrap();
+    glue.execute(
+        "
 		CREATE INDEX primkey ON B (pk)
 	",
-	)
-	.unwrap();
-	(0..100_000).into_iter().for_each(|_| {glue.execute(&format!("INSERT INTO B VALUES ({fk}, {val})", fk=fastrand::i64(0..10_000), val=fastrand::f64())).unwrap();});
+    )
+    .unwrap();
+    (0..100_000).into_iter().for_each(|pk| {
+        glue.execute(&format!(
+            "INSERT INTO B VALUES ({pk}, {fk}, {val})",
+            pk = pk,
+            fk = fastrand::i64(0..10_000),
+            val = fastrand::f64()
+        ))
+        .unwrap();
+    });
 }
 
 fn setup_c(glue: &mut Glue<IVec, SledStorage>) {
-	glue.execute(
-		"
+    glue.execute(
+        "
 		CREATE TABLE C (
-			pk INTEGER AUTO_INCREMENT PRIMARY KEY,
+			pk INTEGER,
 			fk INTEGER,
 			val FLOAT
 		)
 	",
-	)
-	.unwrap();
-	(0..100_000).into_iter().for_each(|_| {glue.execute(&format!("INSERT INTO C VALUES ({fk}, {val})", fk=fastrand::i64(0..10_000), val=fastrand::f64())).unwrap();});
+    )
+    .unwrap();
+    (0..100_000).into_iter().for_each(|pk| {
+        glue.execute(&format!(
+            "INSERT INTO C VALUES ({pk}, {fk}, {val})",
+            pk = pk,
+            fk = fastrand::i64(0..10_000),
+            val = fastrand::f64()
+        ))
+        .unwrap();
+    });
 }
 
 fn setup() -> Glue<IVec, SledStorage> {
-	let mut glue = setup_glue();
-	setup_a(&mut glue);
-	setup_b(&mut glue);
-	setup_c(&mut glue);
-	glue
+    let mut glue = setup_glue();
+    setup_a(&mut glue);
+    setup_b(&mut glue);
+    setup_c(&mut glue);
+    glue
 }
 
 fn filter(table: &str) -> String {
-	format!(
-		"
+    format!(
+        "
 		SELECT
 			*
 		FROM
@@ -91,12 +104,12 @@ fn filter(table: &str) -> String {
 		WHERE
 			pk < 100
 	",
-		table
-	)
+        table
+    )
 }
 fn find(table: &str) -> String {
-	format!(
-		"
+    format!(
+        "
 		SELECT
 			*
 		FROM
@@ -104,12 +117,12 @@ fn find(table: &str) -> String {
 		WHERE
 			pk = 100
 	",
-		table
-	)
+        table
+    )
 }
 fn sum_group(table: &str) -> String {
-	format!(
-		"
+    format!(
+        "
 		SELECT
 			SUM(val)
 		FROM
@@ -117,12 +130,12 @@ fn sum_group(table: &str) -> String {
 		GROUP BY
 			fk
 	",
-		table
-	)
+        table
+    )
 }
 fn join(table: &str) -> String {
-	format!(
-		"
+    format!(
+        "
 		SELECT
 			SUM(val)
 		FROM
@@ -132,65 +145,65 @@ fn join(table: &str) -> String {
 		GROUP BY
 			A.pk
 	",
-		table = table
-	)
+        table = table
+    )
 }
 
 fn bench(criterion: &mut Criterion) {
-	let mut glue = setup();
+    let mut glue = setup();
 
-	let mut group = criterion.benchmark_group("filter");
-	group.bench_function("a", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&filter("A")).unwrap());
-	});
-	group.bench_function("b", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&filter("B")).unwrap());
-	});
-	group.bench_function("c", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&filter("C")).unwrap());
-	});
-	group.finish();
+    let mut group = criterion.benchmark_group("filter");
+    group.bench_function("a", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&filter("A")).unwrap());
+    });
+    group.bench_function("b", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&filter("B")).unwrap());
+    });
+    group.bench_function("c", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&filter("C")).unwrap());
+    });
+    group.finish();
 
-	let mut group = criterion.benchmark_group("find");
-	group.bench_function("a", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&find("A")).unwrap());
-	});
-	group.bench_function("b", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&find("B")).unwrap());
-	});
-	group.bench_function("c", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&find("C")).unwrap());
-	});
-	group.finish();
+    let mut group = criterion.benchmark_group("find");
+    group.bench_function("a", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&find("A")).unwrap());
+    });
+    group.bench_function("b", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&find("B")).unwrap());
+    });
+    group.bench_function("c", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&find("C")).unwrap());
+    });
+    group.finish();
 
-	let mut group = criterion.benchmark_group("sum_group");
-	group
-		.sampling_mode(SamplingMode::Flat)
-		.measurement_time(Duration::from_secs(20));
-	group.bench_function("b", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&sum_group("B")).unwrap());
-	});
-	group.bench_function("c", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&sum_group("C")).unwrap());
-	});
-	group.finish();
+    let mut group = criterion.benchmark_group("sum_group");
+    group
+        .sampling_mode(SamplingMode::Flat)
+        .measurement_time(Duration::from_secs(20));
+    group.bench_function("b", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&sum_group("B")).unwrap());
+    });
+    group.bench_function("c", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&sum_group("C")).unwrap());
+    });
+    group.finish();
 
-	let mut group = criterion.benchmark_group("join");
-	group
-		.sampling_mode(SamplingMode::Flat)
-		.measurement_time(Duration::from_secs(30));
-	group.bench_function("b", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&join("B")).unwrap());
-	});
-	group.bench_function("c", |benchmarker| {
-		benchmarker.iter(|| glue.execute(&join("C")).unwrap());
-	});
-	group.finish();
+    let mut group = criterion.benchmark_group("join");
+    group
+        .sampling_mode(SamplingMode::Flat)
+        .measurement_time(Duration::from_secs(30));
+    group.bench_function("b", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&join("B")).unwrap());
+    });
+    group.bench_function("c", |benchmarker| {
+        benchmarker.iter(|| glue.execute(&join("C")).unwrap());
+    });
+    group.finish();
 }
 
 criterion_group! {
-	name = benches;
-	config = Criterion::default().noise_threshold(0.05).sample_size(10).warm_up_time(Duration::from_secs(5)).measurement_time(Duration::from_secs(10));
-	targets = bench
+    name = benches;
+    config = Criterion::default().noise_threshold(0.05).sample_size(10).warm_up_time(Duration::from_secs(5)).measurement_time(Duration::from_secs(10));
+    targets = bench
 }
 criterion_main!(benches);
