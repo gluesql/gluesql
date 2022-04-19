f2cf2805e0953e0400d00a4a01a4ae47691e2bc7
Billy Earney <billy.earney@gmail.com>
2022-04-19 08:08:59 -0500

    fix tests (select! => Payload:ShowColumns)

--- test-suite/src/showcolumns.rs
+++ test-suite/src/showcolumns.rs
@@ -1,39 +1,8 @@
 use crate::*;
 
-macro_rules! row {
-    ( $( $p:path )* ; $( $v:expr )* ) => (
-        ($( $p($v) ),*)
-    )
-}
-
-macro_rules! showcolumns {
-    ( $( $c: tt )|+ $( ; )? $( $t: path )|+ ; $( $v: expr )+ ; $( $( $v2: expr )+ );+) => ({
-        let mut rows = vec![
-            row!($( $t )+ ; $( $v )+),
-        ];
-
-        gluesql_core::executor::Payload::ShowColumns (
-          //  labels: vec![$( stringify!($c).to_owned().replace("\"", "")),+],
-           concat_with!(rows ; $( $t )+ ; $( $( $v2 )+ );+)
-        )
-    });
-    ( $( $c: tt )|+ $( ; )? $( $t: path )|+ ; $( $v: expr )+ ) => (
-        gluesql_core::executor::Payload::ShowColumns (
-           // labels: vec![$( stringify!($c).to_owned().replace("\"", "")),+],
-            vec![row!($( $t )+ ; $( $v )+ )],
-        )
-    );
-    ( $( $c: tt )|+ $( ; )?) => (
-        gluesql_core::executor::Payload::ShowColumns (
-            //labels: vec![$( stringify!($c).to_owned().replace("\"", "")),+],
-            vec![],
-        )
-    );
-}
-
 test_case!(showcolumns, async move {
     use gluesql_core::ast::DataType;
-    use gluesql_core::{executor::ExecuteError, prelude::Value::*};
+    use gluesql_core::{executor::ExecuteError, executor::Payload};
 
     run!(
         "
@@ -56,23 +25,21 @@ test_case!(showcolumns, async move {
     );
 
     test!(
-        Ok(showcolumns!(
-            Field               | Type
-            Str                 | Str;
-            "id8".to_owned()      DataType::Int8.to_string();
-            "id".to_owned()       DataType::Int.to_string();
-            "rate".to_owned()     DataType::Float.to_string();
-            "dec".to_owned()      DataType::Decimal.to_string();
-            "flag".to_owned()     DataType::Boolean.to_string();
-            "text".to_owned()     DataType::Text.to_string();
-            "DOB".to_owned()      DataType::Date.to_string();
-            "Tm".to_owned()       DataType::Time.to_string();
-            "ival".to_owned()     DataType::Interval.to_string();
-            "tstamp".to_owned()   DataType::Timestamp.to_string();
-            "uid".to_owned()      DataType::Uuid.to_string();
-            "hash".to_owned()     DataType::Map.to_string();
-            "glist".to_owned()    DataType::List.to_string()
-        )),
+        Ok(Payload::ShowColumns(vec![
+            ("id8".to_owned(), DataType::Int8),
+            ("id".to_owned(), DataType::Int),
+            ("rate".to_owned(), DataType::Float),
+            ("dec".to_owned(), DataType::Decimal),
+            ("flag".to_owned(), DataType::Boolean),
+            ("text".to_owned(), DataType::Text),
+            ("DOB".to_owned(), DataType::Date),
+            ("Tm".to_owned(), DataType::Time),
+            ("ival".to_owned(), DataType::Interval),
+            ("tstamp".to_owned(), DataType::Timestamp),
+            ("uid".to_owned(), DataType::Uuid),
+            ("hash".to_owned(), DataType::Map),
+            ("glist".to_owned(), DataType::List)
+        ])),
         r#"Show columns from mytable"#
     );
 
