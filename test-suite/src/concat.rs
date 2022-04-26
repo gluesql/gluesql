6258a3821c740c803f6dc0947a0969f4c2e65f76
Billy Earney <billy.earney@gmail.com>
2022-04-26 13:46:36 -0500

    remove concat function tests from concat.rs

--- test-suite/src/concat.rs
+++ test-suite/src/concat.rs
@@ -2,7 +2,6 @@ use crate::*;
 
 test_case!(concat, async move {
     use gluesql_core::prelude::Value::*;
-    use gluesql_core::translate::TranslateError;
 
     run!(
         "
@@ -90,51 +89,5 @@ test_case!(concat, async move {
             flag || "wow" || null_value AS Case3
         FROM
             Concat;"#
-    );
-
-    test!(
-        Ok(select!(
-           myc
-           Str;
-           "abcd".to_owned()
-        )),
-        r#"select concat("ab", "cd") as myc from Concat;"#
-    );
-
-    test!(
-        Ok(select!(
-           myconcat
-           Str;
-           "abcdef".to_owned()
-        )),
-        r#"select concat("ab", "cd", "ef") as myconcat from Concat;"#
-    );
-
-    test!(
-        Ok(select!(
-           myconcat
-           Str;
-           "abcdef".to_owned()
-        )),
-        r#"select concat("ab", "cd", NULL, "ef") as myconcat from Concat;"#
-    );
-    // test with non string arguments
-    test!(
-        Ok(select!(
-           myconcat
-           Str;
-           "1234563.14".to_owned()
-        )),
-        r#"select concat(123, 456, 3.14) as myconcat from Concat;"#
-    );
-    // test with zero arguments
-    test!(
-        Err(TranslateError::FunctionArgsLengthNotMatchingMin {
-            name: "CONCAT".to_owned(),
-            expected_minimum: 1,
-            found: 0
-        }
-        .into()),
-        r#"select concat() as myconcat from Concat;"#
     );
 });
