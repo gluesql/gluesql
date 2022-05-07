65130facf4dbba1a723e062e3a22365b7a4d221b
Billy <billy.earney@gmail.com>
2022-05-07 13:27:22 -0500

    remove unnecessary enum values

--- core/src/translate/ast_literal.rs
+++ core/src/translate/ast_literal.rs
@@ -23,8 +23,12 @@ pub fn translate_ast_literal(sql_value: &SqlValue) -> Result<AstLiteral> {
             ..
         } => AstLiteral::Interval {
             value: value.to_owned(),
-            leading_field: leading_field.as_ref().map(translate_datetime_field),
-            last_field: last_field.as_ref().map(translate_datetime_field),
+            leading_field: leading_field
+                .as_ref()
+                .map(|x| translate_datetime_field(x).unwrap()),
+            last_field: last_field
+                .as_ref()
+                .map(|x| translate_datetime_field(x).unwrap()),
         },
         SqlValue::Null => AstLiteral::Null,
         _ => {
@@ -33,31 +37,20 @@ pub fn translate_ast_literal(sql_value: &SqlValue) -> Result<AstLiteral> {
     })
 }
 
-pub fn translate_datetime_field(sql_datetime_field: &SqlDateTimeField) -> DateTimeField {
-    match sql_datetime_field {
+pub fn translate_datetime_field(sql_datetime_field: &SqlDateTimeField) -> Result<DateTimeField> {
+    Ok(match sql_datetime_field {
         SqlDateTimeField::Year => DateTimeField::Year,
         SqlDateTimeField::Month => DateTimeField::Month,
         SqlDateTimeField::Day => DateTimeField::Day,
         SqlDateTimeField::Hour => DateTimeField::Hour,
         SqlDateTimeField::Minute => DateTimeField::Minute,
         SqlDateTimeField::Second => DateTimeField::Second,
-        SqlDateTimeField::Century => DateTimeField::Century,
-        SqlDateTimeField::Decade => DateTimeField::Decade,
-        SqlDateTimeField::Week => DateTimeField::Week,
-        SqlDateTimeField::Dow => DateTimeField::Dow, //Day of Week
-        SqlDateTimeField::Doy => DateTimeField::Doy, //Day of Year
-        SqlDateTimeField::Epoch => DateTimeField::Epoch,
-        SqlDateTimeField::Isodow => DateTimeField::Isodow,
-        SqlDateTimeField::Isoyear => DateTimeField::Isoyear,
-        SqlDateTimeField::Julian => DateTimeField::Julian,
-        SqlDateTimeField::Microseconds => DateTimeField::Microseconds,
-        SqlDateTimeField::Millenium => DateTimeField::Millenium,
-        SqlDateTimeField::Milliseconds => DateTimeField::Milliseconds,
-        SqlDateTimeField::Quarter => DateTimeField::Quarter,
-        SqlDateTimeField::Timezone => DateTimeField::Timezone,
-        SqlDateTimeField::TimezoneHour => DateTimeField::TimezoneHour,
-        SqlDateTimeField::TimezoneMinute => DateTimeField::TimezoneMinute,
-    }
+        _ => {
+            return Err(
+                TranslateError::UnsupportedDateTimeField(sql_datetime_field.to_string()).into(),
+            )
+        }
+    })
 }
 
 pub fn translate_trim_where_field(sql_trim_where_field: &SqlTrimWhereField) -> TrimWhereField {
