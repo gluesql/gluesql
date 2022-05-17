74fb3760910e094f65ea58701fa9fdc20519cfbd
Hoon Wee <mrgravity817@gmail.com>
2022-05-17 22:43:10 +0900

    Move subcrate `pm` to `core-macro`

--- core/core_macros/src/lib.rs
+++ core/core_macros/src/lib.rs
@@ -6,9 +6,7 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn basic_derives(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let expanded = quote!(
-        use serde::{Deserialize, Serialize};
-
-        #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+        #[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
         #input
     );
     TokenStream::from(expanded)
