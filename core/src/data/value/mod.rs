7bd800df4c2479c69d747cc9437d354f94eb611d
Billy Earney <billy.earney@gmail.com>
2022-05-25 14:40:13 -0500

    remove value ref function

--- core/src/data/value/mod.rs
+++ core/src/data/value/mod.rs
@@ -40,12 +40,6 @@ pub enum Value {
     Null,
 }
 
-impl AsRef<Value> for Value {
-    fn as_ref(&self) -> &Value {
-        self
-    }
-}
-
 impl PartialEq<Value> for Value {
     fn eq(&self, other: &Value) -> bool {
         match (self, other) {
