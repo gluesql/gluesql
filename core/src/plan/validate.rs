ce06bd69ce16a912cf92f39127b1b46c6b7f3be2
Hoon Wee <mrgravity817@gmail.com>
2022-05-29 20:58:07 +0900

    Add todo list

--- core/src/plan/validate.rs
+++ core/src/plan/validate.rs
@@ -4,5 +4,30 @@ use {
 };
 
 pub fn validate(schema_map: &HashMap<String, Schema>, statement: Statement) -> Result<Statement> {
+    // TODO #1. Check if selected column is unique across joined tables
+    // TODO #2. If yes, correct statement to `{table_name}.{column}`
+    // TODO #3. If not, omit error for the ambiguous column selection
     Ok(statement)
 }
+
+/*
+{
+    "TxAlter":
+        Schema {
+            table_name: "TxAlter",
+            column_defs: [
+                ColumnDef {
+                    name: "id",
+                    data_type: Int,
+                    options: []
+                },
+                ColumnDef {
+                    name: "num",
+                    data_type: Int,
+                    options: []
+                }
+            ],
+            indexes: []
+        }
+}
+*/
