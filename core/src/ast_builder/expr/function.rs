a1e2482c38cfaa04f9b246b5ee43a5efd0e42125
yujong-lee <yujonglee@g.skku.edu>
2022-07-08 23:35:02 +0900

    Add floor func in ast_builder

--- core/src/ast_builder/expr/function.rs
+++ core/src/ast_builder/expr/function.rs
@@ -9,6 +9,7 @@ use {
 #[derive(Clone)]
 pub enum FunctionNode {
     Abs(ExprNode),
+    Floor(ExprNode),
 }
 
 impl TryFrom<FunctionNode> for Expr {
@@ -21,6 +22,11 @@ impl TryFrom<FunctionNode> for Expr {
                 .map(Function::Abs)
                 .map(Box::new)
                 .map(Expr::Function),
+            FunctionNode::Floor(expr_node) => expr_node
+                .try_into()
+                .map(Function::Floor)
+                .map(Box::new)
+                .map(Expr::Function),
         }
     }
 }
@@ -29,15 +35,22 @@ impl ExprNode {
     pub fn abs(self) -> ExprNode {
         abs(self)
     }
+    pub fn floor(self) -> ExprNode {
+        floor(self)
+    }
 }
 
 pub fn abs<T: Into<ExprNode>>(expr: T) -> ExprNode {
     ExprNode::Function(Box::new(FunctionNode::Abs(expr.into())))
 }
 
+pub fn floor<T: Into<ExprNode>>(expr: T) -> ExprNode {
+    ExprNode::Function(Box::new(FunctionNode::Floor(expr.into())))
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::ast_builder::{abs, col, expr, test_expr};
+    use crate::ast_builder::{abs, col, expr, floor, test_expr};
 
     #[test]
     fn function() {
@@ -48,6 +61,15 @@ mod tests {
 
         let actual = expr("base - 10").abs();
         let expected = "ABS(base - 10)";
+        test_expr(actual, expected);
+
+        // FLOOR
+        let actual = floor(col("num"));
+        let expected = "FLOOR(num)";
+        test_expr(actual, expected);
+
+        let actual = expr("base - 10").floor();
+        let expected = "FLOOR(base - 10)";
         test_expr(actual, expected);
     }
 }
