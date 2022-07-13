0379e42fcc66566558b1e969badb7ad44d782983
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-07-13 22:47:25 +0900

    Implement add reverse func in ast_builder (#637)

--- core/src/ast_builder/expr/function.rs
+++ core/src/ast_builder/expr/function.rs
@@ -21,6 +21,7 @@ pub enum FunctionNode {
     Pi,
     Left(ExprNode, ExprNode),
     Right(ExprNode, ExprNode),
+    Reverse(ExprNode),
 }
 
 impl TryFrom<FunctionNode> for Expr {
@@ -95,6 +96,11 @@ impl TryFrom<FunctionNode> for Expr {
                     .map(Box::new)
                     .map(Expr::Function)
             }),
+            FunctionNode::Reverse(expr_node) => expr_node
+                .try_into()
+                .map(Function::Reverse)
+                .map(Box::new)
+                .map(Expr::Function),
         }
     }
 }
@@ -136,6 +142,10 @@ impl ExprNode {
     }
     pub fn right(self, size: Self) -> Self {
         right(self, size)
+    }
+
+    pub fn reverse(self) -> ExprNode {
+        reverse(self)
     }
 }
 
@@ -180,11 +190,15 @@ pub fn right<T: Into<ExprNode>, V: Into<ExprNode>>(expr: T, size: V) -> ExprNode
     ExprNode::Function(Box::new(FunctionNode::Right(expr.into(), size.into())))
 }
 
+pub fn reverse<T: Into<ExprNode>>(expr: T) -> ExprNode {
+    ExprNode::Function(Box::new(FunctionNode::Reverse(expr.into())))
+}
+
 #[cfg(test)]
 mod tests {
     use crate::ast_builder::{
-        abs, acos, asin, atan, col, cos, expr, floor, ifnull, left, num, pi, right, sin, tan,
-        test_expr, text, upper,
+        abs, acos, asin, atan, col, cos, expr, floor, ifnull, left, num, pi, reverse, right, sin,
+        tan, test_expr, text, upper,
     };
 
     #[test]
@@ -311,6 +325,17 @@ mod tests {
 
         let actual = expr("GlueSQL").right(num(2));
         let expected = "RIGHT(GlueSQL, 2)";
+        test_expr(actual, expected);
+    }
+
+    #[test]
+    fn function_reverse() {
+        let actual = reverse(text("GlueSQL"));
+        let expected = "REVERSE('GlueSQL')";
+        test_expr(actual, expected);
+
+        let actual = expr("GlueSQL").reverse();
+        let expected = "REVERSE(GlueSQL)";
         test_expr(actual, expected);
     }
 }
