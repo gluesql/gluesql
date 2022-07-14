6c9b90bd89f9afc758aa29c3f9018f252b46c967
Hoon Wee <mrgravity817@gmail.com>
2022-07-14 21:47:27 +0900

    Separate schema fetching logic

--- storages/csv-storage/src/lib.rs
+++ storages/csv-storage/src/lib.rs
@@ -1,37 +1,73 @@
-use csv::StringRecord;
-use gluesql_core::{
-    data::Schema,
-    result::{Error, Result},
+use {
+    csv::StringRecord,
+    gluesql_core::{
+        data::Schema,
+        result::{Error, Result},
+    },
+    std::{
+        fs::{self, DirEntry, ReadDir},
+        io,
+        path::Path,
+    },
 };
 
 /// A type that contains data info of imported CSV file.
-pub struct CsvStorage(Schema);
+pub struct CsvStorage {
+    pub db: Vec<Schema>,
+}
 
 impl CsvStorage {
     /// Constructs new `CsvStorage` instance from given CSV file
     /// It will create one-table-database since single CSV file only stands
     /// for a single table.
-    pub fn read_file(csv_path: &str) -> Result<Self> {
-        match csv::ReaderBuilder::new()
-            .has_headers(false)
-            .from_path(csv_path)
-        {
-            Err(_) => Err(Error::StorageMsg(
-                "Cannot read CSV file from given path".into(),
-            )),
-            Ok(mut rdr) => {
-                let records: Result<Vec<_>> = rdr.records().map(check_record).collect();
-                let schema = check_schema(&records?);
-                Ok(CsvStorage(schema?))
-            }
-        }
+    pub fn read_file(csv_path: impl AsRef<Path>) -> Result<Self> {
+        let schema = fetch_schema_from_path(csv_path)?;
+        let db = vec![schema];
+
+        Ok(CsvStorage { db })
     }
 
     /// Constructs new `CsvStorage` instance from given directory.
-    /// It will create multi-table-database if there's more than one 
+    /// It will create multi-table-database if there's more than one
     /// interpretable CSV files.
     pub fn read_dir(dir_path: &str) -> Result<Self> {
-        unimplemented!()
+        let schemas: Result<Vec<Schema>> =
+            read_dir(dir_path)?.map(fetch_schema_from_entry).collect();
+        let db = schemas?;
+
+        Ok(CsvStorage { db })
+    }
+}
+
+pub fn fetch_schema_from_entry(entry: io::Result<DirEntry>) -> Result<Schema> {
+    match entry {
+        Ok(dir_entry) => {
+            let csv_path = dir_entry.path();
+            fetch_schema_from_path(csv_path)
+        }
+        Err(_) => Err(Error::StorageMsg("Cannot read entry from given dir".into())),
+    }
+}
+
+pub fn read_dir(dir_path: impl AsRef<Path>) -> Result<ReadDir> {
+    match fs::read_dir(dir_path) {
+        Ok(read_dir) => Ok(read_dir),
+        Err(_) => Err(Error::StorageMsg("Cannot read dir from given path".into())),
+    }
+}
+
+pub fn fetch_schema_from_path(csv_path: impl AsRef<Path>) -> Result<Schema> {
+    match csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(csv_path)
+    {
+        Err(_) => Err(Error::StorageMsg(
+            "Cannot read CSV file from given path".into(),
+        )),
+        Ok(mut rdr) => {
+            let records: Result<Vec<_>> = rdr.records().map(check_record).collect();
+            check_schema(&records?)
+        }
     }
 }
 
