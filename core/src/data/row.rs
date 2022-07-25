c9cdca5c30d0ecc662be75b38db42ecb1ae78aba
devgony <devgony@gmail.com>
2022-07-25 08:27:49 +0000

    chore: remove redundant Error

--- core/src/data/row.rs
+++ core/src/data/row.rs
@@ -26,9 +26,6 @@ pub enum RowError {
 
     #[error("VALUES lists must all be the same length")]
     NumberOfValuesDifferent,
-
-    #[error("VALUES types {0} and {1} cannot be matched")]
-    ValuesTypeDifferent(String, String),
 }
 
 #[derive(iter_enum::Iterator)]
