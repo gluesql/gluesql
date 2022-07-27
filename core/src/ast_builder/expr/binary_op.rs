5a4cdc0994ad88eda00a0f3020d6151ae8b8652b
Jeong Jinwoo <jwjeong96@gmail.com>
2022-07-27 22:31:15 +0900

    feat(binary_op): implement ast builder for Like

--- core/src/ast_builder/expr/binary_op.rs
+++ core/src/ast_builder/expr/binary_op.rs
@@ -68,6 +68,10 @@ impl ExprNode {
     pub fn or<T: Into<Self>>(self, other: T) -> Self {
         self.binary_op(BinaryOperator::Or, other)
     }
+
+    pub fn like<T: Into<Self>>(self, other: T) -> Self {
+        self.binary_op(BinaryOperator::Like, other)
+    }
 }
 
 #[cfg(test)]
@@ -130,6 +134,10 @@ mod tests {
 
         let actual = (col("id").gt(num(10))).or(col("id").lt(num(20)));
         let expected = "id > 10 OR id < 20";
+        test_expr(actual, expected);
+
+        let actual = col("name").like(text("a%"));
+        let expected = "name LIKE 'a%'";
         test_expr(actual, expected);
     }
 }
