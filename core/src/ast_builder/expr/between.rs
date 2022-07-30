989bba5bfb8c8d5754acbe48fa3cdd4a6e58ed40
CEOJINSUNG <jinsung1048@gmail.com>
2022-07-30 10:53:25 +0900

    [Fix] Implement AST builder between expression function

--- core/src/ast_builder/expr/between.rs
+++ core/src/ast_builder/expr/between.rs
@@ -1,10 +1,19 @@
 use super::ExprNode;
 
 impl ExprNode {
-    pub fn between<T: Into<Self>>(self, negated: bool, low: T, high: T) -> Self {
+    pub fn between<T: Into<Self>>(self, low: T, high: T) -> Self {
         Self::Between {
             expr: Box::new(self),
-            negated,
+            negated: false,
+            low: Box::new(low.into()),
+            high: Box::new(high.into()),
+        }
+    }
+
+    pub fn not_between<T: Into<Self>>(self, low: T, high: T) -> Self {
+        Self::Between {
+            expr: Box::new(self),
+            negated: true,
             low: Box::new(low.into()),
             high: Box::new(high.into()),
         }
@@ -17,19 +26,19 @@ mod tests {
 
     #[test]
     fn between() {
-        let actual = col("num").between(false, 1, 10);
+        let actual = col("num").between(1, 10);
         let expected = "num BETWEEN 1 AND 10";
         test_expr(actual, expected);
 
-        let actual = col("date").between(false, text("2022-01-01"), text("2023-01-01"));
+        let actual = col("date").between(text("2022-01-01"), text("2023-01-01"));
         let expected = "date BETWEEN '2022-01-01' AND '2023-01-01'";
         test_expr(actual, expected);
 
-        let actual = col("num").between(true, 1, 10);
+        let actual = col("num").not_between(1, 10);
         let expected = "num NOT BETWEEN 1 AND 10";
         test_expr(actual, expected);
 
-        let actual = col("date").between(true, text("2022-01-01"), text("2023-01-01"));
+        let actual = col("date").not_between(text("2022-01-01"), text("2023-01-01"));
         let expected = "date NOT BETWEEN '2022-01-01' AND '2023-01-01'";
         test_expr(actual, expected);
     }
