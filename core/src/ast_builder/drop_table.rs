4636ca0094ac7def04c538e6428dd1358a615665
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-07-30 17:38:36 +0900

    Implement AST builder drop_table statement (#695)
    Support drop_table, drop_table_if_exists

---
+++ core/src/ast_builder/drop_table.rs
@@ -0,0 +1,42 @@
+use crate::{
+    ast::{ObjectName, Statement},
+    result::Result,
+};
+
+#[derive(Clone)]
+pub struct DropTableNode {
+    table_name: String,
+    if_exists: bool,
+}
+
+impl DropTableNode {
+    pub fn new(table_name: String, exists: bool) -> Self {
+        Self {
+            table_name,
+            if_exists: exists,
+        }
+    }
+
+    pub fn build(self) -> Result<Statement> {
+        let names = vec![ObjectName(vec![self.table_name])];
+        let if_exists = self.if_exists;
+
+        Ok(Statement::DropTable { names, if_exists })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ast_builder::{table, test};
+
+    #[test]
+    fn drop_table() {
+        let actual = table("Foo").drop_table().build();
+        let expected = "DROP TABLE Foo";
+        test(actual, expected);
+
+        let actual = table("Foo").drop_table_if_exists().build();
+        let expected = "DROP TABLE IF EXISTS Foo";
+        test(actual, expected);
+    }
+}
