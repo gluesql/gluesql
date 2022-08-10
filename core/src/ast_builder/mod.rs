466bd21bb814843760007ac7902625328178e455
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-08-10 23:08:08 +0900

    Feat: Add repeat function in ast builder

--- core/src/ast_builder/mod.rs
+++ core/src/ast_builder/mod.rs
@@ -40,7 +40,7 @@ pub use expr::{
     aggregate::{avg, count, max, min, stdev, sum, variance, AggregateNode},
     function::{
         abs, acos, asin, atan, ceil, cos, floor, gcd, generate_uuid, ifnull, lcm, left, ln, log,
-        log10, log2, now, pi, power, reverse, right, round, sign, sin, sqrt, tan, upper,
+        log10, log2, now, pi, power, repeat, reverse, right, round, sign, sin, sqrt, tan, upper,
         FunctionNode,
     },
 };
