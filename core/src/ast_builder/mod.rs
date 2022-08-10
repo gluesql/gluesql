712f7f2c9396bdb447a39fbd36b1ff0bad891f9d
Seongbin Bernie Cho <chobobdev@naver.com>
2022-08-10 22:17:59 +0900

    Implement `Gcd` and `Lcm` function to `ast_builder` (#734)
    * feat:[#732] Implement `Gcd` and `Lcm` function to `ast_builder`

--- core/src/ast_builder/mod.rs
+++ core/src/ast_builder/mod.rs
@@ -39,8 +39,8 @@ pub use {index::CreateIndexNode, index::DropIndexNode};
 pub use expr::{
     aggregate::{avg, count, max, min, stdev, sum, variance, AggregateNode},
     function::{
-        abs, acos, asin, atan, ceil, cos, floor, ifnull, left, ln, log, log10, log2, now, pi,
-        power, reverse, right, round, sign, sin, sqrt, tan, upper, FunctionNode,
+        abs, acos, asin, atan, ceil, cos, floor, gcd, ifnull, lcm, left, ln, log, log10, log2, now,
+        pi, power, reverse, right, round, sign, sin, sqrt, tan, upper, FunctionNode,
     },
 };
 
