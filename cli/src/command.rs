959d9c778571c75b54e48dfaae5dcd7b3a317204
devgony <devgony@gmail.com>
2022-08-12 08:01:54 +0000

    refactor: use more concise match arm for spool off

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -39,7 +39,7 @@ impl Command {
                 ".version" => Ok(Self::Execute("SHOW VERSION".to_owned())),
                 ".execute" if params.len() == 2 => Ok(Self::ExecuteFromFile(params[1].to_owned())),
                 ".spool" => match params.get(1) {
-                    Some(&arg) if arg == "off" => Ok(Self::SpoolOff),
+                    Some(&"off") => Ok(Self::SpoolOff),
                     Some(path) => Ok(Self::SpoolOn(path.to_string())),
                     None => Err(CommandError::LackOfFile),
                 },
