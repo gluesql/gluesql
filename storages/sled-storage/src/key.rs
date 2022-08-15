4c8380c6f54c6218ac0847ad4a08e0fde639aeb2
Taehoon Moon <taehoon.moon@outlook.com>
2022-08-15 13:21:53 +0900

    Update SledStorage key.rs to use IVec::from_iter, not IVec::from

--- storages/sled-storage/src/key.rs
+++ storages/sled-storage/src/key.rs
@@ -12,10 +12,9 @@ pub fn data(table_name: &str, key: Vec<u8>) -> IVec {
     let key = data_prefix(table_name)
         .into_bytes()
         .into_iter()
-        .chain(key.into_iter())
-        .collect::<Vec<_>>();
+        .chain(key.into_iter());
 
-    IVec::from(key)
+    IVec::from_iter(key)
 }
 
 macro_rules! prefix {
@@ -29,45 +28,29 @@ macro_rules! prefix {
 }
 
 pub fn temp_data_prefix(txid: u64) -> IVec {
-    IVec::from(prefix!(txid, TEMP_DATA).collect::<Vec<_>>())
+    IVec::from_iter(prefix!(txid, TEMP_DATA))
 }
 
 pub fn temp_schema_prefix(txid: u64) -> IVec {
-    IVec::from(prefix!(txid, TEMP_SCHEMA).collect::<Vec<_>>())
+    IVec::from_iter(prefix!(txid, TEMP_SCHEMA))
 }
 
 pub fn temp_index_prefix(txid: u64) -> IVec {
-    IVec::from(prefix!(txid, TEMP_INDEX).collect::<Vec<_>>())
+    IVec::from_iter(prefix!(txid, TEMP_INDEX))
 }
 
 pub fn temp_data(txid: u64, data_key: &IVec) -> IVec {
-    let key = prefix!(txid, TEMP_DATA)
-        .chain(data_key.iter().copied())
-        .collect::<Vec<_>>();
-
-    IVec::from(key)
+    IVec::from_iter(prefix!(txid, TEMP_DATA).chain(data_key.iter().copied()))
 }
 
 pub fn temp_data_str(txid: u64, data_key: &str) -> IVec {
-    let key = prefix!(txid, TEMP_DATA)
-        .chain(data_key.as_bytes().iter().copied())
-        .collect::<Vec<_>>();
-
-    IVec::from(key)
+    IVec::from_iter(prefix!(txid, TEMP_DATA).chain(data_key.as_bytes().iter().copied()))
 }
 
 pub fn temp_schema(txid: u64, table_name: &str) -> IVec {
-    let key = prefix!(txid, TEMP_SCHEMA)
-        .chain(table_name.as_bytes().iter().copied())
-        .collect::<Vec<_>>();
-
-    IVec::from(key)
+    IVec::from_iter(prefix!(txid, TEMP_SCHEMA).chain(table_name.as_bytes().iter().copied()))
 }
 
 pub fn temp_index(txid: u64, index_key: &[u8]) -> IVec {
-    let key = prefix!(txid, TEMP_INDEX)
-        .chain(index_key.iter().copied())
-        .collect::<Vec<_>>();
-
-    IVec::from(key)
+    IVec::from_iter(prefix!(txid, TEMP_INDEX).chain(index_key.iter().copied()))
 }
