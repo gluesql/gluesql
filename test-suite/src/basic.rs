cb188b76f3f8badd5ee4b0a28f95e67eeb8ec0d3
devgony <devgony@gmail.com>
2022-08-20 12:50:37 +0000

    test: split test to series

--- test-suite/src/basic.rs
+++ test-suite/src/basic.rs
@@ -1,11 +1,4 @@
-use {
-    crate::*,
-    gluesql_core::{
-        executor::FetchError,
-        prelude::{Payload, Value},
-        translate::TranslateError,
-    },
-};
+use {crate::*, gluesql_core::prelude::Value::*};
 
 test_case!(basic, async move {
     run!(
@@ -34,8 +27,6 @@ CREATE TABLE TestA (
     run!("INSERT INTO TestB (id) SELECT id FROM Test");
 
     test!(Ok(select!(id I64; 1; 1; 3; 4)), "SELECT * FROM TestB");
-
-    use Value::*;
 
     test!(
         Ok(select!(
@@ -70,146 +61,6 @@ CREATE TABLE TestA (
         (
             Ok(select!(id | num; I64 | I64; 2 2; 2 9; 2 4; 2 7)),
             "SELECT id, num FROM Test",
-        ),
-        (
-            // SELECT without Table
-            Ok(select!(
-                1   | 'a'       | true | "1 + 2" | "'a' || 'b'"
-                I64 | Str       | Bool | I64     | Str;
-                1     "a".into()  true   3         "ab".into()
-            )),
-            "SELECT 1, 'a', true, 1 + 2, 'a' || 'b'",
-        ),
-        (
-            // SELECT without Table in Scalar subquery
-            Ok(select!(
-                (SELECT 1)
-                I64;
-                1
-            )),
-            "SELECT (SELECT 1)",
-        ),
-        (
-            // SELECT without Table with Column aliases
-            Ok(select!(
-                id  | max
-                I64 | I64;
-                1     9
-            )),
-            "SELECT 1 AS id, (SELECT MAX(num) FROM TestA) AS max",
-        ),
-        (
-            // SELECT without Table in Drived
-            Ok(select!(
-                1
-                I64;
-                1
-            )),
-            "SELECT * FROM (SELECT 1) AS Drived",
-        ),
-        (
-            // `SELECT *` fetches column `N` for now
-            Ok(select!(
-                N
-                I64;
-                1
-            )),
-            "SELECT *",
-        ),
-        (
-            // SERIES(N) has intenal column `N`
-            Ok(select!(
-                N
-                I64;
-                1;
-                2;
-                3
-            )),
-            "SELECT * FROM SERIES(3)",
-        ),
-        (
-            // SERIES(N) with lowercase works
-            Ok(select!(
-                N
-                I64;
-                1;
-                2;
-                3
-            )),
-            "SELECT * FROM series(3)",
-        ),
-        (
-            // SERIES(N) with table alias
-            Ok(select!(
-                N
-                I64;
-                1;
-                2;
-                3
-            )),
-            "SELECT S.* FROM SERIES(3) as S",
-        ),
-        (
-            // SERIES with unary plus is allowed
-            Ok(select!(
-                N
-                I64;
-                1;
-                2;
-                3
-            )),
-            "SELECT * FROM SERIES(+3)",
-        ),
-        (
-            // CTAS without Table
-            Ok(Payload::Create),
-            "CREATE TABLE TargetTable AS SELECT 1",
-        ),
-        (
-            Ok(select!(
-                N
-                I64;
-                1
-            )),
-            "SELECT * FROM TargetTable",
-        ),
-        (
-            // CTAS with SERIES(N)
-            Ok(Payload::Create),
-            "CREATE TABLE SeriesTable AS SELECT * FROM SERIES(3)",
-        ),
-        (
-            Ok(select!(
-                N
-                I64;
-                1;
-                2;
-                3
-            )),
-            "SELECT * FROM SeriesTable",
-        ),
-        (
-            // SERIES with size 0 is allowed
-            Ok(Payload::Select {
-                labels: vec!["N".into()],
-                rows: Vec::new(),
-            }),
-            "SELECT * FROM SERIES(0)",
-        ),
-        (
-            // SERIES without parentheses is a normal table name
-            Err(FetchError::TableNotFound("SERIES".into()).into()),
-            "SELECT * FROM SERIES",
-        ),
-        (
-            // SERIES without size is not allowed
-            Err(TranslateError::LackOfArgs.into()),
-            "SELECT * FROM SERIES()",
-        ),
-        (
-            // SERIES with unary minus is not allowed
-            Err(FetchError::SeriesSizeWrong(-1).into()),
-            "SELECT * FROM SERIES(-1)",
         ),
     ];
 
