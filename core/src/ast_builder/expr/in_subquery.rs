23ab3a1f4be29d2aa50758ec1301d5b4cfdd3544
CEOJINSUNG <jinsung1048@gmail.com>
2022-08-20 16:59:50 +0900

    Update in_subquery.rs

--- core/src/ast_builder/expr/in_subquery.rs
+++ core/src/ast_builder/expr/in_subquery.rs
@@ -28,7 +28,8 @@ mod test {
         let expected = "id IN (SELECT * FROM FOO WHERE id IS NULL LIMIT 10)";
         test_expr(actual, expected);
 
-        let actual = col("id").not_in_subquery(table("FOO").select().filter("id IS NULL").limit(10));
+        let actual =
+            col("id").not_in_subquery(table("FOO").select().filter("id IS NULL").limit(10));
         let expected = "id NOT IN (SELECT * FROM FOO WHERE id IS NULL LIMIT 10)";
         test_expr(actual, expected);
 
