b2cc6783c8aa5b7458ff37dd3e28fa5b6196eb53
CEOJINSUNG <jinsung1048@gmail.com>
2022-08-20 21:14:06 +0900

    Add Not Exists Test Cases

--- core/src/ast_builder/expr/exists.rs
+++ core/src/ast_builder/expr/exists.rs
@@ -29,12 +29,28 @@ mod test {
             "SELECT * FROM FOO WHERE EXISTS (SELECT * FROM BAR WHERE id IS NOT NULL GROUP BY name)";
         test(actual, expected);
 
+        let actual = table("FOO")
+            .select()
+            .filter(exists(
+                table("BAR")
+                    .select()
+                    .filter("id IS NOT NULL")
+            ).not())
+            .build();
+        let expected =
+            "SELECT * FROM FOO WHERE NOT EXISTS (SELECT * FROM BAR WHERE id IS NOT NULL)";
+        test(actual, expected);
+
         let actual = exists(table("FOO").select().filter(col("id").gt(2)));
         let expected = "EXISTS (SELECT * FROM FOO WHERE id > 2)";
         test_expr(actual, expected);
 
         let actual = exists("SELECT * FROM FOO");
         let expected = "EXISTS (SELECT * FROM FOO)";
+        test_expr(actual, expected);
+
+        let actual = exists("SELECT * FROM FOO").not();
+        let expected = "NOT EXISTS (SELECT * FROM FOO)";
         test_expr(actual, expected);
     }
 }
