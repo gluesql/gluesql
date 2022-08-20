2834f04b15d8664e1ede71d46a91f866911b7831
CEOJINSUNG <jinsung1048@gmail.com>
2022-08-20 20:51:49 +0900

    [Feat] EXISTS Expression

---
+++ core/src/ast_builder/expr/exists.rs
@@ -0,0 +1,43 @@
+use {super::ExprNode, crate::ast_builder::QueryNode};
+
+impl ExprNode {
+    pub fn exists<T: Into<QueryNode>>(self, query: T) -> Self {
+        Self::Exists(Box::new(query.into()))
+    }
+}
+
+pub fn exists<T: Into<QueryNode>>(query: T) -> ExprNode {
+    ExprNode::Exists(Box::new(query.into()))
+}
+
+#[cfg(test)]
+mod test {
+    use {
+        super::exists,
+        crate::ast_builder::{col, table, test, test_expr},
+    };
+
+    #[test]
+    fn exist() {
+        let actual = table("FOO")
+            .select()
+            .filter(exists(
+                table("BAR")
+                    .select()
+                    .filter("id IS NOT NULL")
+                    .group_by("name"),
+            ))
+            .build();
+        let expected =
+            "SELECT * FROM FOO WHERE EXISTS (SELECT * FROM BAR WHERE id IS NOT NULL GROUP BY name)";
+        test(actual, expected);
+
+        let actual = exists(table("FOO").select().filter(col("id").gt(2)));
+        let expected = "EXISTS (SELECT * FROM FOO WHERE id > 2)";
+        test_expr(actual, expected);
+
+        let actual = exists("SELECT * FROM FOO");
+        let expected = "EXISTS (SELECT * FROM FOO)";
+        test_expr(actual, expected);
+    }
+}
--- core/src/ast_builder/expr/mod.rs
+++ core/src/ast_builder/expr/mod.rs
@@ -6,6 +6,7 @@ mod unary_op;
 pub mod aggregate;
 pub mod between;
 pub mod cast;
+pub mod exists;
 pub mod extract;
 pub mod function;
 pub mod in_list;
@@ -77,6 +78,7 @@ pub enum ExprNode {
         expr: Box<ExprNode>,
         data_type: DataTypeNode,
     },
+    Exists(Box<QueryNode>),
 }
 
 impl TryFrom<ExprNode> for Expr {
@@ -170,6 +172,10 @@ impl TryFrom<ExprNode> for Expr {
             ExprNode::Aggregate(aggr_expr) => Aggregate::try_from(*aggr_expr)
                 .map(Box::new)
                 .map(Expr::Aggregate),
+            ExprNode::Exists(query) => {
+                let query = Query::try_from(*query).map(Box::new)?;
+                Ok(Expr::Exists(query))
+            }
         }
     }
 }
