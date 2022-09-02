758c911050ee01d0218be0e51ee46d669f2c0124
Seoyoung Lee <lsyhime@snu.ac.kr>
2022-09-03 01:46:17 +0900

    impl alter_table_op, column_def to_sql

--- core/src/ast/ddl.rs
+++ core/src/ast/ddl.rs
@@ -1,3 +1,4 @@
+use crate::ast::ToSql;
 use {
     super::{DataType, Expr, ObjectName},
     serde::{Deserialize, Serialize},
@@ -45,3 +46,62 @@ pub enum ColumnOption {
     /// `{ PRIMARY KEY | UNIQUE }`
     Unique { is_primary: bool },
 }
+
+impl ToSql for AlterTableOperation {
+    fn to_sql(&self) -> String {
+        match self {
+            AlterTableOperation::AddColumn { column_def } => format!("ADD {}", column_def.to_sql()),
+            AlterTableOperation::DropColumn {
+                column_name,
+                if_exists,
+            } => match if_exists {
+                true => format!("DROP IF EXISTS {column_name}"),
+                false => format!("DROP {column_name}"),
+            },
+            AlterTableOperation::RenameColumn {
+                old_column_name,
+                new_column_name,
+            } => format!("RENAME {old_column_name} TO {new_column_name}"),
+            AlterTableOperation::RenameTable { table_name } => format!("RENAME TO {table_name}"),
+        }
+    }
+}
+
+impl ToSql for ColumnDef {
+    fn to_sql(&self) -> String {
+        match self {
+            ColumnDef {
+                name,
+                data_type,
+                options,
+            } => {
+                let options = options
+                    .iter()
+                    .map(|op| op.option.to_sql()) // TODO name..
+                    .collect::<Vec<_>>()
+                    .join(" ");
+                format!("{name} {data_type} {options}")
+            }
+        }
+    }
+}
+
+impl ToSql for ColumnOption {
+    fn to_sql(&self) -> String {
+        match self {
+            ColumnOption::Null => "NULL".to_string(),
+            ColumnOption::NotNull => "NOT NULL".to_string(),
+            ColumnOption::Default(expr) => format!("DEFAULT {}", expr.to_sql()),
+            ColumnOption::Unique { is_primary } => {
+                // TODO Q. simple bool match vs if else ????
+                if is_primary {
+                    "PRIMARY KEY".to_string()
+                } else {
+                    "UNIQUE".to_string()
+                }
+            }
+        }
+    }
+}
+
+// TODO move & add test code
