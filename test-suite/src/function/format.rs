52608f450984d40d9f66b44e66d59ffdd9a2327b
chobobdev <chobobdev@naver.com>
2022-09-05 22:49:15 +0900

    test : edit test cases for `FORMAT` function

--- test-suite/src/function/format.rs
+++ test-suite/src/function/format.rs
@@ -1,10 +1,7 @@
 use crate::*;
 
 test_case!(format, async move {
-    use gluesql_core::{
-        executor::{EvaluateError, SelectError},
-        prelude::Value::*,
-    };
+    use gluesql_core::{executor::EvaluateError, prelude::Value::*};
 
     let test_cases = vec![
         (
@@ -16,12 +13,16 @@ test_case!(format, async move {
             )),
         ),
         (
-            r#"SELECT FORMAT(DATE "2017-06-15", "%Y-%m") AS DATE"#,
+            r#"SELECT FORMAT(TIMESTAMP "2015-09-05 23:56:04", "%Y-%m-%d %H") AS TIMESTAMP"#,
             Ok(select!(
-                "DATE"
+                "TIMESTAMP"
                 Str;
-                "2017-06".to_owned()
+                "2015-09-05 23".to_owned()
             )),
+        ),
+        (
+            r#"SELECT FORMAT("2015-09-05 23:56:04", "%Y-%m-%d %H") AS TIMESTAMP"#,
+            Err(EvaluateError::FunctionRequiresFormattableValue("FORMAT".to_string()).into()),
         ),
     ];
     for (sql, expected) in test_cases {
