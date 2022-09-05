441c36885478078fdb2d9ba0ef1ecee0073dad28
chobobdev <chobobdev@naver.com>
2022-09-05 22:01:55 +0900

    test : add new test case

--- test-suite/src/function/format.rs
+++ test-suite/src/function/format.rs
@@ -6,14 +6,24 @@ test_case!(format, async move {
         prelude::Value::*,
     };
 
-    let test_cases = vec![(
-        r#"SELECT FORMAT(DATE "2017-06-15", "%Y-%m") AS DATE"#,
-        Ok(select!(
-            "DATE"
-            Str;
-            "2017-06".to_owned()
-        )),
-    )];
+    let test_cases = vec![
+        (
+            r#"SELECT FORMAT(DATE "2017-06-15", "%Y") AS DATE"#,
+            Ok(select!(
+                "DATE"
+                Str;
+                "2017".to_owned()
+            )),
+        ),
+        (
+            r#"SELECT FORMAT(DATE "2017-06-15", "%Y-%m") AS DATE"#,
+            Ok(select!(
+                "DATE"
+                Str;
+                "2017-06".to_owned()
+            )),
+        ),
+    ];
     for (sql, expected) in test_cases {
         test!(expected, sql);
     }
