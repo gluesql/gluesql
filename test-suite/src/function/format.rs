ddcf275f6369db282467205349e9af04a34c6783
chobobdev <chobobdev@naver.com>
2022-09-08 12:52:40 +0900

    test: test case for invalic specifier error

--- test-suite/src/function/format.rs
+++ test-suite/src/function/format.rs
@@ -25,8 +25,8 @@ test_case!(format, async move {
             Err(EvaluateError::FunctionRequiresFormattableValue("FORMAT".to_string()).into()),
         ),
         (
-            r#"SELECT FORMAT(TIMESTAMP "2015-09-05 23:56:04", "Y-M") AS TIMESTAMP"#,
-            Err(EvaluateError::InvalidSpecifierGiven("Y-M".to_string()).into()),
+            r#"SELECT FORMAT(DATE "2017-06-15", "%9f") AS DATE"#,
+            Err(EvaluateError::InvalidSpecifierGiven("%9f".to_string()).into()),
         ),
     ];
     for (sql, expected) in test_cases {
