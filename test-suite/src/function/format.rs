331e0d44b95701993fbb4f38a35e008a5c47cc66
chobobdev <chobobdev@naver.com>
2022-09-10 16:43:54 +0900

    test: add test case for seperate in column cases

--- test-suite/src/function/format.rs
+++ test-suite/src/function/format.rs
@@ -21,8 +21,23 @@ test_case!(format, async move {
             )),
         ),
         (
+            r#"SELECT 
+         FORMAT(TIMESTAMP "2015-09-05 23:56:04", "%Y") AS YEAR
+        ,FORMAT(TIMESTAMP "2015-09-05 23:56:04", "%m") AS MONTH
+        ,FORMAT(TIMESTAMP "2015-09-05 23:56:04", "%d") AS DAY
+        "#,
+            Ok(select!(
+            "YEAR" | "MONTH" | "DAY";
+            Str | Str |Str;
+            "2015".to_owned() "09".to_owned() "05".to_owned()
+            )),
+        ),
+        (
             r#"SELECT FORMAT("2015-09-05 23:56:04", "%Y-%m-%d %H") AS TIMESTAMP"#,
-            Err(EvaluateError::UnsupportedDataType("test".to_string()).into()),
+            Err(
+                EvaluateError::UnsupportedExprForFormatFunction("2015-09-05 23:56:04".to_string())
+                    .into(),
+            ),
         ),
     ];
     for (sql, expected) in test_cases {
