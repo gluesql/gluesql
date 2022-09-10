77be3294d38b4914dbcd14c8ef84bee39c6518c7
chobobdev <chobobdev@naver.com>
2022-09-10 15:25:57 +0900

    test: update test cases for renamed error

--- test-suite/src/function/format.rs
+++ test-suite/src/function/format.rs
@@ -22,7 +22,7 @@ test_case!(format, async move {
         ),
         (
             r#"SELECT FORMAT("2015-09-05 23:56:04", "%Y-%m-%d %H") AS TIMESTAMP"#,
-            Err(EvaluateError::FunctionRequiresFormattableValue("FORMAT".to_string()).into()),
+            Err(EvaluateError::UnsupportedDataType("test".to_string()).into()),
         ),
     ];
     for (sql, expected) in test_cases {
