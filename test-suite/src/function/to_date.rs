c5444dfd3115ba8bab7a5cd73c44219d6e0e55d1
chobobdev <chobobdev@naver.com>
2022-09-11 17:13:07 +0900

    test: add new test cases

--- test-suite/src/function/to_date.rs
+++ test-suite/src/function/to_date.rs
@@ -61,6 +61,14 @@ test_case!(to_date, async move {
             r#"SELECT TO_TIMESTAMP("2015-05 23", "%Y-%d %H") AS timestamp"#,
             Err(EvaluateError::ChronoFormat(ChronoFormatError::NotEnough).into()),
         ),
+        (
+            r#"SELECT TO_DATE(DATE "2017-06-15","%Y-%m-%d") AS date"#,
+            Err(EvaluateError::FunctionRequiresStringValue("TO_DATE".to_owned()).into()),
+        ),
+        (
+            r#"SELECT TO_TIMESTAMP(TIMESTAMP "2015-09-05 23:56:04","%Y-%m-%d") AS date"#,
+            Err(EvaluateError::FunctionRequiresStringValue("TO_TIMESTAMP".to_owned()).into()),
+        ),
     ];
     for (sql, expected) in test_cases {
         test!(sql, expected);
