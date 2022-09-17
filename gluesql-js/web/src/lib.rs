28f561a9ee352f33dcc64d91508c9989206dec5a
Taehoon Moon <taehoon.moon@outlook.com>
2022-09-17 13:58:17 +0900

    Remove deprecated wee_alloc dependency use in gluesql-js/

--- gluesql-js/web/src/lib.rs
+++ gluesql-js/web/src/lib.rs
@@ -13,12 +13,6 @@ use {
     wasm_bindgen_futures::future_to_promise,
 };
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
