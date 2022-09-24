adf18edec3e87210b9673662904329fafb3d2463
Taehoon Moon <taehoon.moon@outlook.com>
2022-09-24 21:46:18 +0900

    Update test-suite Tester trait to use Glue struct (#872)

--- storages/shared-memory-storage/tests/shared_memory_storage.rs
+++ storages/shared-memory-storage/tests/shared_memory_storage.rs
@@ -1,23 +1,21 @@
 use {
-    gluesql_shared_memory_storage::SharedMemoryStorage,
-    std::{cell::RefCell, rc::Rc},
-    test_suite::*,
+    gluesql_core::prelude::Glue, gluesql_shared_memory_storage::SharedMemoryStorage, test_suite::*,
 };
 
 struct SharedMemoryTester {
-    storage: Rc<RefCell<Option<SharedMemoryStorage>>>,
+    glue: Glue<SharedMemoryStorage>,
 }
 
 impl Tester<SharedMemoryStorage> for SharedMemoryTester {
     fn new(_: &str) -> Self {
-        let storage = Some(SharedMemoryStorage::new());
-        let storage = Rc::new(RefCell::new(storage));
+        let storage = SharedMemoryStorage::new();
+        let glue = Glue::new(storage);
 
-        SharedMemoryTester { storage }
+        SharedMemoryTester { glue }
     }
 
-    fn get_cell(&mut self) -> Rc<RefCell<Option<SharedMemoryStorage>>> {
-        Rc::clone(&self.storage)
+    fn get_glue(&mut self) -> &mut Glue<SharedMemoryStorage> {
+        &mut self.glue
     }
 }
 
