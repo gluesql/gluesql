fc847bd5d8d4b761ef3d807a92bfa0ad567f15d7
Jiseok CHOI <jiseok.dev@gmail.com>
2022-10-10 16:17:47 +0900

    Implement `generate_binary_op_tests` macro

--- core/src/data/value/binary_op/integer/macros.rs
+++ core/src/data/value/binary_op/integer/macros.rs
@@ -136,3 +136,401 @@ macro_rules! impl_try_binary_op {
         }
     };
 }
+
+#[macro_export]
+macro_rules! generate_binary_op_tests {
+    ($variant: ident, $primitive: ident) => {
+        mod tests {
+            use {
+                rust_decimal::prelude::Decimal,
+                std::cmp::Ordering,
+                $crate::data::{
+                    value::{
+                        TryBinaryOperator,
+                        Value::{self, *},
+                    },
+                    NumericBinaryOperator::{self, *},
+                    ValueError,
+                },
+            };
+
+            fn overflow_err(
+                lhs: Value,
+                rhs: Value,
+                op: NumericBinaryOperator,
+            ) -> Result<Value, $crate::result::Error> {
+                Err(ValueError::BinaryOperationOverflow {
+                    lhs,
+                    rhs,
+                    operator: op,
+                }
+                .into())
+            }
+
+            #[test]
+            fn eq_primitive() {
+                assert_eq!($primitive::from(0), $variant(0));
+                assert_eq!($primitive::from(1), $variant(1));
+                assert_eq!($primitive::MAX, $variant($primitive::MAX));
+                assert_eq!($primitive::MIN, $variant($primitive::MIN));
+            }
+
+            #[test]
+            fn eq() {
+                let base: $primitive = 1.into();
+
+                assert_eq!(base, I8(1));
+                assert_eq!(base, I16(1));
+                assert_eq!(base, I32(1));
+                assert_eq!(base, I64(1));
+                assert_eq!(base, I128(1));
+                assert_eq!(base, U8(1));
+                assert_eq!(base, F64(1.0));
+                assert_eq!(base, Decimal(Decimal::ONE));
+
+                assert_ne!(base, Bool(true));
+            }
+
+            #[test]
+            fn partial_cmp() {
+                let base: $primitive = 1.into();
+
+                assert_eq!(base.partial_cmp(&I8(0)), Some(Ordering::Greater));
+                assert_eq!(base.partial_cmp(&I16(0)), Some(Ordering::Greater));
+                assert_eq!(base.partial_cmp(&I32(0)), Some(Ordering::Greater));
+                assert_eq!(base.partial_cmp(&I64(0)), Some(Ordering::Greater));
+                assert_eq!(base.partial_cmp(&I128(0)), Some(Ordering::Greater));
+                assert_eq!(base.partial_cmp(&U8(0)), Some(Ordering::Greater));
+                assert_eq!(base.partial_cmp(&F64(0.0)), Some(Ordering::Greater));
+
+                assert_eq!(base.partial_cmp(&I8(1)), Some(Ordering::Equal));
+                assert_eq!(base.partial_cmp(&I16(1)), Some(Ordering::Equal));
+                assert_eq!(base.partial_cmp(&I32(1)), Some(Ordering::Equal));
+                assert_eq!(base.partial_cmp(&I64(1)), Some(Ordering::Equal));
+                assert_eq!(base.partial_cmp(&I128(1)), Some(Ordering::Equal));
+                assert_eq!(base.partial_cmp(&U8(1)), Some(Ordering::Equal));
+                assert_eq!(base.partial_cmp(&F64(1.0)), Some(Ordering::Equal));
+
+                assert_eq!(base.partial_cmp(&I8(2)), Some(Ordering::Less));
+                assert_eq!(base.partial_cmp(&I16(2)), Some(Ordering::Less));
+                assert_eq!(base.partial_cmp(&I32(2)), Some(Ordering::Less));
+                assert_eq!(base.partial_cmp(&I64(2)), Some(Ordering::Less));
+                assert_eq!(base.partial_cmp(&I128(2)), Some(Ordering::Less));
+                assert_eq!(base.partial_cmp(&U8(2)), Some(Ordering::Less));
+                assert_eq!(base.partial_cmp(&F64(2.0)), Some(Ordering::Less));
+
+                assert_eq!(
+                    base.partial_cmp(&Decimal(Decimal::ONE)),
+                    Some(Ordering::Equal)
+                );
+
+                assert_eq!(base.partial_cmp(&Bool(true)), None);
+            }
+
+            #[test]
+            fn add_overflow() {
+                assert_eq!(
+                    $primitive::MAX.try_add(&Decimal(Decimal::from($primitive::from(1)))),
+                    overflow_err(
+                        $variant($primitive::MAX),
+                        Decimal(Decimal::from($primitive::from(1))),
+                        Add
+                    )
+                );
+
+                assert_eq!(
+                    $primitive::MAX.try_add(&I8(1)),
+                    overflow_err($variant($primitive::MAX), I8(1), Add)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_add(&I16(1)),
+                    overflow_err($variant($primitive::MAX), I64(1), Add)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_add(&I32(1)),
+                    overflow_err($variant($primitive::MAX), I32(1), Add)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_add(&I64(1)),
+                    overflow_err($variant($primitive::MAX), I64(1), Add)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_add(&I128(1)),
+                    overflow_err($variant($primitive::MAX), I128(1), Add)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_add(&U8(1)),
+                    overflow_err($variant($primitive::MAX), U8(1), Add)
+                );
+            }
+
+            #[test]
+            fn sub_overflow() {
+                assert_eq!(
+                    $primitive::MIN.try_subtract(&Decimal(Decimal::from($primitive::from(1)))),
+                    overflow_err(
+                        $variant($primitive::MIN),
+                        Decimal(Decimal::from($primitive::from(1))),
+                        Subtract
+                    )
+                );
+
+                assert_eq!(
+                    $primitive::MIN.try_subtract(&I8(1)),
+                    overflow_err($variant($primitive::MIN), I8(1), Subtract)
+                );
+                assert_eq!(
+                    $primitive::MIN.try_subtract(&I16(1)),
+                    overflow_err($variant($primitive::MIN), I16(1), Subtract)
+                );
+                assert_eq!(
+                    $primitive::MIN.try_subtract(&I32(1)),
+                    overflow_err($variant($primitive::MIN), I32(1), Subtract)
+                );
+                assert_eq!(
+                    $primitive::MIN.try_subtract(&I64(1)),
+                    overflow_err($variant($primitive::MIN), I64(1), Subtract)
+                );
+                assert_eq!(
+                    $primitive::MIN.try_subtract(&I128(1)),
+                    overflow_err($variant($primitive::MIN), I128(1), Subtract)
+                );
+                assert_eq!(
+                    $primitive::MIN.try_subtract(&U8(1)),
+                    overflow_err($variant($primitive::MIN), U8(1), Subtract)
+                );
+            }
+
+            #[test]
+            fn mul_overflow() {
+                assert_eq!(
+                    $primitive::MAX.try_multiply(&Decimal(Decimal::from($primitive::from(2)))),
+                    overflow_err(
+                        $variant($primitive::MAX),
+                        Decimal(Decimal::from($primitive::from(2))),
+                        Multiply
+                    )
+                );
+
+                assert_eq!(
+                    $primitive::MAX.try_multiply(&I8(2)),
+                    overflow_err($variant($primitive::MAX), I8(2), Multiply)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_multiply(&I16(2)),
+                    overflow_err($variant($primitive::MAX), I16(2), Multiply)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_multiply(&I32(2)),
+                    overflow_err($variant($primitive::MAX), I32(2), Multiply)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_multiply(&I64(2)),
+                    overflow_err($variant($primitive::MAX), I64(2), Multiply)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_multiply(&I128(2)),
+                    overflow_err(I8($primitive::MAX), I128(2), Multiply)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_multiply(&U8(2)),
+                    overflow_err($variant($primitive::MAX), U8(2), Multiply)
+                );
+            }
+
+            #[test]
+            fn div_overflow() {
+                assert_eq!(
+                    $primitive::MAX.try_divide(&Decimal(Decimal::from($primitive::from(0)))),
+                    overflow_err(
+                        $variant($primitive::MAX),
+                        Decimal(Decimal::from($primitive::from(0))),
+                        Divide
+                    )
+                );
+
+                assert_eq!(
+                    $primitive::MAX.try_divide(&I8(0)),
+                    overflow_err($variant($primitive::MAX), I8(0), Divide)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_divide(&I16(0)),
+                    overflow_err($variant($primitive::MAX), I16(0), Divide)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_divide(&I32(0)),
+                    overflow_err($variant($primitive::MAX), I32(0), Divide)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_divide(&I64(0)),
+                    overflow_err($variant($primitive::MAX), I64(0), Divide)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_divide(&I128(0)),
+                    overflow_err($variant($primitive::MAX), I128(0), Divide)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_divide(&U8(0)),
+                    overflow_err($variant($primitive::MAX), U8(0), Divide)
+                );
+            }
+
+            #[test]
+            fn mod_overlfow() {
+                assert_eq!(
+                    $primitive::MAX.try_modulo(&Decimal(Decimal::from($primitive::from(0)))),
+                    overflow_err(
+                        $variant($primitive::MAX),
+                        Decimal(Decimal::from($primitive::from(0))),
+                        Modulo
+                    )
+                );
+
+                assert_eq!(
+                    $primitive::MAX.try_modulo(&I8(0)),
+                    overflow_err($variant($primitive::MAX), I8(0), Modulo)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_modulo(&I16(0)),
+                    overflow_err($variant($primitive::MAX), I16(0), Modulo)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_modulo(&I32(0)),
+                    overflow_err($variant($primitive::MAX), I32(0), Modulo)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_modulo(&I64(0)),
+                    overflow_err($variant($primitive::MAX), I64(0), Modulo)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_modulo(&I128(0)),
+                    overflow_err($variant($primitive::MAX), I128(0), Modulo)
+                );
+                assert_eq!(
+                    $primitive::MAX.try_modulo(&U8(0)),
+                    overflow_err($variant($primitive::MAX), U8(0), Modulo)
+                );
+            }
+
+            #[test]
+            fn try_add() {
+                let base: $primitive = 1.into();
+
+                assert_eq!(base.try_add(&Decimal(Decimal::ONE)), Ok($variant(2)));
+                assert_eq!(base.try_add(&F64(1.0)), Ok($variant(2)));
+                assert_eq!(base.try_add(&I8(1)), Ok($variant(2)));
+                assert_eq!(base.try_add(&I16(1)), Ok($variant(2)));
+                assert_eq!(base.try_add(&I32(1)), Ok($variant(2)));
+                assert_eq!(base.try_add(&I64(1)), Ok($variant(2)));
+                assert_eq!(base.try_add(&I128(1)), Ok($variant(2)));
+                assert_eq!(base.try_add(&U8(1)), Ok($variant(2)));
+
+                assert_eq!(
+                    base.try_add(&Bool(true)),
+                    Err(ValueError::NonNumericMathOperation {
+                        lhs: $variant(base),
+                        operator: NumericBinaryOperator::Add,
+                        rhs: Bool(true)
+                    }
+                    .into())
+                );
+            }
+
+            #[test]
+            fn try_subtract() {
+                let base: $primitive = 1.into();
+
+                assert_eq!(base.try_subtract(&Decimal(Decimal::ONE)), Ok($variant(0)));
+                assert_eq!(base.try_subtract(&F64(1.0)), Ok($variant(0)));
+                assert_eq!(base.try_subtract(&I8(1)), Ok($variant(0)));
+                assert_eq!(base.try_subtract(&I16(1)), Ok($variant(0)));
+                assert_eq!(base.try_subtract(&I32(1)), Ok($variant(0)));
+                assert_eq!(base.try_subtract(&I64(1)), Ok($variant(0)));
+                assert_eq!(base.try_subtract(&I128(1)), Ok($variant(0)));
+                assert_eq!(base.try_subtract(&U8(1)), Ok($variant(0)));
+
+                assert_eq!(
+                    base.try_subtract(&Bool(true)),
+                    Err(ValueError::NonNumericMathOperation {
+                        lhs: $variant(base),
+                        operator: NumericBinaryOperator::Subtract,
+                        rhs: Bool(true)
+                    }
+                    .into())
+                );
+            }
+
+            #[test]
+            fn try_multiply() {
+                let base: $primitive = 3.into();
+
+                assert_eq!(base.try_multiply(&Decimal(Decimal::TWO)), Ok($variant(6)));
+                assert_eq!(base.try_multiply(&F64(2.0)), Ok($variant(6)));
+                assert_eq!(base.try_multiply(&I8(2)), Ok($variant(6)));
+                assert_eq!(base.try_multiply(&I16(2)), Ok($variant(6)));
+                assert_eq!(base.try_multiply(&I32(2)), Ok($variant(6)));
+                assert_eq!(base.try_multiply(&I64(2)), Ok($variant(6)));
+                assert_eq!(base.try_multiply(&I128(2)), Ok($variant(6)));
+                assert_eq!(base.try_multiply(&U8(2)), Ok($variant(6)));
+
+                assert_eq!(
+                    base.try_multiply(&Bool(true)),
+                    Err(ValueError::NonNumericMathOperation {
+                        lhs: $variant(base),
+                        operator: NumericBinaryOperator::Multiply,
+                        rhs: Bool(true)
+                    }
+                    .into())
+                );
+            }
+
+            #[test]
+            fn try_divide() {
+                let base: $primitive = 6.into();
+
+                assert_eq!(base.try_divide(&Decimal(Decimal::TWO)), Ok($variant(3)));
+                assert_eq!(base.try_divide(&F64(2.0)), Ok($variant(3)));
+                assert_eq!(base.try_divide(&I8(2)), Ok($variant(3)));
+                assert_eq!(base.try_divide(&I16(2)), Ok($variant(3)));
+                assert_eq!(base.try_divide(&I32(2)), Ok($variant(3)));
+                assert_eq!(base.try_divide(&I64(2)), Ok($variant(3)));
+                assert_eq!(base.try_divide(&I128(2)), Ok($variant(3)));
+                assert_eq!(base.try_divide(&U8(2)), Ok($variant(3)));
+
+                assert_eq!(
+                    base.try_divide(&Bool(true)),
+                    Err(ValueError::NonNumericMathOperation {
+                        lhs: $variant(base),
+                        operator: NumericBinaryOperator::Divide,
+                        rhs: Bool(true)
+                    }
+                    .into())
+                );
+            }
+
+            #[test]
+            fn try_modulo() {
+                let base: $primitive = 9.into();
+
+                assert_eq!(base.try_modulo(&Decimal(Decimal::ONE)), Ok($variant(0)));
+                assert_eq!(base.try_modulo(&F64(1.0)), Ok($variant(0)));
+                assert_eq!(base.try_modulo(&I8(1)), Ok($variant(0)));
+                assert_eq!(base.try_modulo(&I16(1)), Ok($variant(0)));
+                assert_eq!(base.try_modulo(&I32(1)), Ok($variant(0)));
+                assert_eq!(base.try_modulo(&I64(1)), Ok($variant(0)));
+                assert_eq!(base.try_modulo(&I128(1)), Ok($variant(0)));
+                assert_eq!(base.try_modulo(&U8(1)), Ok($variant(0)));
+
+                assert_eq!(
+                    base.try_modulo(&Bool(true)),
+                    Err(ValueError::NonNumericMathOperation {
+                        lhs: $variant(base),
+                        operator: NumericBinaryOperator::Modulo,
+                        rhs: Bool(true)
+                    }
+                    .into())
+                );
+            }
+        }
+    };
+}
