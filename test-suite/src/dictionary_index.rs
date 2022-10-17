19a68f1c568322585bf98f263ee5afb82a223873
devgony <devgony@gmail.com>
2022-10-17 16:04:46 +0000

    test: add a edge case without primary key

--- test-suite/src/dictionary_index.rs
+++ test-suite/src/dictionary_index.rs
@@ -1,21 +1,30 @@
 use {crate::*, gluesql_core::prelude::Value::*};
 
 test_case!(ditionary_index, async move {
-    run!("CREATE TABLE Foo (id INT PRIMARY KEY, name TEXT);");
-    run!("CREATE INDEX Foo_id_1 ON Foo (id + 1)");
-    run!("CREATE INDEX Foo_name_concat ON Foo (name + '_')");
+    run!("CREATE TABLE Foo (id INT, name TEXT);");
+    run!("CREATE INDEX Foo_id ON Foo (id)");
+    run!("CREATE INDEX Foo_id_2 ON Foo (id + 2)");
+    test!(
+        "SELECT * FROM GLUE_INDEXES",
+        Ok(select!(
+            TABLE_NAME       | INDEX_NAME            | ORDER             | EXPRESSION         | UNIQUENESS;
+            Str              | Str                   | Str               | Str                | Bool;
+            "Foo".to_owned()   "Foo_id".to_owned()     "BOTH".to_owned()   "id".to_owned()      false;
+            "Foo".to_owned()   "Foo_id_2".to_owned()   "BOTH".to_owned()   "id + 2".to_owned()  false
+        ))
+    );
 
-    let test_cases = [(
+    run!("CREATE TABLE Bar (id INT PRIMARY KEY, name TEXT);");
+    run!("CREATE INDEX Bar_name_concat ON Bar (name + '_')");
+    test!(
         "SELECT * FROM GLUE_INDEXES",
         Ok(select!(
             TABLE_NAME       | INDEX_NAME                  | ORDER             | EXPRESSION               | UNIQUENESS;
             Str              | Str                         | Str               | Str                      | Bool;
-            "Foo".to_owned()   "PRIMARY".to_owned()          "BOTH".to_owned()   "id".to_owned()            true;
-            "Foo".to_owned()   "Foo_id_1".to_owned()         "BOTH".to_owned()   "id + 1".to_owned()        false;
-            "Foo".to_owned()   "Foo_name_concat".to_owned()  "BOTH".to_owned()   "name + \"_\"".to_owned()  false
-        )),
-    )];
-    for (sql, expected) in test_cases {
-        test!(sql, expected);
-    }
+            "Bar".to_owned()   "PRIMARY".to_owned()          "BOTH".to_owned()   "id".to_owned()            true;
+            "Bar".to_owned()   "Bar_name_concat".to_owned()  "BOTH".to_owned()   "name + \"_\"".to_owned()  false;
+            "Foo".to_owned()   "Foo_id".to_owned()           "BOTH".to_owned()   "id".to_owned()            false;
+            "Foo".to_owned()   "Foo_id_2".to_owned()         "BOTH".to_owned()   "id + 2".to_owned()        false
+        ))
+    );
 });
