44834a52a1c8d851512671d4a3cf4e451eadaafc
devgony <devgony@gmail.com>
2022-10-17 14:58:21 +0000

    test: add case for primary key

--- test-suite/src/dictionary_index.rs
+++ test-suite/src/dictionary_index.rs
@@ -1,19 +1,18 @@
 use {crate::*, gluesql_core::prelude::Value::*};
 
 test_case!(ditionary_index, async move {
-    run!("CREATE TABLE Foo (id INT);");
-    run!("CREATE INDEX Foo_id ON Foo (id ASC)");
-    run!("CREATE INDEX Foo_id_1 ON Foo (id + 1 DESC)");
-    run!("CREATE INDEX Foo_id_2 ON Foo (id * 2)");
+    run!("CREATE TABLE Foo (id INT PRIMARY KEY, name TEXT);");
+    run!("CREATE INDEX Foo_id_1 ON Foo (id + 1)");
+    run!("CREATE INDEX Foo_name_upper ON Foo (name + '_')");
 
     let test_cases = [(
         "SELECT * FROM GLUE_INDEXES",
         Ok(select!(
-            TABLE_NAME       | INDEX_NAME           | ORDER             | EXPRESSION;
-            Str              | Str                  | Str               | Str;
-            "Foo".to_owned()   "Foo_id".to_owned()    "BOTH".to_owned()    "id".to_owned();
-            "Foo".to_owned()   "Foo_id_1".to_owned()  "BOTH".to_owned()   "id + 1".to_owned();
-            "Foo".to_owned()   "Foo_id_2".to_owned()  "BOTH".to_owned()   "id * 2".to_owned()
+            TABLE_NAME       | INDEX_NAME                  | ORDER             | EXPRESSION               | UNIQUENESS;
+            Str              | Str                         | Str               | Str                      | Bool;
+            "Foo".to_owned()   "PRIMARY".to_owned()          "BOTH".to_owned()   "id".to_owned()            true;
+            "Foo".to_owned()   "Foo_id_1".to_owned()         "BOTH".to_owned()   "id + 1".to_owned()        false;
+            "Foo".to_owned()   "Foo_name_upper".to_owned()   "BOTH".to_owned()   "name + \"_\"".to_owned()   false
         )),
     )];
     for (sql, expected) in test_cases {
