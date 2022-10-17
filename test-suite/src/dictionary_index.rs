e6f08c9128564b70199338908e8f65e6c838f193
devgony <devgony@gmail.com>
2022-10-17 15:46:45 +0000

    chore: fix test naming

--- test-suite/src/dictionary_index.rs
+++ test-suite/src/dictionary_index.rs
@@ -3,7 +3,7 @@ use {crate::*, gluesql_core::prelude::Value::*};
 test_case!(ditionary_index, async move {
     run!("CREATE TABLE Foo (id INT PRIMARY KEY, name TEXT);");
     run!("CREATE INDEX Foo_id_1 ON Foo (id + 1)");
-    run!("CREATE INDEX Foo_name_upper ON Foo (name + '_')");
+    run!("CREATE INDEX Foo_name_concat ON Foo (name + '_')");
 
     let test_cases = [(
         "SELECT * FROM GLUE_INDEXES",
@@ -12,7 +12,7 @@ test_case!(ditionary_index, async move {
             Str              | Str                         | Str               | Str                      | Bool;
             "Foo".to_owned()   "PRIMARY".to_owned()          "BOTH".to_owned()   "id".to_owned()            true;
             "Foo".to_owned()   "Foo_id_1".to_owned()         "BOTH".to_owned()   "id + 1".to_owned()        false;
-            "Foo".to_owned()   "Foo_name_upper".to_owned()   "BOTH".to_owned()   "name + \"_\"".to_owned()  false
+            "Foo".to_owned()   "Foo_name_concat".to_owned()  "BOTH".to_owned()   "name + \"_\"".to_owned()  false
         )),
     )];
     for (sql, expected) in test_cases {
