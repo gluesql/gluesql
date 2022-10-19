8e259f112402c9ad7f6448f714362a38cc0ee395
devgony <devgony@gmail.com>
2022-10-19 12:06:01 +0000

    test: add cases for DROP and CREATE PRIMARY

--- test-suite/src/dictionary_index.rs
+++ test-suite/src/dictionary_index.rs
@@ -1,4 +1,7 @@
-use {crate::*, gluesql_core::prelude::Value::*};
+use {
+    crate::*,
+    gluesql_core::{executor::AlterError, prelude::Value::*, store::IndexError},
+};
 
 test_case!(ditionary_index, async move {
     run!("CREATE TABLE Foo (id INT, name TEXT);");
@@ -27,4 +30,18 @@ test_case!(ditionary_index, async move {
             "Foo".to_owned()   "Foo_id_2".to_owned()         "BOTH".to_owned()   "id + 2".to_owned()        false
         ))
     );
+
+    let test_cases = [
+        (
+            "DROP INDEX Bar.PRIMARY",
+            Err(IndexError::CannotDropPrimary.into()),
+        ),
+        (
+            "CREATE INDEX Primary ON Foo (id)",
+            Err(AlterError::ReservedIndexName("Primary".to_owned()).into()),
+        ),
+    ];
+    for (sql, expected) in test_cases {
+        test!(sql, expected);
+    }
 });
