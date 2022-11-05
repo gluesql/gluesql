1f4e26b5abe404dc96c59bedca3abd51869b75b9
devgony <devgony@gmail.com>
2022-11-05 08:40:36 +0000

    test: add more cases

--- cli/tests/dump.rs
+++ cli/tests/dump.rs
@@ -18,16 +18,54 @@ async fn dump_and_import() {
     let mut source_glue = Glue::new(source_storage);
 
     let sqls = vec![
-        "CREATE TABLE User (id INT, name TEXT);",
-        "CREATE INDEX User_id ON User (id);",
-        "INSERT INTO User SELECT N, 'a' FROM SERIES(101);",
+        "CREATE TABLE Foo (
+            boolean BOOLEAN,
+            int8 INT8,
+            int16 INT16,
+            int32 INT32,
+            int INT,
+            int128 INT128,
+            uinti8 UINT8,
+            float FLOAT,
+            text TEXT,
+            bytea BYTEA,
+            date DATE,
+            timestamp TIMESTAMP,
+            time TIME,
+            interval INTERVAL,
+            uuid UUID,
+            map MAP,
+            list LIST,
+            decimal DECIMAL
+         );",
+        r#"INSERT INTO Foo
+         VALUES ( 
+         true,
+         1,
+         2,
+         3,
+         4,
+         5,
+         6,
+         7,
+         'a',
+         X'123456',
+         DATE '2022-11-01',
+         TIMESTAMP '2022-11-02',
+         TIME '23:59:59',
+         INTERVAL '1' DAY,
+         '550e8400-e29b-41d4-a716-446655440000',
+         '{"a": {"red": "apple", "blue": 1}, "b": 10}',
+         '[{ "foo": 100, "bar": [true, 0, [10.5, false] ] }, 10, 20]',
+         8);"#,
+        "CREATE INDEX Foo_int ON Foo (int);",
     ];
 
     for sql in sqls {
         source_glue.execute(sql).unwrap();
     }
 
-    let sql = "SELECT * FROM User;";
+    let sql = "SELECT * FROM Foo;";
     let source_data = source_glue.execute(sql).unwrap();
 
     let source_storage = dump_database(source_glue.storage.unwrap(), dump_path.clone()).unwrap();
