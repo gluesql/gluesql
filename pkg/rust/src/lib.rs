42ecbfb32e37eda0f4bcdfb5ba70d5474ad7418d
ShaddyDC <shaddythefirst@gmail.com>
2022-11-15 15:18:48 +0100

    Add indexeddb to interface

--- pkg/rust/src/lib.rs
+++ pkg/rust/src/lib.rs
@@ -84,6 +84,11 @@ pub mod sled_storage {
     pub use sled_storage::*;
 }
 
+#[cfg(feature = "indexeddb-storage")]
+pub mod indexeddb_storage {
+    pub use indexeddb_storage::*;
+}
+
 #[cfg(feature = "test-suite")]
 pub mod test_suite {
     pub use test_suite::*;
@@ -91,6 +96,8 @@ pub mod test_suite {
 
 pub mod prelude {
     pub use gluesql_core::prelude::*;
+    #[cfg(feature = "indexeddb-storage")]
+    pub use indexeddb_storage::IndexeddbStorage;
     #[cfg(feature = "memory-storage")]
     pub use memory_storage::MemoryStorage;
     #[cfg(feature = "shared-memory-storage")]
