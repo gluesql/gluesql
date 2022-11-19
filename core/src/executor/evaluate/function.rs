fb8b5064499db268d5128679454653127c040726
Jiseok CHOI <jiseok.dev@gmail.com>
2022-11-19 14:38:12 +0900

    Change to use internal chrono errors

--- core/src/executor/evaluate/function.rs
+++ core/src/executor/evaluate/function.rs
@@ -1,5 +1,5 @@
 use {
-    super::{ChronoFormatError, EvaluateError, Evaluated},
+    super::{EvaluateError, Evaluated},
     crate::{
         ast::{DataType, DateTimeField, TrimWhereField},
         data::Value,
@@ -541,10 +541,14 @@ pub fn to_date<'a>(
     match expr.try_into()? {
         Value::Str(expr) => {
             let format = eval_to_str!(name, format);
+
             chrono::NaiveDate::parse_from_str(&expr, &format)
-                .map_err(ChronoFormatError::err_into)
                 .map(Value::Date)
                 .map(Evaluated::from)
+                .map_err(|err| {
+                    let err: EvaluateError = err.into();
+                    err.into()
+                })
         }
         _ => Err(EvaluateError::FunctionRequiresStringValue(name).into()),
     }
@@ -558,10 +562,14 @@ pub fn to_timestamp<'a>(
     match expr.try_into()? {
         Value::Str(expr) => {
             let format = eval_to_str!(name, format);
+
             chrono::NaiveDateTime::parse_from_str(&expr, &format)
-                .map_err(ChronoFormatError::err_into)
                 .map(Value::Timestamp)
                 .map(Evaluated::from)
+                .map_err(|err| {
+                    let err: EvaluateError = err.into();
+                    err.into()
+                })
         }
         _ => Err(EvaluateError::FunctionRequiresStringValue(name).into()),
     }
@@ -575,10 +583,14 @@ pub fn to_time<'a>(
     match expr.try_into()? {
         Value::Str(expr) => {
             let format = eval_to_str!(name, format);
+
             chrono::NaiveTime::parse_from_str(&expr, &format)
                 .map(Value::Time)
-                .map_err(ChronoFormatError::err_into)
                 .map(Evaluated::from)
+                .map_err(|err| {
+                    let err: EvaluateError = err.into();
+                    err.into()
+                })
         }
         _ => Err(EvaluateError::FunctionRequiresStringValue(name).into()),
     }
