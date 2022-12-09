a1c085a9c8ade420e75c5340efc4db932803cc9e
devgony <devgony@gmail.com>
2022-12-09 11:09:16 +0000

    feat: impl values.build()

--- core/src/ast_builder/values.rs
+++ core/src/ast_builder/values.rs
@@ -1,4 +1,7 @@
-use crate::ast::{Query, Statement};
+use crate::{
+    ast::{Expr, Query, SetExpr, Statement, Values},
+    result::Result,
+};
 
 use super::{
     select::{NodeData, Prebuild},
@@ -7,26 +10,31 @@ use super::{
 
 #[derive(Clone, Debug)]
 pub struct ValuesNode<'a> {
-    pub values: QueryNode<'a>,
+    // pub values: QueryNode<'a>,
+    pub values: Vec<ExprList<'a>>,
 }
 
 impl<'a> ValuesNode<'a> {
-    pub fn build(self) {
-        Statement::Query(Query {
-            body: crate::ast::SetExpr::Values(self.values.try_into()?),
+    pub fn build(self) -> Result<Statement> {
+        let values = self
+            .values
+            .into_iter()
+            .map(|a| a.try_into())
+            .collect::<Result<Vec<Vec<Expr>>>>()?;
+
+        Ok(Statement::Query(Query {
+            body: SetExpr::Values(Values(values)),
             order_by: Vec::new(),
             limit: None,
             offset: None,
-        });
+        }))
     }
 }
 
 pub fn values<'a, T: Into<ExprList<'a>>>(values: Vec<T>) -> ValuesNode<'a> {
     let values: Vec<ExprList> = values.into_iter().map(Into::into).collect();
 
-    ValuesNode {
-        values: QueryNode::Values(values),
-    }
+    ValuesNode { values }
 }
 
 // impl<'a> Prebuild for ValuesNode<'a> {
@@ -36,3 +44,17 @@ pub fn values<'a, T: Into<ExprList<'a>>>(values: Vec<T>) -> ValuesNode<'a> {
 //         self.values
 //     }
 // }
+
+#[cfg(test)]
+mod tests {
+    use crate::ast_builder::{num, table, test, Build};
+
+    use super::values;
+
+    #[test]
+    fn values_test() {
+        let actual = values(vec![vec![num(7)]]).build();
+        let expected = "VALUES(7)";
+        test(actual, expected);
+    }
+}
