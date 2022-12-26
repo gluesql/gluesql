78b5a9f27b9bf1784e81b918f1725b35b749523b
Taehoon Moon <taehoon.moon@outlook.com>
2022-12-26 20:01:30 +0900

    Add WebStorage - support localStorage & sessionStorage for web browsers

---
+++ storages/web-storage/tests/session_storage.rs
@@ -0,0 +1,32 @@
+use {
+    async_trait::async_trait,
+    gluesql_core::prelude::Glue,
+    gluesql_web_storage::{WebStorage, WebStorageType},
+    test_suite::*,
+};
+
+use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+struct SessionStorageTester {
+    glue: Glue<WebStorage>,
+}
+
+#[async_trait(?Send)]
+impl Tester<WebStorage> for SessionStorageTester {
+    async fn new(_: &str) -> Self {
+        let storage = WebStorage::new(WebStorageType::Session);
+        storage.raw().clear().unwrap();
+
+        let glue = Glue::new(storage);
+
+        SessionStorageTester { glue }
+    }
+
+    fn get_glue(&mut self) -> &mut Glue<WebStorage> {
+        &mut self.glue
+    }
+}
+
+generate_store_tests!(wasm_bindgen_test, SessionStorageTester);
