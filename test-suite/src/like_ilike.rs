5be8713df2fbd976eac8270f470314e0b82c9637
MeenSeek Kim <meenseek5929@naver.com>
2023-01-23 15:34:12 +0900

    test: add like function

--- test-suite/src/like_ilike.rs
+++ test-suite/src/like_ilike.rs
@@ -46,7 +46,18 @@ test_case!(like_ilike, async move {
         (2, "SELECT name FROM Item WHERE name LIKE '%r%'"),
         (2, "SELECT name FROM Item WHERE name LIKE '%a'"),
         (2, "SELECT name FROM Item WHERE SUBSTR(name, 1) LIKE '%a'"),
-        (2, "SELECT name FROM Item WHERE LOWER(name) LIKE '%a'"),
+        (
+            2,
+            "SELECT name FROM Item WHERE SUBSTR(name, 1) LIKE '%' || SUBSTR('a', 1)",
+        ),
+        (
+            2,
+            "SELECT name FROM Item WHERE LOWER(name) LIKE '%' || SUBSTR('a', 1)",
+        ),
+        (
+            2,
+            "SELECT name FROM Item WHERE SUBSTR(name, 1) LIKE '%' || LOWER('A')",
+        ),
         (5, "SELECT name FROM Item WHERE name LIKE '%%'"),
         (0, "SELECT name FROM Item WHERE name LIKE 'g%'"),
         (2, "SELECT name FROM Item WHERE name ILIKE '_A%'"),
