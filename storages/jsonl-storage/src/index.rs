fafe14cb3e3ffcb0e954062b968d65ec4c4dbb7c
devgony <devgony@gmail.com>
2023-02-06 08:27:17 +0900

    refactor: remove unimplemented

--- storages/jsonl-storage/src/index.rs
+++ storages/jsonl-storage/src/index.rs
@@ -1,47 +1,8 @@
 #![cfg(feature = "index")]
-
 use {
     super::JsonlStorage,
-    async_trait::async_trait,
-    gluesql_core::{
-        ast::{IndexOperator, OrderByExpr},
-        data::Value,
-        result::{Error, Result},
-        store::{Index, IndexMut, RowIter},
-    },
+    gluesql_core::store::{Index, IndexMut},
 };
 
-#[async_trait(?Send)]
-impl Index for JsonlStorage {
-    async fn scan_indexed_data(
-        &self,
-        _table_name: &str,
-        _index_name: &str,
-        _asc: Option<bool>,
-        _cmp_value: Option<(&IndexOperator, Value)>,
-    ) -> Result<RowIter> {
-        Err(Error::StorageMsg(
-            "[JsonlStorage] index is not supported".to_owned(),
-        ))
-    }
-}
-
-#[async_trait(?Send)]
-impl IndexMut for JsonlStorage {
-    async fn create_index(
-        &mut self,
-        _table_name: &str,
-        _index_name: &str,
-        _column: &OrderByExpr,
-    ) -> Result<()> {
-        Err(Error::StorageMsg(
-            "[JsonlStorage] index is not supported".to_owned(),
-        ))
-    }
-
-    async fn drop_index(&mut self, _table_name: &str, _index_name: &str) -> Result<()> {
-        Err(Error::StorageMsg(
-            "[JsonlStorage] index is not supported".to_owned(),
-        ))
-    }
-}
+impl Index for JsonlStorage {}
+impl IndexMut for JsonlStorage {}
--- storages/jsonl-storage/src/transaction.rs
+++ storages/jsonl-storage/src/transaction.rs
@@ -1,35 +1,4 @@
 #![cfg(feature = "transaction")]
+use {super::JsonlStorage, gluesql_core::store::Transaction};
 
-use {
-    super::JsonlStorage,
-    async_trait::async_trait,
-    gluesql_core::{
-        result::{Error, Result},
-        store::Transaction,
-    },
-};
-
-#[async_trait(?Send)]
-impl Transaction for JsonlStorage {
-    async fn begin(&mut self, autocommit: bool) -> Result<bool> {
-        if autocommit {
-            return Ok(false);
-        }
-
-        Err(Error::StorageMsg(
-            "[JsonlStorage] transaction is not supported".to_owned(),
-        ))
-    }
-
-    async fn rollback(&mut self) -> Result<()> {
-        Err(Error::StorageMsg(
-            "[JsonlStorage] transaction is not supported".to_owned(),
-        ))
-    }
-
-    async fn commit(&mut self) -> Result<()> {
-        Err(Error::StorageMsg(
-            "[JsonlStorage] transaction is not supported".to_owned(),
-        ))
-    }
-}
+impl Transaction for JsonlStorage {}
