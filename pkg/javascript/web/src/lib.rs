7f5d7a70a87c68113c4dec8b67cbad61bb602269
Taehoon Moon <taehoon.moon@outlook.com>
2023-02-09 00:46:02 +0900

    Apply CompositeStorage to JS package

--- pkg/javascript/web/src/lib.rs
+++ pkg/javascript/web/src/lib.rs
@@ -4,13 +4,16 @@ mod payload;
 mod utils;
 
 use {
+    composite_storage::CompositeStorage,
     gluesql_core::prelude::{execute, parse, plan, translate},
+    idb_storage::IdbStorage,
     js_sys::Promise,
     memory_storage::MemoryStorage,
     payload::convert,
     std::{cell::RefCell, rc::Rc},
     wasm_bindgen::prelude::*,
     wasm_bindgen_futures::future_to_promise,
+    web_storage::{WebStorage, WebStorageType},
 };
 
 #[wasm_bindgen]
@@ -21,7 +24,7 @@ extern "C" {
 
 #[wasm_bindgen]
 pub struct Glue {
-    storage: Rc<RefCell<Option<MemoryStorage>>>,
+    storage: Rc<RefCell<Option<CompositeStorage>>>,
 }
 
 impl Default for Glue {
@@ -37,12 +40,79 @@ impl Glue {
     pub fn new() -> Self {
         utils::set_panic_hook();
 
-        let storage = Some(MemoryStorage::default());
-        let storage = Rc::new(RefCell::new(storage));
+        let mut storage = CompositeStorage::default();
+        storage.push("memory", MemoryStorage::default());
+        storage.push("localStorage", WebStorage::new(WebStorageType::Local));
+        storage.push("sessionStorage", WebStorage::new(WebStorageType::Session));
+        storage.set_default("memory");
+
+        log("[GlueSQL] loaded: memory, localStorage, sessionStorage");
+        log("[GlueSQL] default engine: memory");
+
+        let storage = Rc::new(RefCell::new(Some(storage)));
 
         log("[GlueSQL] hello :)");
 
         Self { storage }
+    }
+
+    #[wasm_bindgen(js_name = loadIndexedDB)]
+    pub fn load_indexeddb(&mut self) -> Promise {
+        let cell = Rc::clone(&self.storage);
+
+        future_to_promise(async move {
+            let mut storage = cell.replace(None).unwrap();
+
+            if storage.storages.contains_key("indexedDB") {
+                cell.replace(Some(storage));
+
+                return Err(JsValue::from_str("indexedDB storage is already loaded"));
+            }
+
+            let idb_storage = match IdbStorage::new(None).await {
+                Ok(storage) => storage,
+                Err(error) => {
+                    cell.replace(Some(storage));
+
+                    return Err(JsValue::from_str(&format!("{error}")));
+                }
+            };
+
+            storage.push("indexedDB", idb_storage);
+            log("[GlueSQL] loaded: indexedDB");
+
+            cell.replace(Some(storage));
+
+            Ok(JsValue::NULL)
+        })
+    }
+
+    #[wasm_bindgen(js_name = setDefaultEngine)]
+    pub fn set_default_engine(&mut self, default_engine: String) -> Result<(), JsValue> {
+        let cell = Rc::clone(&self.storage);
+        let mut storage = cell.replace(None).unwrap();
+
+        let result = {
+            if !["memory", "localStorage", "sessionStorage", "indexedDB"]
+                .iter()
+                .any(|engine| engine == &default_engine.as_str())
+            {
+                Err(JsValue::from_str(
+                    format!("{default_engine} is not supported (options: memory, localStorage, sessionStorage, indexedDB)").as_str()
+                ))
+            } else if default_engine == "indexedDB" && !storage.storages.contains_key("indexedDB") {
+                Err(JsValue::from_str(
+                    "indexedDB is not loaded - run loadIndexedDB() first",
+                ))
+            } else {
+                storage.set_default(default_engine);
+
+                Ok(())
+            }
+        };
+
+        cell.replace(Some(storage));
+        result
     }
 
     pub fn query(&mut self, sql: String) -> Promise {
@@ -52,7 +122,7 @@ impl Glue {
             let queries = parse(&sql).map_err(|error| JsValue::from_str(&format!("{error}")))?;
 
             let mut payloads = vec![];
-            let mut storage: MemoryStorage = cell.replace(None).unwrap();
+            let mut storage: CompositeStorage = cell.replace(None).unwrap();
 
             for query in queries.iter() {
                 let statement = translate(query);
