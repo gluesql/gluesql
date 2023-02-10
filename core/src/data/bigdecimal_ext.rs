5a9db7f4a8dea9bb83fdfd183a81dcb09d4896ee
chobobdev <chobobdev@naver.com>
2023-02-10 16:52:52 +0900

    fix: fix merge error

--- core/src/data/bigdecimal_ext.rs
+++ core/src/data/bigdecimal_ext.rs
@@ -10,9 +10,7 @@ pub trait BigDecimalExt {
     fn to_u16(&self) -> Option<u16>;
     fn to_u32(&self) -> Option<u32>;
     fn to_u128(&self) -> Option<u128>;
-    fn to_u32(&self) -> Option<u32>;
     fn to_u64(&self) -> Option<u64>;
-    fn to_u128(&self) -> Option<u128>;
     fn to_f64(&self) -> Option<f64>;
 }
 
@@ -58,16 +56,6 @@ impl BigDecimalExt for BigDecimal {
             true => bigdecimal::ToPrimitive::to_u64(self),
             false => None,
         }
-    }
-    fn to_u128(&self) -> Option<u128> {
-        match self.is_integer() {
-            true => bigdecimal::ToPrimitive::to_u128(self),
-            false => None,
-        }
-    }
-    fn to_u32(&self) -> Option<u32> {
-        self.is_integer()
-            .then(|| bigdecimal::ToPrimitive::to_u32(self))?
     }
     fn to_u128(&self) -> Option<u128> {
         self.is_integer()
