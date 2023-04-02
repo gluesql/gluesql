a9797e397cf4bf03bf49802ba6c14e9e3589e601
chobobdev <chobobdev@naver.com>
2023-04-02 13:21:18 +0900

    feat: change the value error type

--- core/src/data/value/convert.rs
+++ core/src/data/value/convert.rs
@@ -543,7 +543,7 @@ impl TryFrom<&Value> for u128 {
     fn try_from(v: &Value) -> Result<u128> {
         match v {
             Value::Uuid(value) => Ok(*value),
-            Value::Str(value) =>u128::from_str_radix(value,10).map_err(|_| ValueError::ImpossibleCast.into()),
+            Value::Str(value) =>u128::from_str_radix(value,10).map_err(|_| ValueError::FailedToParseNumber.into()),
             Value::Inet(IpAddr::V6(v)) => Ok(u128::from(*v)),
             _ => Err(ValueError::ImpossibleCast.into()),
         }
