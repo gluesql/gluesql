da3be0db90970efba32773d9a5fd7c907abe2283
chobobdev <chobobdev@naver.com>
2023-04-06 17:11:09 +0900

    test : fix misleading test case

--- core/src/data/key.rs
+++ core/src/data/key.rs
@@ -493,7 +493,7 @@ mod tests {
         assert!(Key::U16(10) > Key::U16(3));
         assert!(Key::U16(1) > Key::Decimal(dec("1")));
 
-        assert!(Key::U32(10) > Key::U16(3));
+        assert!(Key::U32(10) > Key::U32(3));
         assert!(Key::U32(1) > Key::Decimal(dec("1")));
 
         assert!(Key::U64(10) > Key::U64(3));
