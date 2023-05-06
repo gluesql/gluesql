532b8511ffee71588390aeed831e492686642814
Taehoon Moon <taehoon.moon@outlook.com>
2023-05-06 15:42:10 +0900

    Refactor geometry function tests in test-suite, (#1176)
    Remove unnecessary table uses (SingleItem) and columns (id).
    Rearrange tests to show error cases at the last.

--- test-suite/src/function/geometry/get_y.rs
+++ test-suite/src/function/geometry/get_y.rs
@@ -9,8 +9,20 @@ use {
 test_case!(get_y, async move {
     let test_cases = [
         (
-            "CREATE TABLE SingleItem (id FLOAT DEFAULT GET_Y(POINT(0.3134, 0.156)))",
+            "CREATE TABLE PointGroup (point_field POINT)",
             Ok(Payload::Create),
+        ),
+        (
+            r#"INSERT INTO PointGroup VALUES (POINT(0.3134, 0.156))"#,
+            Ok(Payload::Insert(1)),
+        ),
+        (
+            r#"SELECT GET_Y(point_field) AS point_field FROM PointGroup;"#,
+            Ok(select!(
+                point_field
+                F64;
+                0.156
+            )),
         ),
         (
             r#"SELECT GET_Y(CAST('POINT(0.1 -0.2)' AS POINT)) AS ptx"#,
@@ -31,22 +43,6 @@ test_case!(get_y, async move {
         (
             r#"SELECT GET_Y('cheese') AS ptx"#,
             Err(EvaluateError::FunctionRequiresPointValue("GET_Y".to_owned()).into()),
-        ),
-        (
-            "CREATE TABLE POINT (point_field POINT)",
-            Ok(Payload::Create),
-        ),
-        (
-            r#"INSERT INTO POINT VALUES (POINT(0.3134, 0.156))"#,
-            Ok(Payload::Insert(1)),
-        ),
-        (
-            r#"SELECT GET_Y(point_field) AS point_field FROM POINT;"#,
-            Ok(select!(
-                point_field
-                F64;
-                0.156
-            )),
         ),
     ];
 
