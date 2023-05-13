dbd57c7f8a463b6509d1337acadb635f9d75cea1
LEE026 <58213749+LEE026@users.noreply.github.com>
2023-05-13 20:36:18 +0900

    Add rounding test cases for ast_builder - round, ceil, floor (#1186)

---
+++ test-suite/src/ast_builder/function/math/rounding.rs
@@ -0,0 +1,81 @@
+use {
+    crate::*,
+    gluesql_core::{ast_builder::*, executor::Payload, prelude::Value::*},
+};
+
+test_case!(rounding, async move {
+    let glue = get_glue!();
+
+    // create table - Number
+    let actual = table("Number")
+        .create_table()
+        .add_column("id INTEGER PRIMARY KEY")
+        .add_column("number FLOAT")
+        .execute(glue)
+        .await;
+    let expected = Ok(Payload::Create);
+    test(actual, expected);
+
+    // insert into Number
+    let actual = table("Number")
+        .insert()
+        .values(vec!["1, 0.3", "2, -0.8", "3, 10", "4, 6.87421"])
+        .execute(glue)
+        .await;
+    let expected = Ok(Payload::Insert(4));
+    test(actual, expected);
+
+    // ceil
+    let actual = table("Number")
+        .select()
+        .project("id")
+        .project(ceil("number"))
+        .project(col("number").ceil())
+        .execute(glue)
+        .await;
+    let expected = Ok(select!(
+        id  | "CEIL(\"number\")" | "CEIL(\"number\")"
+        I64 | F64                | F64;
+        1     1.0                  1.0;
+        2     0.0                  0.0;
+        3     10.0                 10.0;
+        4     7.0                  7.0
+    ));
+    test(actual, expected);
+
+    //floor
+    let actual = table("Number")
+        .select()
+        .project("id")
+        .project(floor("number"))
+        .project(col("number").floor())
+        .execute(glue)
+        .await;
+    let expected = Ok(select!(
+        id  | "FLOOR(\"number\")" | "FLOOR(\"number\")"
+        I64 | F64                 | F64;
+        1     0.0                   0.0;
+        2     f64::from(-1)         f64::from(-1);
+        3     10.0                  10.0;
+        4     6.0                   6.0
+    ));
+    test(actual, expected);
+
+    //round
+    let actual = table("Number")
+        .select()
+        .project("id")
+        .project(round("number"))
+        .project(col("number").round())
+        .execute(glue)
+        .await;
+    let expected = Ok(select!(
+        id  | "ROUND(\"number\")" | "ROUND(\"number\")"
+        I64 | F64                 | F64;
+        1     0.0                   0.0;
+        2     f64::from(-1)         f64::from(-1);
+        3     10.0                  10.0;
+        4     7.0                   7.0
+    ));
+    test(actual, expected);
+});
