8a4d430362d8641ee74d52ce29b45db0ba4e8c5b
Bangseungjae <tmdwo5825@naver.com>
2023-05-14 12:53:29 +0900

    fix: cargo fmt

--- test-suite/src/ast_builder/function/text/position_and_indexing.rs
+++ test-suite/src/ast_builder/function/text/position_and_indexing.rs
@@ -1,8 +1,12 @@
+use gluesql_core::ast_builder::left;
 use {
     crate::*,
-    gluesql_core::{ast_builder::{text, table, Execute, find_idx, num, position, right}, executor::Payload, prelude::Value::*},
+    gluesql_core::{
+        ast_builder::{find_idx, num, position, right, table, text, Execute},
+        executor::Payload,
+        prelude::Value::*,
+    },
 };
-use gluesql_core::ast_builder::left;
 
 test_case!(position_and_indexing, async move {
     // test - find_idx
@@ -24,18 +28,17 @@ test_case!(position_and_indexing, async move {
 
     // insert table - Item
     let actual = table("Item")
-    .insert()
-    .columns("id, index")
-    .values(vec![vec![num(1), test_num]])
-    .execute(glue)
-    .await;
+        .insert()
+        .columns("id, index")
+        .values(vec![vec![num(1), test_num]])
+        .execute(glue)
+        .await;
 
     let expected = Ok(Payload::Insert(1));
     test(actual, expected);
 
     // select - table - Item
     let actual = table("Item").select().execute(glue).await;
-
 
     let expected = Ok(select!(
         id  | index
@@ -45,22 +48,25 @@ test_case!(position_and_indexing, async move {
 
     test(actual, expected);
 
-    let test_num = find_idx(text("Oracle Database 12c Release"), text("as"), Some(num(15)));
+    let test_num = find_idx(
+        text("Oracle Database 12c Release"),
+        text("as"),
+        Some(num(15)),
+    );
 
     // insert table - Item
     let actual = table("Item")
-    .insert()
-    .columns("id, index")
-    .values(vec![vec![num(2), test_num]])
-    .execute(glue)
-    .await;
+        .insert()
+        .columns("id, index")
+        .values(vec![vec![num(2), test_num]])
+        .execute(glue)
+        .await;
 
     let expected = Ok(Payload::Insert(1));
     test(actual, expected);
 
     // select - table - Item
     let actual = table("Item").select().execute(glue).await;
-
 
     let expected = Ok(select!(
         id  | index
@@ -73,21 +79,19 @@ test_case!(position_and_indexing, async move {
 
     let test_num = text("Oracle Database 12c Release").find_idx(text("as"), Some(num(15)));
 
-
     // insert table - Item
     let actual = table("Item")
-    .insert()
-    .columns("id, index")
-    .values(vec![vec![num(3), test_num]])
-    .execute(glue)
-    .await;
+        .insert()
+        .columns("id, index")
+        .values(vec![vec![num(3), test_num]])
+        .execute(glue)
+        .await;
 
     let expected = Ok(Payload::Insert(1));
     test(actual, expected);
 
     // select - table - Item
     let actual = table("Item").select().execute(glue).await;
-
 
     let expected = Ok(select!(
         id  | index
@@ -102,14 +106,13 @@ test_case!(position_and_indexing, async move {
     // test - position
     let test_num = position(text("cake"), text("ke"));
 
-
     // insert table - Item
     let actual = table("Item")
-    .insert()
-    .columns("id, index")
-    .values(vec![vec![num(4), test_num]])
-    .execute(glue)
-    .await;
+        .insert()
+        .columns("id, index")
+        .values(vec![vec![num(4), test_num]])
+        .execute(glue)
+        .await;
 
     let expected = Ok(Payload::Insert(1));
     test(actual, expected);
@@ -131,14 +134,13 @@ test_case!(position_and_indexing, async move {
     // test - left
     let test_str = left(text("Hello, World"), num(7));
 
-
     // insert table - Item
     let actual = table("Item")
-    .insert()
-    .columns("id, index")
-    .values(vec![vec![num(5), test_str]])
-    .execute(glue)
-    .await;
+        .insert()
+        .columns("id, index")
+        .values(vec![vec![num(5), test_str]])
+        .execute(glue)
+        .await;
 
     let expected = Ok(Payload::Insert(1));
     test(actual, expected);
@@ -161,14 +163,13 @@ test_case!(position_and_indexing, async move {
     // test - right
     let test_str = right(text("Hello, World"), num(7));
 
-
     // insert table - Item
     let actual = table("Item")
-    .insert()
-    .columns("id, index")
-    .values(vec![vec![num(6), test_str]])
-    .execute(glue)
-    .await;
+        .insert()
+        .columns("id, index")
+        .values(vec![vec![num(6), test_str]])
+        .execute(glue)
+        .await;
 
     let expected = Ok(Payload::Insert(1));
     test(actual, expected);
