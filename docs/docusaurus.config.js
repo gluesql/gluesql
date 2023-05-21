b2405a237584360ba9f1a646e54eac0c3cf9f6de
Taehoon Moon <taehoon.moon@outlook.com>
2023-05-21 17:53:20 +0900

    Update docs/ favicon and remove docusaurus default images

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -64,7 +64,6 @@ const config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       disableSwitch: true,
     },
