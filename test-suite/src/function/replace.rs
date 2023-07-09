7da7919917f1ca660023a143431930cc48ba6093
chobobdev <chobobdev@naver.com>
2023-07-09 22:37:06 +0900

    test: add test cases for REPLACE function

--- test-suite/src/function/replace.rs
+++ test-suite/src/function/replace.rs
@@ -1,1 +1,30 @@
+use {
+    crate::*,
+    gluesql_core::{
+        executor::EvaluateError,
+        prelude::{Payload, Value},
+    },
+};
 
+test_case!(replace, async move {
+    let test_cases = [
+        (
+            "CREATE TABLE Item (name TEXT DEFAULT REPLACE('SQL Tutorial', 'T', 'M'))",
+            Ok(Payload::Create),
+        ),
+        (
+            "INSERT INTO Item VALUES ('Tticky GlueTQL')",
+            Ok(Payload::Insert(1)),
+        ),
+        (
+            "SELECT REPLACE(name,'T','S') AS test FROM Item;",
+            Ok(select!(
+                "test"
+                Value::Str;
+                "Sticky GlueSQL".to_owned()
+            )),
+        )      ];
+    for (sql, expected) in test_cases {
+        test!(sql, expected);
+    }
+});
