5a2f29e1dda9886f312a87f5d085d98245cbce15
Gioh Kim <gurugio@linecorp.com>
2023-07-19 00:33:51 +0900

    add test cases for ~ operator

--- test-suite/src/function/bit_not.rs
+++ test-suite/src/function/bit_not.rs
@@ -37,5 +37,42 @@ test_case!(bit_not, async move {
         expected: Ok(select!("test" Value::U64; 11936128518282651045))
     };
 
+    // operator ~
+    test! {
+        name: "bit-wise invert all bits: 1 => -2",
+        sql: "SELECT ~1 AS test;",
+        expected: Ok(select!("test" Value::I64; -2))
+    };
+
+    test! {
+        name: "bit-wise invert all bits: -2 => 1",
+        sql: "SELECT ~-2 AS test;",
+        expected: Ok(select!("test" Value::I64; 1))
+    };
+
+    test! {
+        name: "bit-wise invert all bits: 0 => 18446744073709551615",
+        sql: "SELECT ~0 AS test;",
+        expected: Ok(select!("test" Value::I64; -1))
+    };
+
+    test! {
+        name: "bit-wise invert all bits: 18446744073709551615 => 0",
+        sql: "SELECT ~-1 AS test;",
+        expected: Ok(select!("test" Value::I64; 0))
+    };
+
+    test! {
+        name: "bit-wise invert all bits",
+        sql: "SELECT ~(CAST(11936128518282651045 AS UINT64)) AS test;",
+        expected: Ok(select!("test" Value::U64; 6510615555426900570))
+    };
+
+    test! {
+        name: "bit-wise invert all bits",
+        sql: "SELECT ~(CAST(6510615555426900570 AS UINT64)) AS test;",
+        expected: Ok(select!("test" Value::U64; 11936128518282651045))
+    };
+
     /* TODO: add failure cases; Is there any failure case for bit_not?? */
 });
