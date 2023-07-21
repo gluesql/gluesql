ae45887516b66a6c8a85b91cd4090cc39d843584
kite707 <12201680@inha.edu>
2023-07-21 12:53:23 +0900

    test: add test case of the year change

--- test-suite/src/function/add_month.rs
+++ test-suite/src/function/add_month.rs
@@ -85,4 +85,41 @@ test_case!(add_month, async move {
             date!("2017-07-28")
         ))
     };
+
+    test! {
+        name: "[DATE TYPE]plus test in the case of year change",
+        sql: "SELECT ADD_MONTH(DATE '2017-01-31',13) AS test;",
+        expected: Ok(select!(
+            "test"
+            Value::Date;
+            date!("2017-02-28")
+        ))
+    };
+    test! {
+        name: "[TO_DATE FUNCTION]plus test on the last day of February",
+        sql: "ADD_MONTH(TO_DATE('2017-01-31','%Y-%m-%d'),13) ;",
+        expected: Ok(select!(
+            "test"
+            Value::Date;
+            date!("2017-07-28")
+        ))
+    };
+    test! {
+        name: "[DATE TYPE]minus test on the last day of February",
+        sql: "SELECT ADD_MONTH(DATE '2017-06-30',-12) AS test;",
+        expected: Ok(select!(
+            "test"
+            Value::Date;
+            date!("2017-06-30")
+        ))
+    };
+    test! {
+        name: "[TO_DATE FUNCTION]minus test on the last day of February",
+        sql: "ADD_MONTH(TO_DATE('2017-06-30','%Y-%m-%d'),-12) ;",
+        expected: Ok(select!(
+            "test"
+            Value::Date;
+            date!("2017-06-30")
+        ))
+    };
 });
