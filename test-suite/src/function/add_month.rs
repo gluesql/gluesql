343f69c2d1bade520144a57e8e0d12a6799fd8b0
kite707 <12201680@inha.edu>
2023-07-21 12:37:58 +0900

    test: add minus test

--- test-suite/src/function/add_month.rs
+++ test-suite/src/function/add_month.rs
@@ -13,21 +13,39 @@ test_case!(add_month, async move {
         };
     }
     test! {
-        name: "check add_month function works with date type",
+        name: "[DATE TYPE]check add_month function works as plus",
         sql: "SELECT ADD_MONTH(DATE '2017-06-15',1) AS test;",
         expected: Ok(select!(
             "test"
             Value::Date;
-            date!("2017-06-15")
+            date!("2017-07-15")
         ))
     };
     test! {
-        name: "check add_month function works with to_date function",
+        name: "[TO_DATE FUNCTION]check add_month function works as plus",
         sql: "ADD_MONTH(TO_DATE('2017-06-15','%Y-%m-%d'),1) ;",
         expected: Ok(select!(
             "test"
             Value::Date;
-            date!("2017-06-15")
+            date!("2017-07-15")
+        ))
+    };
+    test! {
+        name: "[DATE TYPE]check add_month function works as minus",
+        sql: "SELECT ADD_MONTH(DATE '2017-06-15',-1) AS test;",
+        expected: Ok(select!(
+            "test"
+            Value::Date;
+            date!("2017-05-15")
+        ))
+    };
+    test! {
+        name: "[TO_DATE FUNCTION]check add_month function works as minus",
+        sql: "ADD_MONTH(TO_DATE('2017-06-15','%Y-%m-%d'),-1) ;",
+        expected: Ok(select!(
+            "test"
+            Value::Date;
+            date!("2017-05-15")
         ))
     };
 });
