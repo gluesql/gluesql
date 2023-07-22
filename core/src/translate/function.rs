5b8feec78433efeeb25e4f13dd248bf96e9cc552
2-NOW <kj109888@gmail.com>
2023-07-22 10:55:05 +0900

    feat(translate): add support for ENTRIES function

--- core/src/translate/function.rs
+++ core/src/translate/function.rs
@@ -392,6 +392,13 @@ pub fn translate_function(sql_function: &SqlFunction) -> Result<Expr> {
                 divisor,
             })))
         }
+        "ENTRIES" => {
+            check_len(name, args.len(), 1)?;
+
+            let expr = translate_expr(args[0])?;
+
+            Ok(Expr::Function(Box::new(Function::Entries(expr))))
+        }
         "MOD" => {
             check_len(name, args.len(), 2)?;
 
