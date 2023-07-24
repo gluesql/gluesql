8cc16bc042610470ed8ca2aeb7f7961b2e0913d4
김민식 <meenseek@ms.local>
2023-07-24 23:50:45 +0900

    [feat] add store function
    Signed-off-by: 김민식 <meenseek@ms.local>

--- storages/parquet-storage/src/store.rs
+++ storages/parquet-storage/src/store.rs
@@ -9,23 +9,51 @@ use {
         error::Result,
         store::{DataRow, RowIter, Store},
     },
-    serde_json::Value as JsonValue,
     std::{ffi::OsStr, fs},
 };
 
 #[async_trait(?Send)]
-#[deny(implied_bounds_entailment)]
 impl Store for ParquetStorage {
     async fn fetch_schema(&self, table_name: &str) -> Result<Option<Schema>> {
         self.fetch_schema(table_name)
     }
 
     async fn fetch_all_schemas(&self) -> Result<Vec<Schema>> {
-        todo!();
+        let paths = fs::read_dir(&self.path).map_storage_err()?;
+        let mut schemas = paths
+            .map(|result| {
+                let path = result.map_storage_err()?.path();
+                let extension = path.extension().and_then(OsStr::to_str);
+                if extension != Some("parquet") {
+                    return Ok(None);
+                }
+
+                let table_name = path
+                    .file_stem()
+                    .and_then(OsStr::to_str)
+                    .map_storage_err(ParquetStorageError::FileNotFound)?;
+
+                self.fetch_schema(table_name)?
+                    .map_storage_err(ParquetStorageError::TableDoesNotExist)
+                    .map(Some)
+            })
+            .filter_map(Result::transpose)
+            .collect::<Result<Vec<Schema>>>()?;
+
+        schemas.sort_by(|a, b| a.table_name.cmp(&b.table_name));
+
+        Ok(schemas)
     }
 
     async fn fetch_data(&self, table_name: &str, target: &Key) -> Result<Option<DataRow>> {
-        todo!();
+        for item in self.scan_data(table_name)?.0 {
+            let (key, row) = item?;
+
+            if &key == target {
+                return Ok(Some(row));
+            }
+        }
+        return Ok(None);
     }
 
     async fn scan_data(&self, table_name: &str) -> Result<RowIter> {
