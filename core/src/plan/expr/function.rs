72a4d2be46d30bc89f15df9b29e5e1d9b00bc3cb
cl-kim <nias0327@naver.com>
2023-07-24 13:09:11 +0900

    fix : Remove unnecessary quotation marks

--- core/src/plan/expr/function.rs
+++ core/src/plan/expr/function.rs
@@ -286,7 +286,7 @@ mod tests {
         test("REPEAT(col || col2, 3)", &["col || col2", "3"]);
         test("REPEAT(column, 2)", &["column", "2"]);
         test(r#"UNWRAP(field, "foo.1")"#, &["field", r#""foo.1""#]);
-        test(r#"SKIP(list, 2)"#, &[r#""list""#, r#""2""#]);
+        test(r#"SKIP(list, 2)"#, &[r#""list""#, r#"2"#]);
 
         // Triple
         test(
