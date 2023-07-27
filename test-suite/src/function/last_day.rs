cab0ccfa14d5d2903b4c7fb2632b4edf2dea4b92
Junyoung Choi(최준영) <cjy13753@gmail.com>
2023-07-27 20:52:20 +0900

    Modify unit tests to cover more lines

--- test-suite/src/function/last_day.rs
+++ test-suite/src/function/last_day.rs
@@ -1,23 +1,64 @@
-use {crate::*, chrono::NaiveDate, gluesql_core::prelude::Value::*};
+use {
+    crate::*, chrono::NaiveDate, gluesql_core::executor::EvaluateError,
+    gluesql_core::prelude::Value::*,
+};
 
 test_case!(last_day, async move {
+    run!(
+        "CREATE TABLE LastDay (
+            id INTEGER,
+            date DATE,
+            timestamp TIMESTAMP,
+        );"
+    );
+
+    run!("INSERT INTO LastDay (id, date) VALUES (1, LAST_DAY(date '2017-12-15'));");
     test! {
-        name: "Should return the last day of the month that a given date belongs to",
-        sql: "VALUES(LAST_DAY(date '2017-12-15'));",
+        name: "Should insert the last day of the month that a given date belongs to",
+        sql: "SELECT date FROM LastDay WHERE id = 1;",
         expected: Ok(select!(
-            column1
+            date;
             Date;
             NaiveDate::from_ymd_opt(2017, 12, 31).unwrap()
         ))
     };
 
+    run!("INSERT INTO LastDay (id, date) VALUES (2, date '2017-01-01');");
     test! {
-        name: "Should return the last day of the month that a given timestamp belongs to",
-        sql: "VALUES(LAST_DAY(timestamp '2017-12-15 12:00:00'));",
+        name: "Should return the last day of the month that a retrieved date belongs to",
+        sql: "SELECT LAST_DAY(date) as date FROM LastDay WHERE id = 2;",
         expected: Ok(select!(
-            column1
+            date;
+            Date;
+            NaiveDate::from_ymd_opt(2017, 1, 31).unwrap()
+        ))
+    };
+
+    run!("INSERT INTO LastDay (id, date) VALUES (3, LAST_DAY(timestamp '2017-12-15 12:12:20'));");
+    test! {
+        name: "Should insert the last day of the month that a given timestamp belongs to",
+        sql: "SELECT date FROM LastDay WHERE id = 3;",
+        expected: Ok(select!(
+            date;
             Date;
             NaiveDate::from_ymd_opt(2017, 12, 31).unwrap()
         ))
+    };
+
+    run!("INSERT INTO LastDay (id, timestamp) VALUES (4, timestamp '2017-01-01 12:12:20');");
+    test! {
+        name: "Should return the last day of the month that a retrieved timestamp belongs to",
+        sql: "SELECT LAST_DAY(timestamp) as date FROM LastDay WHERE id = 4;",
+        expected: Ok(select!(
+            date;
+            Date;
+            NaiveDate::from_ymd_opt(2017, 1, 31).unwrap()
+        ))
+    };
+
+    test! {
+        name: "Should only give date or timestamp value to LAST_DAY function",
+        sql: "VALUES (LAST_DAY('dfafsdf3243252454325342'));",
+        expected: Err(EvaluateError::FunctionRequiresDateOrDateTimeValue("LAST_DAY".to_owned()).into())
     };
 });
