3ff050ef1f59b50a595f7d78f8f524dd056b9f8e
권태현 <kygkth2011@gmail.com>
2023-07-28 21:06:29 +0900

    test : add Slice func test

--- test-suite/src/function/slice.rs
+++ test-suite/src/function/slice.rs
@@ -7,23 +7,51 @@ use {
 };
 
 test_case!(slice, async move {
+    run!("CREATE TABLE Test (list LIST)");
+    run!("INSERT INTO Test VALUES ('[1,2,3,4]')");
     test! {
-        name: "test replace function works while creating a table simultaneously",
-        sql: "CREATE TABLE Test (list LIST)",
-        expected: Ok(Payload::Create)
-    };
-    test! {
-        name: "test if the sample string gets inserted to table",
-        sql: "INSERT INTO Test VALUES ('[1,2,3,4]')",
-        expected: Ok(Payload::Insert(1))
-    };
-    test! {
-        name: "check id the replace function works with the previously inserted string",
-        sql: "SELECT SLICE(list, 1, 2) AS test FROM Item;",
+        name: "slice start in index 0",
+        sql: "SELECT SLICE(list, 0, 2) AS value FROM Test;",
         expected: Ok(select!(
-            "test"
+            "value"
             List;
-            vec![I64(2),I64(3)]
+            vec![I64(1),I64(2)]
         ))
     };
+    test! {
+        name: "slice with size",
+        sql: "SELECT SLICE(list, 0, 4) AS value FROM Test;",
+        expected: Ok(select!(
+            "value"
+            List;
+            vec![I64(1), I64(2),I64(3), I64(4)]
+        ))
+    };
+    test! {
+        name: "slice with size that pass over array size",
+        sql: "SELECT SLICE(list, 2, 5) AS value FROM Test;",
+        expected: Ok(select!(
+            "value"
+            List;
+            vec![I64(3), I64(4)]
+        ))
+    };
+    test! {
+        name: "slice that over array size",
+        sql: "SELECT SLICE(list, 100, 5) AS value FROM Test;",
+        expected: Ok(select!(
+            "value"
+            List;
+            vec![]
+        ))
+    };
+    // test! {
+    //     name: "",
+    //     sql: "SELECT SLICE(list, -1, 5) AS value FROM Test;",
+    //     expected: Ok(select!(
+    //         "value"
+    //         List;
+    //         vec![]
+    //     ))
+    // };
 });
