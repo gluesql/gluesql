a56469e2baded1b6cb3ab4b690c898501232035d
권태현 <kygkth2011@gmail.com>
2023-07-29 17:39:31 +0900

    test : slice funciton in negative infdex

--- test-suite/src/function/slice.rs
+++ test-suite/src/function/slice.rs
@@ -53,17 +53,53 @@ test_case!(slice, async move {
         expected : Err(EvaluateError::FunctionRequiresIntegerValue("SLICE".to_owned()).into())
     };
     test! {
+        name: "start value should be Positive USIZE Value",
+        sql: "SELECT SLICE(list, -1, 1) AS value FROM Test;",
+        expected : Ok(select!(
+            "value"
+            List;
+            vec![I64(4)]
+        ))
+    };
+    test! {
+        name: "start value should be Positive USIZE Value",
+        sql: "SELECT SLICE(list, -2, 4) AS value FROM Test;",
+        expected : Ok(select!(
+            "value"
+            List;
+            vec![I64(3), I64(4)]
+        ))
+    };
+    test! {
+        name: "start value should be Positive USIZE Value",
+        sql: "SELECT SLICE(list, 9999, 4) AS value FROM Test;",
+        expected : Ok(select!(
+            "value"
+            List;
+            vec![]
+        ))
+    };
+    test! {
+        name: "start value should be Positive USIZE Value",
+        sql: "SELECT SLICE(list, 0, 1234) AS value FROM Test;",
+        expected : Ok(select!(
+            "value"
+            List;
+            vec![I64(1), I64(2),I64(3), I64(4)]
+        ))
+    };
+    test! {
+        name: "if absoulte value of negative index over length of list, covert to index 0",
+        sql: "SELECT SLICE(list, -234, 4) AS value FROM Test;",
+        expected : Ok(select!(
+            "value"
+            List;
+            vec![I64(1), I64(2),I64(3), I64(4)]
+        ))
+    };
+    test! {
         name: "length value should be Integer Value",
         sql: "SELECT SLICE(list, 2, 'a') AS value FROM Test;",
     expected : Err(EvaluateError::FunctionRequiresIntegerValue("SLICE".to_owned()).into())
     };
-    // test! {  
-    //     name: "",
-    //     sql: "SELECT SLICE(list, -1, 5) AS value FROM Test;",
-    //     expected: Ok(select!(
-    //         "value"
-    //         List;
-    //         vec![]
-    //     ))
-    // };
 });
