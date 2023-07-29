b588c9bce9c41ee8c405b14161697820f586e479
ding-co <pangoons@naver.com>
2023-07-29 15:37:46 +0900

    feat: Add AST Builder for Take function

--- core/src/ast_builder/mod.rs
+++ core/src/ast_builder/mod.rs
@@ -75,7 +75,7 @@ pub use expr::{
         degrees, divide, exp, extract, find_idx, floor, format, gcd, generate_uuid, get_x, get_y,
         ifnull, initcap, is_empty, lcm, left, length, ln, log, log10, log2, lower, lpad, ltrim,
         md5, modulo, now, pi, point, position, power, radians, rand, repeat, replace, reverse,
-        right, round, rpad, rtrim, sign, sin, skip, sqrt, substr, tan, to_date, to_time,
+        right, round, rpad, rtrim, sign, sin, skip, sqrt, substr, take, tan, to_date, to_time,
         to_timestamp, upper, FunctionNode,
     },
 };
