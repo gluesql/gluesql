40cea7e628cb1b74f71164f9d20c5fe4f5c4637a
권태현 <kygkth2011@gmail.com>
2023-07-29 16:08:37 +0900

    test : Add parameter data type test

--- test-suite/src/function/slice.rs
+++ test-suite/src/function/slice.rs
@@ -1,4 +1,7 @@
-use {crate::*, gluesql_core::prelude::Value::*};
+use {
+    crate::*,
+    gluesql_core::{error::EvaluateError, prelude::Value::*},
+};
 
 test_case!(slice, async move {
     run!("CREATE TABLE Test (list LIST)");
@@ -39,7 +42,22 @@ test_case!(slice, async move {
             vec![]
         ))
     };
-    // test! {
+    test! {
+        name: "list value should be List Value",
+        sql: "SELECT SLICE(1, 2, 2) AS value FROM Test;",
+        expected : Err(EvaluateError::ListTypeRequired.into())
+    }
+    test! {
+        name: "start value should be Integer Value",
+        sql: "SELECT SLICE(list, 'b', 5) AS value FROM Test;",
+        expected : Err(EvaluateError::FunctionRequiresIntegerValue("SLICE".to_owned()).into())
+    };
+    test! {
+        name: "length value should be Integer Value",
+        sql: "SELECT SLICE(list, 2, 'a') AS value FROM Test;",
+    expected : Err(EvaluateError::FunctionRequiresIntegerValue("SLICE".to_owned()).into())
+    };
+    // test! {  
     //     name: "",
     //     sql: "SELECT SLICE(list, -1, 5) AS value FROM Test;",
     //     expected: Ok(select!(
