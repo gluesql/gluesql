0b5c29ecadb722b0b10093e84da2172060dbacad
Gyubong <jopemachine@naver.com>
2023-08-04 12:49:57 +0900

    Update gluesql.pyi

--- pkg/python/gluesql.pyi
+++ pkg/python/gluesql.pyi
@@ -4,47 +4,39 @@ Type hints for Native Rust Extension
 
 from abc import ABCMeta
 
+class Glue:
+    def query(sql: str): ...
+    def set_default_engine(engine: "Storage"): ...
 
 class Storage(metaclass=ABCMeta):
     pass
 
-
-class Glue:
-    def query(sql: str): ...
-    def set_default_engine(engine: Storage): ...
-
-
 class MemoryStorage(Storage):
-    pass
+    def __init__(): ...
 
+class SharedMemoryStorage(Storage):
+    def __init__(): ...
 
 class JsonStorage(Storage):
     def __init__(path: str): ...
 
-
-class SharedMemoryStorage(Storage):
+class SledStorage(Storage):
     def __init__(path: str): ...
 
-
 class GlueSQLError(Exception):
-    pass
-
+    """ """
 
 class EngineNotLoadedError(GlueSQLError):
-    pass
-
+    """ """
 
 class ParsingError(GlueSQLError):
     """ """
 
-
 class TranslateError(GlueSQLError):
     """ """
 
-
 class ExecuteError(GlueSQLError):
     """ """
-
 
 class PlanError(GlueSQLError):
     """ """
