209a7758a8d105cbfa54daef7f5eb8ade73e4947
Gyubong <jopemachine@naver.com>
2023-08-06 02:34:13 +0900

    Update gluesql.pyi

--- pkg/python/gluesql.pyi
+++ pkg/python/gluesql.pyi
@@ -22,11 +22,26 @@ class JsonStorage(Storage):
     def __init__(path: str): ...
 
 class SledStorageModeConfig:
+    """
+    In this mode, the database will make
+    decisions that favor using less space
+    instead of supporting the highest possible
+    write throughput. This mode will also
+    rewrite data more frequently as it
+    strives to reduce fragmentation.
+    """
     LowSpace: Final[Any]
+
+    """
+    In this mode, the database will try
+    to maximize write throughput while
+    potentially using more disk space.
+    """
     HighThroughput: Final[Any]
 
 class SledStorageConfig:
     def __init__(
+        *,
         path: str,
         temporary: bool,
         use_compression: bool,
@@ -48,7 +63,7 @@ class SledStorageConfig:
 class SledStorage(Storage):
     def __init__(path: str): ...
     @staticmethod
-    def try_from(config: SledStorageConfig) -> "SledStorage": ...
+    def try_from(config: "SledStorageConfig") -> "SledStorage": ...
 
 class GlueSQLError(Exception):
     """ """
