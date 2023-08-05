730e2712faa48f4d4e2bbb17d13b2f50bf4a2d49
2-NOW <kj109888@gmail.com>
2023-08-05 15:11:43 +0900

    feat(ast_builder): add entries function

--- core/src/ast_builder/mod.rs
+++ core/src/ast_builder/mod.rs
@@ -78,7 +78,7 @@ pub use expr::{
         ifnull, initcap, is_empty, last_day, lcm, left, length, ln, log, log10, log2, lower, lpad,
         ltrim, md5, modulo, now, pi, point, position, power, radians, rand, repeat, replace,
         reverse, right, round, rpad, rtrim, sign, sin, skip, sqrt, substr, take, tan, to_date,
-        to_time, to_timestamp, upper, FunctionNode,
+        to_time, to_timestamp, upper, entries, FunctionNode,
     },
 };
 
