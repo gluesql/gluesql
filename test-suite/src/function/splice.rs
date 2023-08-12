75de8fe7bbbd1a059c532a2c6aeb593e9d3503fa
jinlee0 <wls4370@gmail.com>
2023-08-12 16:07:32 +0900

    test: ListTypeReqired

--- test-suite/src/function/splice.rs
+++ test-suite/src/function/splice.rs
@@ -1,4 +1,7 @@
-use {crate::*, gluesql_core::prelude::Value::*};
+use {
+    crate::*,
+    gluesql_core::{executor::EvaluateError, prelude::Value::*},
+};
 
 test_case!(splice, async move {
     run!(
@@ -41,5 +44,17 @@ test_case!(splice, async move {
         name: "SPLICE(CAST('[1, 2, 3]' AS List), 1, 100, CAST('[100, 99]' AS List)) should return '[1, 100, 99]')",
         sql: "SELECT SPLICE(CAST('[1, 2, 3]' AS List), 1, 100, CAST('[100, 99]' AS List)) AS actual",
         expected: Ok(select!(actual List; vec!(I64(1), I64(100), I64(99))))
+    );
+
+    test!(
+        name: "SPLICE(3, 1, 2) sholud return EvaluateError::ListTypeRequired",
+        sql: "SELECT SPLICE(1, 2, 3) AS actual",
+        expected: Err(EvaluateError::ListTypeRequired.into())
+    );
+
+    test!(
+        name: "SPLICE(CAST('[1, 2, 3]' AS List), 2, 4, 9) should return EvaluateError::ListTypeRequired",
+        sql:"SELECT SPLICE(CAST('[1, 2, 3]' AS List), 2, 4, 9), AS actual",
+        expected: Err(EvaluateError::ListTypeRequired.into())
     );
 });
