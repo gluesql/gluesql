56aced0b11a1b535bf57dc1bdb102db830338389
ding-co <pangoons@naver.com>
2023-09-02 16:54:52 +0900

    refactor: Cargo clippy for is_none fn

--- core/src/executor/evaluate/expr.rs
+++ core/src/executor/evaluate/expr.rs
@@ -50,14 +50,14 @@ pub fn binary_op<'a>(
         BinaryOperator::NotEq => cmp!(!l.evaluate_eq(&r)),
         BinaryOperator::Lt => cmp!(l.evaluate_cmp(&r) == Some(Ordering::Less)),
         BinaryOperator::LtEq => {
-            if l.evaluate_cmp(&r) == None {
+            if l.evaluate_cmp(&r).is_none() {
                 return Ok(Evaluated::from(Value::Bool(false)));
             }
             cmp!(l.evaluate_cmp(&r) != Some(Ordering::Greater))
         }
         BinaryOperator::Gt => cmp!(l.evaluate_cmp(&r) == Some(Ordering::Greater)),
         BinaryOperator::GtEq => {
-            if l.evaluate_cmp(&r) == None {
+            if l.evaluate_cmp(&r).is_none() {
                 return Ok(Evaluated::from(Value::Bool(false)));
             }
             cmp!(l.evaluate_cmp(&r) != Some(Ordering::Less))
