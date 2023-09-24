eb3ae2677bc6320f6fa144f6405107b66a788e54
zmrdltl <meenseek5929@naver.com>
2023-09-24 11:26:43 +0900

    test: remove unused test & add nested list, map

--- storages/parquet-storage/tests/schema.rs
+++ storages/parquet-storage/tests/schema.rs
@@ -1,9 +1,13 @@
-use gluesql_core::chrono::NaiveDateTime;
 use parquet::data_type::ByteArray;
+
 use {
-    gluesql_core::prelude::{
-        Glue,
-        Value::{self, *},
+    gluesql_core::{
+        chrono::NaiveDateTime,
+        error::Error,
+        prelude::{
+            Glue,
+            Value::{self, *},
+        },
     },
     gluesql_parquet_storage::ParquetStorage,
     test_suite::{concat_with, row, select, stringify_label},
@@ -56,174 +60,52 @@ async fn test_alltypes_select() {
                 1     false      1             1              1         10           1.1         10.1         bytea("01/01/09")  bytea("1")     ts("2009-01-01T00:01:00")
             )),
         ),
+        (
+            glue.execute("SELECT * FROM null_list").await,
+            Ok(select!(
+                emptylist;
+                List;
+                vec![]
+            )),
+        ),
+        (
+            glue.execute("SELECT * FROM nested_lists_snappy").await,
+            Ok(select!(
+                a | b;
+                List | I32;
+                vec![
+                    List(vec![
+                        List(vec![Str("a".to_owned()), Str("b".to_owned())]),
+                        List(vec![Str("c".to_owned())])
+                    ]),
+                    List(vec![Null, List(vec![Str("d".to_owned())])])
+                ] 1;
+                vec![
+                    List(vec![
+                        List(vec![Str("a".to_owned()), Str("b".to_owned())]),
+                        List(vec![Str("c".to_owned()), Str("d".to_owned())])
+                    ]),
+                    List(vec![Null, List(vec![Str("e".to_owned())])])
+                ] 1;
+                vec![
+                    List(vec![
+                        List(vec![Str("a".to_owned()), Str("b".to_owned())]),
+                        List(vec![Str("c".to_owned()), Str("d".to_owned())]),
+                        List(vec![Str("e".to_owned())])
+                    ]),
+                    List(vec![Null, List(vec![Str("f".to_owned())])])
+                ] 1
+            )),
+        ),
+        (
+            glue.execute("SELECT * FROM nested_maps_snappy").await,
+            Err(Error::StorageMsg(
+                "Unexpected key type for map: received Int(1), expected String".to_string(),
+            )),
+        ),
     ];
-
-    //let actual = Payload::SelectMap(actual);
 
     for (actual, expected) in cases {
         assert_eq!(actual.map(|mut payloads| payloads.remove(0)), expected);
     }
 }
-
-#[tokio::test]
-async fn test_schemaless() {
-    // let actual = glue
-    //     .execute("SELECT * FROM alltypes_dictionary")
-    //     .await
-    //     .unwrap()
-    //     .into_iter()
-    //     .next()
-    //     .unwrap();
-
-    // let expected = [
-    //     vec![
-    //         ("id", I32(0)),
-    //         ("bool_col", Bool(true)),
-    //         ("tinyint_col", I32(0)),
-    //         ("smallint_col", I32(0)),
-    //         ("int_col", I32(0)),
-    //         ("bigint_col", I64(0)),
-    //         ("float_col", F32(0.0)),
-    //         ("double_col", F64(0.0)),
-    //         (
-    //             "date_string_col",
-    //             Value::Bytea(vec![48, 49, 47, 48, 49, 47, 48, 57]),
-    //         ),
-    //         ("string_col", Value::Bytea(vec![48])),
-    //         ("timestamp_col", I64(0)),
-    //     ],
-    //     vec![
-    //         ("id", I32(1)),
-    //         ("bool_col", Bool(false)),
-    //         ("tinyint_col", I32(1)),
-    //         ("smallint_col", I32(1)),
-    //         ("int_col", I32(1)),
-    //         ("bigint_col", I64(10)),
-    //         ("float_col", F32(1.100000023841858)),
-    //         ("double_col", F64(10.1)),
-    //         (
-    //             "date_string_col",
-    //             Value::Bytea(vec![48, 49, 47, 48, 49, 47, 48, 57]),
-    //         ),
-    //         ("string_col", Value::Bytea(vec![49])),
-    //         ("timestamp_col", I64(0)),
-    //     ],
-    // ]
-    // .into_iter()
-    // .map(|row| {
-    //     row.into_iter()
-    //         .map(|(k, v)| (k.to_owned(), v))
-    //         .collect::<HashMap<_, _>>()
-    // })
-    // .collect::<Vec<_>>();
-    // let expected = Payload::SelectMap(expected);
-    // assert_eq!(actual, expected);
-    // let convertu8_to_string =
-    //     |input: Vec<u8>| -> Result<String, std::string::FromUtf8Error> { String::from_utf8(input) };
-    // let convert_to_bytea = |input: String| -> Vec<u8> { input.into_bytes() };
-}
-
-// #[tokio::test]
-// async fn test_alltypes_dictionary_fetch_schema() {
-//     let path = "./tests/samples/data";
-//     let parquet_storage = ParquetStorage::new(path).unwrap();
-//     let mut glue = Glue::new(parquet_storage.clone());
-//     macro_rules! run {
-//         ($sql:expr) => {
-//             run($sql, &mut glue, None).await.unwrap()
-//         };
-//     }
-//     run!("SELECT * FROM alltypes_dictionary");
-//     let schemas = parquet_storage
-//         .fetch_all_schemas()
-//         .await
-//         .expect("Failed to fetch schemas");
-
-//     let expected_schema = Schema {
-//         table_name: "alltypes_dictionary".to_string(),
-//         column_defs: Some(vec![
-//             ColumnDef {
-//                 name: "id".to_string(),
-//                 data_type: Int32,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "bool_col".to_string(),
-//                 data_type: Boolean,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "tinyint_col".to_string(),
-//                 data_type: Int32,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "smallint_col".to_string(),
-//                 data_type: Int32,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "int_col".to_string(),
-//                 data_type: Int32,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "bigint_col".to_string(),
-//                 data_type: Int,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "float_col".to_string(),
-//                 data_type: Float32,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "double_col".to_string(),
-//                 data_type: Float,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "date_string_col".to_string(),
-//                 data_type: DataType::Bytea,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "string_col".to_string(),
-//                 data_type: DataType::Bytea,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//             ColumnDef {
-//                 name: "timestamp_col".to_string(),
-//                 data_type: Int128,
-//                 nullable: false,
-//                 default: None,
-//                 unique: None,
-//             },
-//         ]),
-//         indexes: vec![],
-//         engine: None,
-//     };
-
-//     // 이 부분은 스키마가 어떻게 저장되고 가져와지는지에 따라 조정해야 할 수 있습니다.
-//     assert_eq!(schemas.get(0), Some(&expected_schema));
-// }
