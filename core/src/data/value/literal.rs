9925e9ad3e18741df5510573fb204c7c790d4b08
zmrdltl <meenseek5929@naver.com>
2023-09-28 22:57:11 +0900

    fix: Parsing Fractional Strings as Int resolve #1309 Modified the parsing logic in the match arm for Literal::Number to properly handle strings containing fractional values. When a string containing a '.' is encountered, it is now parsed as a floating-point number (f64), otherwise as an integer (i64).
    Signed-off-by: zmrdltl <meenseek5929@naver.com>

--- core/src/data/value/literal.rs
+++ core/src/data/value/literal.rs
@@ -24,11 +24,20 @@ impl TryFrom<&Literal<'_>> for Value {
 
     fn try_from(literal: &Literal<'_>) -> Result<Self> {
         match literal {
-            Literal::Number(v) => v
-                .to_i64()
-                .map(Value::I64)
-                .or_else(|| v.to_f64().map(Value::F64))
-                .ok_or_else(|| ValueError::FailedToParseNumber.into()),
+            Literal::Number(v) => {
+                let num_str = v.to_string();
+                if num_str.contains('.') {
+                    match num_str.parse::<f64>() {
+                        Ok(num_as_f64) => Ok(Value::F64(num_as_f64)),
+                        Err(_) => Err(ValueError::FailedToParseNumber.into()),
+                    }
+                } else {
+                    match num_str.parse::<i64>() {
+                        Ok(num_as_i64) => Ok(Value::I64(num_as_i64)),
+                        Err(_) => Err(ValueError::FailedToParseNumber.into()),
+                    }
+                }
+            }
             Literal::Boolean(v) => Ok(Value::Bool(*v)),
             Literal::Text(v) => Ok(Value::Str(v.as_ref().to_owned())),
             Literal::Bytea(v) => Ok(Value::Bytea(v.to_vec())),
