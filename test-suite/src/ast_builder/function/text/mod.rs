7d04ce73fddb346b0dad18ac1a91e38b8d86ae95
henry <devgony@gmail.com>
2023-10-15 14:58:25 +0900

    Add ast builder character_conversion test cases (#1424)

--- test-suite/src/ast_builder/function/text/mod.rs
+++ test-suite/src/ast_builder/function/text/mod.rs
@@ -4,5 +4,8 @@ pub use trimming::trimming;
 mod case_conversion;
 pub use case_conversion::case_conversion;
 
+mod character_conversion;
+pub use character_conversion::character_conversion;
+
 mod position_and_indexing;
 pub use position_and_indexing::position_and_indexing;
