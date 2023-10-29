8a527db8ce2affe961d4f123984d10ee36d5912f
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-10-29 14:49:16 +0900

    fix: merge code to prebuild

--- core/src/ast_builder/index_item/mod.rs
+++ core/src/ast_builder/index_item/mod.rs
@@ -51,30 +51,20 @@ impl<'a> Prebuild<IndexItem> for IndexItemNode<'a> {
                 name,
                 asc,
                 cmp_expr,
-            } => build_non_clustered(name, asc, cmp_expr),
-            IndexItemNode::PrimaryKey(expr) => build_primary_key(expr),
+            } => {
+                let (index_operator, expr) = cmp_expr.unzip();
+                let expr_result: Option<Expr> = expr.map(ExprNode::try_into).transpose()?;
+                let cmp_expr_result: Option<(IndexOperator, Expr)> = index_operator
+                    .zip(expr_result)
+                    .map(|(index_operator, expr)| (index_operator, expr));
+
+                Ok(IndexItem::NonClustered {
+                    name,
+                    asc,
+                    cmp_expr: cmp_expr_result,
+                })
+            }
+            IndexItemNode::PrimaryKey(expr) => Ok(IndexItem::PrimaryKey(expr.try_into()?)),
         }
     }
-}
-
-fn build_non_clustered(
-    name: String,
-    asc: Option<bool>,
-    cmp_expr: Option<(IndexOperator, ExprNode)>,
-) -> Result<IndexItem> {
-    let (index_operator, expr) = cmp_expr.unzip();
-    let expr_result: Option<Expr> = expr.map(ExprNode::try_into).transpose()?;
-    let cmp_expr_result: Option<(IndexOperator, Expr)> = index_operator
-        .zip(expr_result)
-        .map(|(index_operator, expr)| (index_operator, expr));
-
-    Ok(IndexItem::NonClustered {
-        name,
-        asc,
-        cmp_expr: cmp_expr_result,
-    })
-}
-
-fn build_primary_key(expr: ExprNode) -> Result<IndexItem> {
-    Ok(IndexItem::PrimaryKey(expr.try_into()?))
 }
