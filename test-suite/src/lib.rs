429281a9c3ba56fe3ccc3f4b5c9daafffd4a7bb9
henry <devgony@gmail.com>
2023-10-29 12:49:16 +0900

    test: add test cases for astb-padding

--- test-suite/src/lib.rs
+++ test-suite/src/lib.rs
@@ -248,6 +248,10 @@ macro_rules! generate_store_tests {
             ast_builder::function::text::character_conversion
         );
         glue!(
+            ast_builder_function_text_padding,
+            ast_builder::function::text::padding
+        );
+        glue!(
             ast_builder_function_other_coalesce,
             ast_builder::function::other::coalesce::coalesce
         );
