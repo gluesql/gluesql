8a2cb5b5d238cd9f80821457541d0adab4b5c2a4
gurugio <gurugio@gmail.com>
2023-11-11 14:10:29 +0900

    Implement Redis storage (#1396)
    Implement RedisStorage with `Store`, `StoreMut`, `AlterTable` and `Metadata` trait impl.
    
    Co-authored-by: Gioh Kim <gurugio@linecorp.com>

---
+++ storages/redis-storage/src/alter_table.rs
@@ -0,0 +1,254 @@
+use {
+    super::RedisStorage,
+    async_trait::async_trait,
+    gluesql_core::{
+        ast::ColumnDef,
+        data::Value,
+        error::{AlterTableError, Error, Result},
+        store::{AlterTable, DataRow, Store},
+    },
+    redis::Commands,
+};
+
+#[async_trait(?Send)]
+impl AlterTable for RedisStorage {
+    async fn rename_schema(&mut self, table_name: &str, new_table_name: &str) -> Result<()> {
+        if let Some(mut schema) = self.fetch_schema(table_name).await? {
+            // Which should be done first? deleting or storing?
+            self.redis_delete_schema(table_name)?;
+
+            schema.table_name = new_table_name.to_owned();
+            self.redis_store_schema(&schema)?;
+
+            let redis_key_iter: Vec<String> = self.redis_execute_scan(table_name)?;
+
+            for redis_key in redis_key_iter {
+                if let Some(value) = self.redis_execute_get(&redis_key)? {
+                    let key = Self::redis_parse_key(&redis_key)?;
+                    let new_key = Self::redis_generate_key(&self.namespace, new_table_name, &key)?;
+
+                    self.redis_execute_set(&new_key, &value)?;
+                    self.redis_execute_del(&redis_key)?;
+                }
+            }
+        } else {
+            return Err(AlterTableError::TableNotFound(table_name.to_owned()).into());
+        }
+
+        Ok(())
+    }
+
+    async fn rename_column(
+        &mut self,
+        table_name: &str,
+        old_column_name: &str,
+        new_column_name: &str,
+    ) -> Result<()> {
+        if let Some(mut schema) = self.fetch_schema(table_name).await? {
+            let column_defs = schema
+                .column_defs
+                .as_mut()
+                .ok_or_else(|| AlterTableError::SchemalessTableFound(table_name.to_owned()))?;
+
+            if column_defs
+                .iter()
+                .any(|ColumnDef { name, .. }| name == new_column_name)
+            {
+                return Err(
+                    AlterTableError::AlreadyExistingColumn(new_column_name.to_owned()).into(),
+                );
+            }
+
+            let column_def = column_defs
+                .iter_mut()
+                .find(|column_def| column_def.name == old_column_name)
+                .ok_or(AlterTableError::RenamingColumnNotFound)?;
+
+            column_def.name = new_column_name.to_owned();
+
+            self.redis_delete_schema(table_name)?;
+            self.redis_store_schema(&schema)?;
+        } else {
+            return Err(AlterTableError::TableNotFound(table_name.to_owned()).into());
+        }
+
+        Ok(())
+    }
+
+    async fn add_column(&mut self, table_name: &str, column_def: &ColumnDef) -> Result<()> {
+        if let Some(mut schema) = self.fetch_schema(table_name).await? {
+            let column_defs = schema
+                .column_defs
+                .as_mut()
+                .ok_or_else(|| AlterTableError::SchemalessTableFound(table_name.to_owned()))?;
+
+            if column_defs
+                .iter()
+                .any(|ColumnDef { name, .. }| name == &column_def.name)
+            {
+                let adding_column = column_def.name.to_owned();
+
+                return Err(AlterTableError::AlreadyExistingColumn(adding_column).into());
+            }
+
+            let ColumnDef {
+                data_type,
+                nullable,
+                default,
+                ..
+            } = column_def;
+
+            let new_value_of_new_column = match (default, nullable) {
+                (Some(expr), _) => {
+                    let evaluated = gluesql_core::executor::evaluate_stateless(None, expr).await?;
+
+                    evaluated.try_into_value(data_type, *nullable)?
+                }
+                (None, true) => Value::Null,
+                (None, false) => {
+                    return Err(AlterTableError::DefaultValueRequired(column_def.clone()).into())
+                }
+            };
+
+            // NOTE: It cannot call self.redis_execute_scan/get/set methods directly.
+            // column_defs has a reference to item and the item has a reference to self.
+            // Therefore it cannot call self.redis_execute_scan method because
+            // it needs to use the mutable reference of self.
+            // Otherwise, it will cause a mutable reference conflict.
+            let scan_key = Self::redis_generate_scankey(&self.namespace, table_name);
+            let key_iter: Vec<String> = self
+                .conn
+                .borrow_mut()
+                .scan_match(&scan_key)
+                .map(|iter| iter.collect::<Vec<String>>())
+                .map_err(|_| {
+                    Error::StorageMsg(format!(
+                        "[RedisStorage] failed to execute SCAN: key={}",
+                        scan_key
+                    ))
+                })?;
+
+            for key in key_iter {
+                let value = redis::cmd("GET")
+                    .arg(&key)
+                    .query::<String>(&mut self.conn.borrow_mut())
+                    .map_err(|_| {
+                        Error::StorageMsg(format!(
+                            "[RedisStorage] failed to execute GET: key={}",
+                            key
+                        ))
+                    })?;
+
+                let mut row: DataRow = serde_json::from_str(&value).map_err(|e| {
+                    Error::StorageMsg(format!(
+                        "[RedisStorage] failed to deserialize value={} error={}",
+                        value, e
+                    ))
+                })?;
+                match &mut row {
+                    DataRow::Vec(values) => {
+                        values.push(new_value_of_new_column.clone());
+                    }
+                    DataRow::Map(_) => {
+                        return Err(Error::StorageMsg(
+                            "[RedisStorage] conflict - add_column failed: schemaless row found"
+                                .to_owned(),
+                        ));
+                    }
+                }
+
+                let new_value = serde_json::to_string(&row).map_err(|_e| {
+                    Error::StorageMsg(format!(
+                        "[RedisStorage] failed to serialize row={:?} error={}",
+                        row, _e
+                    ))
+                })?;
+                let _: () = redis::cmd("SET")
+                    .arg(&key)
+                    .arg(new_value)
+                    .query(&mut self.conn.borrow_mut())
+                    .map_err(|_| {
+                        Error::StorageMsg(format!(
+                            "[RedisStorage] add_column: failed to execute SET for row={:?}",
+                            row
+                        ))
+                    })?;
+            }
+
+            column_defs.push(column_def.clone());
+            self.redis_delete_schema(table_name)?; // No problem yet, finally it's ok to delete the old schema
+            self.redis_store_schema(&schema)?;
+        } else {
+            return Err(AlterTableError::TableNotFound(table_name.to_owned()).into());
+        }
+
+        Ok(())
+    }
+
+    async fn drop_column(
+        &mut self,
+        table_name: &str,
+        column_name: &str,
+        if_exists: bool,
+    ) -> Result<()> {
+        if let Some(mut schema) = self.fetch_schema(table_name).await? {
+            let column_defs = schema
+                .column_defs
+                .as_mut()
+                .ok_or_else(|| AlterTableError::SchemalessTableFound(table_name.to_owned()))?;
+
+            let column_index = column_defs
+                .iter()
+                .position(|column_def| column_def.name == column_name);
+
+            match column_index {
+                Some(column_index) => {
+                    column_defs.remove(column_index);
+
+                    let key_iter = self.redis_execute_scan(table_name)?;
+                    for key in key_iter {
+                        if let Some(value) = self.redis_execute_get(&key)? {
+                            let mut row: DataRow = serde_json::from_str(&value).map_err(|e| {
+                                Error::StorageMsg(format!(
+                                    "[RedisStorage] failed to deserialize value={} error={}",
+                                    value, e
+                                ))
+                            })?;
+                            match &mut row {
+                                DataRow::Vec(values) => {
+                                    values.remove(column_index);
+                                }
+                                DataRow::Map(_) => {
+                                    return Err(Error::StorageMsg(
+                                    "[RedisStorage] conflict - add_column failed: schemaless row found".to_owned(),
+                                ));
+                                }
+                            }
+
+                            let new_value = serde_json::to_string(&row).map_err(|e| {
+                                Error::StorageMsg(format!(
+                                    "[RedisStorage] failed to serialize row={:?} error={}",
+                                    row, e
+                                ))
+                            })?;
+                            self.redis_execute_set(&key, &new_value)?;
+                        }
+                    }
+                }
+                None if if_exists => {}
+                None => {
+                    return Err(
+                        AlterTableError::DroppingColumnNotFound(column_name.to_owned()).into(),
+                    )
+                }
+            };
+
+            self.redis_delete_schema(table_name)?; // No problem yet, finally it's ok to delete the old schema
+            self.redis_store_schema(&schema)?;
+        } else {
+            return Err(AlterTableError::TableNotFound(table_name.to_owned()).into());
+        }
+
+        Ok(())
+    }
+}
---
+++ storages/redis-storage/tests/redis_table.rs
@@ -0,0 +1,229 @@
+use {
+    gluesql_core::prelude::{Glue, Payload, Value},
+    gluesql_redis_storage::RedisStorage,
+    std::{env, fs},
+};
+
+macro_rules! exec {
+    ($glue: ident $sql: literal) => {
+        $glue.execute($sql).await.unwrap();
+    };
+}
+
+/// MUST run redis locally before test
+/// eg.) docker run --rm -p 6379:6379 redis
+#[tokio::test]
+async fn redis_storage_tables() {
+    use chrono::NaiveDate;
+
+    let mut path = env::current_dir().unwrap();
+    path.push("tests/redis-storage.toml");
+    let redis_config_str = fs::read_to_string(path).unwrap();
+    let redis_config: toml::Value = toml::from_str(&redis_config_str).unwrap();
+    let url = redis_config["redis"]["url"].as_str().unwrap();
+    let port: u16 = redis_config["redis"]["port"].as_integer().unwrap() as u16;
+
+    let storage = RedisStorage::new("redis_storage_tables", url, port);
+    let mut glue = Glue::new(storage);
+
+    exec!(glue "DROP TABLE IF EXISTS DC;");
+    exec!(glue "DROP TABLE IF EXISTS Marvels;");
+
+    exec!(glue "CREATE TABLE DC (id INTEGER, name TEXT, birth DATE);");
+    exec!(glue r#"INSERT INTO DC (id, name, birth) values (1, 'Superman', '2023-12-31');"#);
+    exec!(glue r#"INSERT INTO DC (id, name, birth) values (2, 'Flash', '2011-12-31');"#);
+
+    exec!(glue "CREATE TABLE Marvels (id INTEGER, name TEXT);");
+    exec!(glue r#"INSERT INTO Marvels (id, name) values (1, 'Ironman');"#);
+    exec!(glue r#"INSERT INTO Marvels (id, name) values (2, 'Wanda');"#);
+
+    let ret: Vec<Payload> = glue
+        .execute("SELECT id, name FROM Marvels WHERE name = 'Wanda';")
+        .await
+        .unwrap();
+    match &ret[0] {
+        Payload::Select { labels, rows } => {
+            assert_eq!(labels[0], "id");
+            assert_eq!(labels[1], "name");
+
+            assert_eq!(rows[0][0], Value::I64(2));
+            assert_eq!(rows[0][1], Value::Str("Wanda".to_string()));
+        }
+        _ => unreachable!(),
+    }
+
+    exec!(glue "DROP TABLE Marvels;");
+    assert!(glue.execute("SELECT id, name FROM Marvels;").await.is_err());
+
+    let ret: Vec<Payload> = glue
+        .execute("SELECT id, name, birth FROM DC WHERE name = 'Superman';")
+        .await
+        .unwrap();
+    match &ret[0] {
+        Payload::Select { labels, rows } => {
+            assert_eq!(labels[0], "id");
+            assert_eq!(labels[1], "name");
+            assert_eq!(labels[2], "birth");
+
+            assert_eq!(rows[0][0], Value::I64(1));
+            assert_eq!(rows[0][1], Value::Str("Superman".to_string()));
+            assert_eq!(
+                rows[0][2],
+                Value::Date(NaiveDate::from_ymd_opt(2023, 12, 31).unwrap())
+            );
+        }
+        _ => unreachable!(),
+    }
+
+    exec!(glue "DROP TABLE DC;");
+}
+
+#[tokio::test]
+async fn redis_storage_add_column() {
+    let mut path = env::current_dir().unwrap();
+    path.push("tests/redis-storage.toml");
+    let redis_config_str = fs::read_to_string(path).unwrap();
+    let redis_config: toml::Value = toml::from_str(&redis_config_str).unwrap();
+    let url = redis_config["redis"]["url"].as_str().unwrap();
+    let port: u16 = redis_config["redis"]["port"].as_integer().unwrap() as u16;
+
+    let storage = RedisStorage::new("redis_storage_add_column", url, port);
+    let mut glue = Glue::new(storage);
+
+    exec!(glue "CREATE TABLE dummy (id INTEGER PRIMARY KEY);");
+    exec!(glue "INSERT INTO dummy (id) values (1)");
+    exec!(glue "INSERT INTO dummy (id) values (11)");
+    exec!(glue "ALTER TABLE dummy ADD newcol TEXT");
+
+    let ret: Vec<Payload> = glue.execute("SELECT id, newcol FROM dummy;").await.unwrap();
+    match &ret[0] {
+        Payload::Select { labels, rows } => {
+            assert_eq!(labels[0], "id");
+            assert_eq!(labels[1], "newcol");
+            assert_eq!(rows[0][0], Value::I64(1));
+            assert_eq!(rows[0][1], Value::Null);
+            assert_eq!(rows[1][0], Value::I64(11));
+            assert_eq!(rows[0][1], Value::Null);
+        }
+        _ => unreachable!(),
+    }
+
+    exec!(glue "DROP TABLE dummy;");
+
+    // TABLE dummy was dropped.
+    // It should be able to create the same namespace again.
+    let storage = RedisStorage::new("redis_Storage_add_column", url, port);
+    let mut glue = Glue::new(storage);
+
+    exec!(glue "CREATE TABLE dummy (id INTEGER);");
+    exec!(glue "INSERT INTO dummy (id) values (1)");
+    exec!(glue "INSERT INTO dummy (id) values (11)");
+    exec!(glue "ALTER TABLE dummy ADD newcol TEXT");
+
+    let ret: Vec<Payload> = glue.execute("SELECT id, newcol FROM dummy;").await.unwrap();
+    match &ret[0] {
+        Payload::Select { labels, rows } => {
+            assert_eq!(labels[0], "id");
+            assert_eq!(labels[1], "newcol");
+            assert_eq!(rows[0][0], Value::I64(1));
+            assert_eq!(rows[0][1], Value::Null);
+            assert_eq!(rows[1][0], Value::I64(11));
+            assert_eq!(rows[0][1], Value::Null);
+        }
+        _ => unreachable!(),
+    }
+
+    exec!(glue "DROP TABLE dummy;");
+}
+
+#[tokio::test]
+async fn redis_storage_drop_column() {
+    let mut path = env::current_dir().unwrap();
+    path.push("tests/redis-storage.toml");
+    let redis_config_str = fs::read_to_string(path).unwrap();
+    let redis_config: toml::Value = toml::from_str(&redis_config_str).unwrap();
+    let url = redis_config["redis"]["url"].as_str().unwrap();
+    let port: u16 = redis_config["redis"]["port"].as_integer().unwrap() as u16;
+
+    let storage = RedisStorage::new("redis_storage_drop_column", url, port);
+    let mut glue = Glue::new(storage);
+
+    exec!(glue "CREATE TABLE dummy (id INTEGER PRIMARY KEY, name TEXT);");
+    exec!(glue r#"INSERT INTO dummy (id, name) values (1, 'Superman');"#);
+    exec!(glue r#"INSERT INTO dummy (id, name) values (11, 'Batman');"#);
+    exec!(glue "ALTER TABLE dummy DROP COLUMN name");
+
+    let ret: Vec<Payload> = glue.execute("SELECT * FROM dummy;").await.unwrap();
+    match &ret[0] {
+        Payload::Select { labels, rows } => {
+            assert_eq!(labels[0], "id");
+            assert_eq!(rows[0].len(), 1);
+            assert_eq!(rows[1].len(), 1);
+            assert_eq!(rows[0][0], Value::I64(1));
+            assert_eq!(rows[1][0], Value::I64(11));
+        }
+        _ => unreachable!(),
+    }
+
+    exec!(glue "DROP TABLE dummy;");
+
+    // TABLE dummy was dropped.
+    // It should be able to create the same namespace again.
+    let storage = RedisStorage::new("redis_storage_drop_column", url, port);
+    let mut glue = Glue::new(storage);
+
+    // Second test without PRIMARY KEY
+    exec!(glue "CREATE TABLE dummy (id INTEGER, name TEXT);");
+    exec!(glue r#"INSERT INTO dummy (id, name) values (1, 'Superman');"#);
+    exec!(glue r#"INSERT INTO dummy (id, name) values (11, 'Batman');"#);
+    exec!(glue "ALTER TABLE dummy DROP COLUMN name");
+
+    let ret: Vec<Payload> = glue.execute("SELECT * FROM dummy;").await.unwrap();
+    match &ret[0] {
+        Payload::Select { labels, rows } => {
+            assert_eq!(labels[0], "id");
+            assert_eq!(rows[0].len(), 1);
+            assert_eq!(rows[1].len(), 1);
+            assert_eq!(rows[0][0], Value::I64(1));
+            assert_eq!(rows[1][0], Value::I64(11));
+        }
+        _ => unreachable!(),
+    }
+
+    exec!(glue "DROP TABLE dummy;");
+}
+
+#[tokio::test]
+async fn redis_storage_alter_tablename() {
+    let mut path = env::current_dir().unwrap();
+    path.push("tests/redis-storage.toml");
+    let redis_config_str = fs::read_to_string(path).unwrap();
+    let redis_config: toml::Value = toml::from_str(&redis_config_str).unwrap();
+    let url = redis_config["redis"]["url"].as_str().unwrap();
+    let port: u16 = redis_config["redis"]["port"].as_integer().unwrap() as u16;
+
+    let storage = RedisStorage::new("redis_storage_alter_tablename", url, port);
+    let mut glue = Glue::new(storage);
+
+    exec!(glue "CREATE TABLE dummy (id INTEGER PRIMARY KEY);");
+    exec!(glue "INSERT INTO dummy (id) values (1)");
+    exec!(glue "INSERT INTO dummy (id) values (11)");
+    exec!(glue "ALTER TABLE dummy RENAME TO dumdum");
+
+    let ret = glue.execute("SELECT * FROM dummy;").await;
+    assert!(ret.is_err());
+
+    let ret: Vec<Payload> = glue.execute("SELECT * FROM dumdum;").await.unwrap();
+    match &ret[0] {
+        Payload::Select { labels, rows } => {
+            assert_eq!(labels[0], "id");
+            assert_eq!(rows[0].len(), 1);
+            assert_eq!(rows[1].len(), 1);
+            assert_eq!(rows[0][0], Value::I64(1));
+            assert_eq!(rows[1][0], Value::I64(11));
+        }
+        _ => unreachable!(),
+    }
+
+    exec!(glue "DROP TABLE dumdum;");
+}
