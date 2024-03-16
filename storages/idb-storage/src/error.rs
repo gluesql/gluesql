5d010aaac05af150c187dfd33c7b154b97cf8ad1
Taehoon Moon <taehoon.moon@outlook.com>
2024-03-15 22:46:15 -1000

    Simplify error conversion codes in IdbStorage, (#1465)
    Add StoreReqIntoFuture trait impl to flatten two err_into into a single into_future call.

--- storages/idb-storage/src/error.rs
+++ storages/idb-storage/src/error.rs
@@ -1,6 +1,8 @@
 use {
+    async_trait::async_trait,
     core::fmt::Display,
     gluesql_core::error::{Error, Result},
+    std::{future::IntoFuture, result::Result as StdResult},
 };
 
 pub trait ErrInto<T> {
@@ -10,5 +12,20 @@ pub trait ErrInto<T> {
 impl<T, E: Display> ErrInto<T> for Result<T, E> {
     fn err_into(self) -> Result<T> {
         self.map_err(|e| Error::StorageMsg(e.to_string()))
+    }
+}
+
+#[async_trait(?Send)]
+pub trait StoreReqIntoFuture<T> {
+    async fn into_future(self) -> Result<T>;
+}
+
+#[async_trait(?Send)]
+impl<F, T, E: Display> StoreReqIntoFuture<T> for Result<F, E>
+where
+    F: IntoFuture<Output = StdResult<T, E>>,
+{
+    async fn into_future(self) -> Result<T> {
+        self.err_into()?.await.err_into()
     }
 }
