c1a66717c29f071169bfe4524ec09b1f44150542
Taehoon Moon <taehoon.moon@outlook.com>
2024-04-13 15:51:47 +0900

    Add COMMENT field to GLUE_TABLE_COLUMNS dictionary table

--- test-suite/src/dictionary.rs
+++ test-suite/src/dictionary.rs
@@ -26,7 +26,7 @@ test_case!(dictionary, {
         .await;
     g.test("SHOW TABLES", tables(vec!["Foo"])).await;
 
-    g.run("CREATE TABLE Zoo (id INTEGER PRIMARY KEY);").await;
+    g.run("CREATE TABLE Zoo (id INTEGER PRIMARY KEY COMMENT 'hello');").await;
     g.run("CREATE TABLE Bar (id INTEGER UNIQUE, name TEXT NOT NULL DEFAULT 'NONE');")
         .await;
 
@@ -63,14 +63,14 @@ test_case!(dictionary, {
     g.test(
         "SELECT * FROM GLUE_TABLE_COLUMNS",
         Ok(select!(
-            TABLE_NAME       | COLUMN_NAME      | COLUMN_ID | NULLABLE | KEY                      | DEFAULT;
-            Str              | Str              | I64       | Bool     | Str                      | Str;
-            "Bar".to_owned()   "id".to_owned()    1           true       "UNIQUE".to_owned()        "".to_owned();
-            "Bar".to_owned()   "name".to_owned()  2           false      "".to_owned()              "'NONE'".to_owned();
-            "Foo".to_owned()   "id".to_owned()    1           true       "".to_owned()              "".to_owned();
-            "Foo".to_owned()   "name".to_owned()  2           true       "".to_owned()              "".to_owned();
-            "Foo".to_owned()   "type".to_owned()  3           true       "".to_owned()              "".to_owned();
-            "Zoo".to_owned()   "id".to_owned()    1           false      "PRIMARY KEY".to_owned()   "".to_owned()
+            TABLE_NAME       | COLUMN_NAME      | COLUMN_ID | NULLABLE | KEY                      | DEFAULT              | COMMENT;
+            Str              | Str              | I64       | Bool     | Str                      | Str                  | Str;
+            "Bar".to_owned()   "id".to_owned()    1           true       "UNIQUE".to_owned()        "".to_owned()          "".to_owned();
+            "Bar".to_owned()   "name".to_owned()  2           false      "".to_owned()              "'NONE'".to_owned()    "".to_owned();
+            "Foo".to_owned()   "id".to_owned()    1           true       "".to_owned()              "".to_owned()          "".to_owned();
+            "Foo".to_owned()   "name".to_owned()  2           true       "".to_owned()              "".to_owned()          "".to_owned();
+            "Foo".to_owned()   "type".to_owned()  3           true       "".to_owned()              "".to_owned()          "".to_owned();
+            "Zoo".to_owned()   "id".to_owned()    1           false      "PRIMARY KEY".to_owned()   "".to_owned()          "hello".to_owned() 
         ))
     ).await;
 });
