90022ab0ddccc5c8040b5e7155567f7bd044045d
henry <devgony@gmail.com>
2024-06-15 23:00:49 +0900

    build: setup first test

--- pkg/dart/integration_test/simple_test.dart
+++ pkg/dart/integration_test/simple_test.dart
@@ -1,13 +1,25 @@
+import 'package:dart/src/rust/api/payload.dart';
+import 'package:dart/src/rust/api/simple.dart';
+import 'package:dart/src/rust/api/value.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dart/main.dart';
 import 'package:dart/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   setUpAll(() async => await RustLib.init());
-  testWidgets('Can call rust function', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
+  test("select", () async {
+    final result = await execute(sql: "SELECT 1;");
+    expect(
+      result.first,
+      const DartPayload.select(
+        labels: [
+          "1",
+        ],
+        rows: [
+          [DartValue.i64(1)],
+        ],
+      ),
+    );
   });
 }
