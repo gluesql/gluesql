0bd9297731e12bb6468cf0cf5b7937968aafccaa
henry <devgony@gmail.com>
2024-06-19 09:29:52 +0900

    fix: rename remainings on python 2

--- pkg/python/src/storages.rs
+++ pkg/python/src/storages.rs
@@ -1,9 +1,9 @@
 use {
     gluesql_json_storage::JsonStorage,
     gluesql_memory_storage::MemoryStorage,
+    gluesql_shared_memory_storage::SharedMemoryStorage,
     gluesql_sled_storage::{sled, SledStorage},
     pyo3::{prelude::*, types::PyString},
-    shared_memory_storage::SharedMemoryStorage,
     std::path::PathBuf,
 };
 
