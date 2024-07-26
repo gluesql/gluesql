b73736b50eb742fd6b83ebfe97ddb0fff09b0ff3
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-07-26 14:34:10 +0200

    Adressed new clippy complaints

--- storages/parquet-storage/src/value.rs
+++ storages/parquet-storage/src/value.rs
@@ -2,7 +2,7 @@ use {
     crate::error::{OptionExt, ParquetStorageError, ResultExt},
     byteorder::{BigEndian, ByteOrder},
     gluesql_core::{
-        chrono::{Duration, NaiveDate, NaiveDateTime, NaiveTime},
+        chrono::{Duration, NaiveDate, DateTime, NaiveTime},
         data::{Schema, Value},
         prelude::{DataType, Error, Result},
     },
@@ -131,12 +131,12 @@ impl ParquetField {
                 Ok(Value::List(list))
             }
             Field::TimestampMillis(v) => Ok(Value::Timestamp(
-                NaiveDateTime::from_timestamp_millis(*v)
+                DateTime::from_timestamp_millis(*v).map(|dt| dt.naive_utc())
                     .map_storage_err("Field::TimestampMillis to Value::Timestamp fail")?,
             )),
 
             Field::TimestampMicros(v) => Ok(Value::Timestamp(
-                NaiveDateTime::from_timestamp_micros(*v)
+                DateTime::from_timestamp_micros(*v).map(|dt| dt.naive_utc())
                     .map_storage_err("Field::TimestampMicros to Value::Timestamp fail")?,
             )),
             Field::Decimal(v) => {
