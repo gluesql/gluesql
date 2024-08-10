699df57744f4cddcbebacd183a15149cca0e51d3
Taehoon Moon <taehoon.moon@outlook.com>
2024-08-10 17:55:38 +0900

    Add pull & push integration test - update gh actions

---
+++ storages/git-storage/tests/pull_and_push.rs
@@ -0,0 +1,55 @@
+#![cfg(feature = "test-git-remote")]
+
+use {
+    gluesql_core::prelude::Glue,
+    gluesql_git_storage::{GitStorage, StorageType},
+    std::{env, fs::remove_dir_all, process::Command},
+    uuid::Uuid,
+};
+
+#[tokio::test]
+async fn pull_and_push() {
+    let remote =
+        env::var("GIT_REMOTE").unwrap_or("git@github.com:gluesql/git-storage-test.git".to_owned());
+    let path = "./tmp/git-storage-test/";
+    let _ = remove_dir_all(path);
+
+    Command::new("git")
+        .current_dir("./tmp")
+        .arg("clone")
+        .arg(&remote)
+        .output()
+        .unwrap();
+
+    let branch = format!("test-{}", Uuid::now_v7());
+    Command::new("git")
+        .current_dir(path)
+        .arg("checkout")
+        .arg("-b")
+        .arg(&branch)
+        .output()
+        .unwrap();
+
+    let mut storage = GitStorage::open(path, StorageType::Json).unwrap();
+    storage.set_branch(branch.clone());
+    storage.pull().unwrap();
+
+    let mut glue = Glue::new(storage);
+    glue.execute("CREATE TABLE Foo (id INTEGER);")
+        .await
+        .unwrap();
+    glue.execute("INSERT INTO Foo VALUES (1), (2), (3);")
+        .await
+        .unwrap();
+
+    glue.storage.push().unwrap();
+
+    Command::new("git")
+        .current_dir(path)
+        .arg("push")
+        .arg(remote)
+        .arg("-d")
+        .arg(branch)
+        .output()
+        .unwrap();
+}
