96de1c9b8c4d199a00c414188fb2001431fd3415
Taehoon Moon <taehoon.moon@outlook.com>
2024-08-25 14:24:56 +0900

    Fix GitStorage GitHub Action to run in forks (#1557)

--- storages/git-storage/tests/pull_and_push.rs
+++ storages/git-storage/tests/pull_and_push.rs
@@ -13,14 +13,13 @@ use {
 
 #[tokio::test]
 async fn pull_and_push() {
-    let remote =
-        env::var("GIT_REMOTE").unwrap_or("git@github.com:gluesql/git-storage-test.git".to_owned());
-    let path = "./tmp/git-storage-test/";
+    let remote = env::var("GIT_REMOTE").unwrap_or("git@github.com:gluesql/gluesql.git".to_owned());
+    let path = ".tmp/gluesql/";
     let _ = remove_dir_all(path);
     let _ = create_dir(".tmp");
 
     Command::new("git")
-        .current_dir("./tmp")
+        .current_dir(".tmp")
         .arg("clone")
         .arg(&remote)
         .output()
