e044240a9aca17c4cd499c707f9a8b87f0d5d10b
Taehoon Moon <taehoon.moon@outlook.com>
2024-08-25 13:10:23 +0900

    Update pull_and_push test

--- storages/git-storage/tests/pull_and_push.rs
+++ storages/git-storage/tests/pull_and_push.rs
@@ -14,6 +14,7 @@ use {
 #[tokio::test]
 async fn pull_and_push() {
     let remote = env::var("GIT_REMOTE").unwrap_or("git@github.com:gluesql/gluesql.git".to_owned());
+    println!("remote: {remote}");
     let path = "./tmp/gluesql/";
     let _ = remove_dir_all(path);
     let _ = create_dir(".tmp");
