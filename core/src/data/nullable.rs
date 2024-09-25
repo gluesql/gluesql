a5e3c9b68a06c4cb7c711b22682f5a60399cae48
Luca <cappelletti.luca94@gmail.com>
2024-09-25 09:52:25 +0200

    Made is_null a test only method

--- core/src/data/nullable.rs
+++ core/src/data/nullable.rs
@@ -9,21 +9,6 @@ use std::ops::BitOr;
 /// # Implementations details
 /// This enum differs from the `Option` enum in that it does not represent
 /// a `Option::None` value, but rather a `Nullable::Null` SQL `NULL` value.
-///
-/// # Examples
-///
-/// ```
-/// use gluesql_core::data::Nullable;
-///
-/// let null: Nullable<i32> = Nullable::Null;
-///
-/// assert!(null.is_null());
-///
-/// let entry: Nullable<i32> = 10.into();
-///
-/// assert!(!entry.is_null());
-/// ```
-///
 pub enum Nullable<T> {
     /// Represents an SQL `NULL` value.
     Null,
@@ -31,29 +16,15 @@ pub enum Nullable<T> {
     Entry(T),
 }
 
+#[cfg(test)]
 impl<T> Nullable<T> {
     /// Returns `true` if the value is `Nullable::Null`.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use gluesql_core::data::Nullable;
-    ///
-    /// let null: Nullable<i32> = Nullable::Null;
-    ///
-    /// assert!(null.is_null());
-    ///
-    /// let entry: Nullable<i32> = 10.into();
-    ///
-    /// assert!(!entry.is_null());
-    /// ```
-    pub fn is_null(&self) -> bool {
+    pub(crate) fn is_null(&self) -> bool {
         matches!(self, Nullable::Null)
     }
 
-    #[cfg(test)]
     /// Returns the value if it is an `Entry`, otherwise panics.
-    pub fn unwrap(self) -> T {
+    pub(crate) fn unwrap(self) -> T {
         match self {
             Nullable::Null => panic!("called `Nullable::unwrap()` on a `Null` value"),
             Nullable::Entry(value) => value,
@@ -83,23 +54,6 @@ impl<T: BitOr<Output = T>> BitOr for Nullable<T> {
     type Output = Self;
 
     /// Bitwise ORs two nullable values, returning NULL if either value is NULL.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use gluesql_core::data::Nullable;
-    ///
-    /// let lhs: Nullable<i32> = Nullable::Null;
-    /// let rhs: Nullable<i32> = 10.into();
-    ///
-    /// assert!((lhs | rhs).is_null());
-    /// assert!((rhs | lhs).is_null());
-    ///
-    /// let lhs: Nullable<i32> = 20.into();
-    ///
-    /// assert!(!(lhs | rhs).is_null());
-    /// ```
-    ///
     fn bitor(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
             (Nullable::Null, _) | (_, Nullable::Null) => Nullable::Null,
