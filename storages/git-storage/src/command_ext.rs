c8b79cc154fbb9f46516dd4e2270da893b3208c6
henry <devgony@gmail.com>
2024-12-07 16:32:06 +0900

    fix: return both out and err on CommandExt

--- storages/git-storage/src/command_ext.rs
+++ storages/git-storage/src/command_ext.rs
@@ -13,11 +13,63 @@ impl CommandExt for Command {
         let output = self.output().map_storage_err()?;
 
         if !output.status.success() {
-            return Err(Error::StorageMsg(
-                String::from_utf8_lossy(&output.stderr).to_string(),
-            ));
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            let stderr = String::from_utf8_lossy(&output.stderr);
+
+            let out_and_err = [
+                (!stdout.is_empty()).then(|| format!("[stdout] {}", stdout)),
+                (!stderr.is_empty()).then(|| format!("[stderr] {}", stderr)),
+            ]
+            .iter()
+            .filter_map(|x| x.clone())
+            .collect::<Vec<_>>()
+            .join("\n");
+
+            return Err(Error::StorageMsg(out_and_err));
         }
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {crate::CommandExt, std::process::Command};
+
+    #[test]
+    fn test_command_ext() {
+        let path = "tmp/command_ext_test";
+
+        Command::new("mkdir").arg("-p").arg(path).execute().unwrap();
+        Command::new("git")
+            .current_dir(path)
+            .arg("init")
+            .execute()
+            .unwrap();
+
+        let executed = Command::new("git")
+            .current_dir(path)
+            .arg("commit")
+            .arg("-m")
+            .arg("test")
+            .execute();
+        assert!(executed.is_err());
+        assert_eq!(
+        executed.unwrap_err().to_string(),
+        "storage: [stdout] On branch main\n\nInitial commit\n\nnothing to commit (create/copy files and use \"git add\" to track)\n"
+    );
+
+        let executed = Command::new("git")
+            .current_dir(path)
+            .arg("commit")
+            .arg("-m")
+            .arg("test")
+            .arg("--amend")
+            .execute();
+        assert!(executed.is_err());
+        assert_eq!(
+            executed.unwrap_err().to_string(),
+            "storage: [stderr] fatal: You have nothing to amend.\n"
+        );
     }
 }
