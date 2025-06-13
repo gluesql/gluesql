8eaf8e65ba9a6cd37ba3ffa7eae9f0079af6e4de
Taehoon Moon <taehoon.moon@outlook.com>
2025-06-13 18:45:13 +0900

    Run cargo fmt

--- test-suite/src/function/generate_uuid.rs
+++ test-suite/src/function/generate_uuid.rs
@@ -6,17 +6,15 @@ use {
 test_case!(generate_uuid, {
     let g = get_tester!();
 
-    let test_cases = [
-        (
-            "SELECT generate_uuid(0) as uuid",
-            Err(TranslateError::FunctionArgsLengthNotMatching {
-                name: "GENERATE_UUID".to_owned(),
-                expected: 0,
-                found: 1,
-            }
-            .into()),
-        ),
-    ];
+    let test_cases = [(
+        "SELECT generate_uuid(0) as uuid",
+        Err(TranslateError::FunctionArgsLengthNotMatching {
+            name: "GENERATE_UUID".to_owned(),
+            expected: 0,
+            found: 1,
+        }
+        .into()),
+    )];
 
     for (sql, expected) in test_cases {
         g.test(sql, expected).await;
@@ -24,6 +22,8 @@ test_case!(generate_uuid, {
 
     g.count("SELECT GENERATE_UUID()", 1).await;
     g.count("VALUES (GENERATE_UUID())", 1).await;
-    g.type_match("SELECT GENERATE_UUID() as uuid", &[DataType::Uuid]).await;
-    g.type_match("VALUES (GENERATE_UUID())", &[DataType::Uuid]).await;
+    g.type_match("SELECT GENERATE_UUID() as uuid", &[DataType::Uuid])
+        .await;
+    g.type_match("VALUES (GENERATE_UUID())", &[DataType::Uuid])
+        .await;
 });
