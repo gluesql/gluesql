01b584cbd0bb76ed17036af164f693ba3180cf00
Taehoon Moon <taehoon.moon@outlook.com>
2025-06-16 13:34:08 +0900

    Update GENERATE_UUID tests (#1633)
    update GENERATE_UUID test to use VALUES/SELECT directly
    verify UUID type without temp table

--- test-suite/src/function/generate_uuid.rs
+++ test-suite/src/function/generate_uuid.rs
@@ -1,39 +1,29 @@
 use {
     crate::*,
-    gluesql_core::{ast::DataType, error::TranslateError, prelude::Payload},
+    gluesql_core::{ast::DataType, error::TranslateError},
 };
 
 test_case!(generate_uuid, {
     let g = get_tester!();
 
-    let test_cases = [
-        (
-            "CREATE TABLE SingleItem (id UUID DEFAULT GENERATE_UUID())",
-            Ok(Payload::Create),
-        ),
-        (
-            r#"INSERT INTO SingleItem VALUES (GENERATE_UUID())"#,
-            Ok(Payload::Insert(1)),
-        ),
-        (
-            "SELECT generate_uuid(0) as uuid FROM SingleItem",
-            Err(TranslateError::FunctionArgsLengthNotMatching {
-                name: "GENERATE_UUID".to_owned(),
-                expected: 0,
-                found: 1,
-            }
-            .into()),
-        ),
-    ];
+    let test_cases = [(
+        "SELECT generate_uuid(0) as uuid",
+        Err(TranslateError::FunctionArgsLengthNotMatching {
+            name: "GENERATE_UUID".to_owned(),
+            expected: 0,
+            found: 1,
+        }
+        .into()),
+    )];
 
     for (sql, expected) in test_cases {
         g.test(sql, expected).await;
     }
 
-    g.count("SELECT GENERATE_UUID() FROM SingleItem", 1).await;
-    g.type_match(
-        "SELECT GENERATE_UUID() as uuid FROM SingleItem",
-        &[DataType::Uuid],
-    )
-    .await;
+    g.count("SELECT GENERATE_UUID()", 1).await;
+    g.count("VALUES (GENERATE_UUID())", 1).await;
+    g.type_match("SELECT GENERATE_UUID() as uuid", &[DataType::Uuid])
+        .await;
+    g.type_match("VALUES (GENERATE_UUID())", &[DataType::Uuid])
+        .await;
 });
