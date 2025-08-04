4258040fe80241f5b9c5d025bac8e965f06f57d6
Lee Dogeon <dev.moreal@gmail.com>
2025-08-04 17:03:47 +0900

    Complement test-suite for operations related with NULL

--- test-suite/src/data_type/null.rs
+++ test-suite/src/data_type/null.rs
@@ -4,58 +4,30 @@ test_case!(null, {
     let g = get_tester!();
 
     g.named_test(
-        "NULL = NULL should return NULL",
-        "SELECT NULL = NULL as res;",
-        Ok(select_with_null!(res; Null)),
-    )
-    .await;
-
-    g.named_test(
-        "NULL IS NULL should return true",
+        "'NULL IS NULL' should return true",
         "SELECT NULL IS NULL as res;",
-        Ok(select!(res Bool; true)),
+        Ok(select!(res; Bool; true)),
     )
     .await;
 
-    g.named_test(
-        "NOT NULL should return NULL",
-        "SELECT NOT NULL as res;",
-        Ok(select_with_null!(res; Null)),
-    )
-    .await;
+    // TODO: Add "|", "^", "&&" to testcase when it is implemented
+    for binary_op in [
+        "=", ">", "<", ">=", "<=", "<>", "&", "||", "<<", ">>", "+", "-", "*", "/", "%",
+    ] {
+        g.named_test(
+            &format!("'NULL {binary_op} NULL' should return NULL"),
+            &format!("SELECT NULL {binary_op} NULL as res;"),
+            Ok(select_with_null!(res; Null)),
+        )
+        .await;
+    }
 
-    g.named_test(
-        "NULL > NULL should return NULL",
-        "SELECT NULL > NULL as res;",
-        Ok(select_with_null!(res; Null)),
-    )
-    .await;
-
-    g.named_test(
-        "NULL < NULL should return NULL",
-        "SELECT NULL < NULL as res;",
-        Ok(select_with_null!(res; Null)),
-    )
-    .await;
-
-    g.named_test(
-        "NULL >= NULL should return NULL",
-        "SELECT NULL >= NULL as res;",
-        Ok(select_with_null!(res; Null)),
-    )
-    .await;
-
-    g.named_test(
-        "NULL <= NULL should return NULL",
-        "SELECT NULL <= NULL as res;",
-        Ok(select_with_null!(res; Null)),
-    )
-    .await;
-
-    g.named_test(
-        "NULL <> NULL should return NULL",
-        "SELECT NULL <> NULL as res;",
-        Ok(select_with_null!(res; Null)),
-    )
-    .await;
+    for unary_op in ["-", "+", "NOT"] {
+        g.named_test(
+            &format!("'{unary_op} NULL' should return NULL"),
+            &format!("SELECT {unary_op} NULL as res;"),
+            Ok(select_with_null!(res; Null)),
+        )
+        .await;
+    }
 });
