9ff0359e2126fea9cf4a523b9a9b2246a7abca99
Taehoon Moon <taehoon.moon@outlook.com>
2025-08-04 21:54:58 +0900

    Remove Send Sync bound from Metadata trait (#1701)

--- core/src/store/metadata.rs
+++ core/src/store/metadata.rs
@@ -8,7 +8,7 @@ type ObjectName = String;
 pub type MetaIter = Box<dyn Iterator<Item = Result<(ObjectName, HashMap<String, Value>)>>>;
 
 #[async_trait]
-pub trait Metadata: Send + Sync {
+pub trait Metadata {
     async fn scan_table_meta(&self) -> Result<MetaIter> {
         Ok(Box::new(empty()))
     }
