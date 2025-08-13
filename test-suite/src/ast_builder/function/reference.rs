7db06f6be3f8ece975facba20eda2142ebc4bbed
minho jang <mjang6988@gmail.com>
2025-08-13 18:38:38 +0900

    Move ast builder tests in other to reference

--- test-suite/src/ast_builder/function/reference.rs
+++ test-suite/src/ast_builder/function/reference.rs
@@ -2,8 +2,11 @@ mod current_date;
 mod current_time;
 mod current_timestamp;
 mod generate_uuid;
+mod ifnull;
+mod nullif;
+mod coalesce;
 
 pub use {
     current_date::current_date, current_time::current_time, current_timestamp::current_timestamp,
-    generate_uuid::generate_uuid,
+    generate_uuid::generate_uuid, ifnull::ifnull, nullif::nullif, coalesce::coalesce
 };
