85e3e5675be9ce7386fbe41312ea672bb86c4d1f
chaerrypick01 <chaerrypick01@gmail.com>
2025-09-22 01:20:46 +0900

    feat: implement MilvusStorage initialization and configuration

---
+++ storages/milvus-storage/src/error.rs
@@ -0,0 +1,11 @@
+use gluesql_core::error::Error;
+
+pub trait ResultExt<T, E: ToString> {
+    fn map_storage_err(self) -> Result<T, Error>;
+}
+
+impl<T, E: ToString> ResultExt<T, E> for std::result::Result<T, E> {
+    fn map_storage_err(self) -> Result<T, Error> {
+        self.map_err(|e| e.to_string()).map_err(Error::StorageMsg)
+    }
+}
---
+++ storages/milvus-storage/src/lib.rs
@@ -0,0 +1,77 @@
+mod error;
+mod store;
+
+use {
+    error::ResultExt,
+    gluesql_core::error::{Error, Result},
+    milvus::{
+        client::{Client, ClientBuilder},
+    },
+};
+
+pub struct MilvusStorageConfig {
+    url: String,
+    username: Option<String>,
+    password: Option<String>,
+    db_name: Option<String>,
+}
+
+impl MilvusStorageConfig {
+    pub fn new(url: impl Into<String>) -> Self {
+        Self {
+            url: url.into(),
+            username: None,
+            password: None,
+            db_name: None,
+        }
+    }
+
+    pub fn with_basic_auth(
+        mut self,
+        username: impl Into<String>,
+        password: impl Into<String>,
+    ) -> Self {
+        self.username = Some(username.into());
+        self.password = Some(password.into());
+        self
+    }
+
+    pub fn with_db_name(mut self, db_name: impl Into<String>) -> Self {
+        self.db_name = Some(db_name.into());
+        self
+    }
+
+    fn into_parts(self) -> (String, Option<String>, Option<String>, Option<String>) {
+        (self.url, self.username, self.password, self.db_name)
+    }
+}
+
+pub struct MilvusStorage {
+    client: Client,
+    db_name: Option<String>,
+}
+
+impl MilvusStorage {
+    pub async fn new(config: MilvusStorageConfig) -> Result<Self> {
+        let (url, username, password, db_name) = config.into_parts();
+
+        let mut builder = ClientBuilder::new(url);
+
+        if let Some(username) = username.as_ref() {
+            builder = builder.username(username);
+
+            if let Some(password) = password.as_ref() {
+                builder = builder.password(password);
+            }
+        }
+
+        let client = builder.build().await.map_storage_err()?;
+
+        Ok(
+            Self {
+                client,
+                db_name,
+            }
+        )
+    }
+}
