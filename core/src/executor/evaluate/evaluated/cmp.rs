4ce53b6fdb74217d973ef83aaa07baa427b9caf0
Taehoon Moon <taehoon.moon@outlook.com>
2025-12-07 15:44:05 +0900

    Split value_cmp_with_literal into number and text variants
    - value_cmp_with_number takes &BigDecimal directly
    - value_cmp_with_text takes &str directly
    - Removes unnecessary Evaluated matching in comparison functions

--- core/src/executor/evaluate/evaluated/cmp.rs
+++ core/src/executor/evaluate/evaluated/cmp.rs
@@ -5,7 +5,7 @@ use {
         value::{parse_date, parse_time, parse_timestamp, parse_uuid},
     },
     bigdecimal::BigDecimal,
-    std::{borrow::Cow, cmp::Ordering, net::IpAddr, str::FromStr},
+    std::{cmp::Ordering, net::IpAddr, str::FromStr},
 };
 
 impl<'a> Evaluated<'a> {
@@ -13,12 +13,14 @@ impl<'a> Evaluated<'a> {
         match (self, other) {
             (Evaluated::Number(l), Evaluated::Number(r)) => Some(l.cmp(r)),
             (Evaluated::Text(l), Evaluated::Text(r)) => Some(l.cmp(r)),
-            (left @ (Evaluated::Number(_) | Evaluated::Text(_)), Evaluated::Value(right)) => {
-                value_cmp_with_literal(right, left).map(Ordering::reverse)
+            (Evaluated::Number(l), Evaluated::Value(r)) => {
+                value_cmp_with_number(r, l).map(Ordering::reverse)
             }
-            (Evaluated::Value(left), right @ (Evaluated::Number(_) | Evaluated::Text(_))) => {
-                value_cmp_with_literal(left, right)
+            (Evaluated::Text(l), Evaluated::Value(r)) => {
+                value_cmp_with_text(r, l).map(Ordering::reverse)
             }
+            (Evaluated::Value(l), Evaluated::Number(r)) => value_cmp_with_number(l, r),
+            (Evaluated::Value(l), Evaluated::Text(r)) => value_cmp_with_text(l, r),
             (Evaluated::Value(left), Evaluated::Value(right)) => left.evaluate_cmp(right),
             (Evaluated::Text(l), Evaluated::StrSlice { source, range }) => {
                 Some(l.as_ref().cmp(&source[range.clone()]))
@@ -28,15 +30,11 @@ impl<'a> Evaluated<'a> {
             }
             (Evaluated::Number(_), Evaluated::Text(_) | Evaluated::StrSlice { .. })
             | (Evaluated::Text(_) | Evaluated::StrSlice { .. }, Evaluated::Number(_)) => None,
-            (Evaluated::Value(left), Evaluated::StrSlice { source, range }) => {
-                let slice = Evaluated::Text(Cow::Borrowed(&source[range.clone()]));
-
-                value_cmp_with_literal(left, &slice)
+            (Evaluated::Value(l), Evaluated::StrSlice { source, range }) => {
+                value_cmp_with_text(l, &source[range.clone()])
             }
-            (Evaluated::StrSlice { source, range }, Evaluated::Value(right)) => {
-                let slice = Evaluated::Text(Cow::Borrowed(&source[range.clone()]));
-
-                value_cmp_with_literal(right, &slice).map(Ordering::reverse)
+            (Evaluated::StrSlice { source, range }, Evaluated::Value(r)) => {
+                value_cmp_with_text(r, &source[range.clone()]).map(Ordering::reverse)
             }
             (
                 Evaluated::StrSlice {
@@ -52,33 +50,27 @@ impl<'a> Evaluated<'a> {
     }
 }
 
-fn value_cmp_with_literal(value: &Value, literal: &Evaluated<'_>) -> Option<Ordering> {
-    match (value, literal) {
-        (Value::I8(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_i8()?),
-        (Value::I16(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_i16()?),
-        (Value::I32(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_i32()?),
-        (Value::I64(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_i64()?),
-        (Value::I128(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_i128()?),
-        (Value::U8(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_u8()?),
-        (Value::U16(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_u16()?),
-        (Value::U32(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_u32()?),
-        (Value::U64(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_u64()?),
-        (Value::U128(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_u128()?),
-        (Value::F32(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_f32()?),
-        (Value::F64(l), Evaluated::Number(r)) => l.partial_cmp(&r.to_f64()?),
-        (Value::Decimal(l), Evaluated::Number(r)) => {
-            BigDecimal::new(l.mantissa().into(), i64::from(l.scale())).partial_cmp(r)
+fn value_cmp_with_number(value: &Value, number: &BigDecimal) -> Option<Ordering> {
+    match value {
+        Value::I8(l) => l.partial_cmp(&number.to_i8()?),
+        Value::I16(l) => l.partial_cmp(&number.to_i16()?),
+        Value::I32(l) => l.partial_cmp(&number.to_i32()?),
+        Value::I64(l) => l.partial_cmp(&number.to_i64()?),
+        Value::I128(l) => l.partial_cmp(&number.to_i128()?),
+        Value::U8(l) => l.partial_cmp(&number.to_u8()?),
+        Value::U16(l) => l.partial_cmp(&number.to_u16()?),
+        Value::U32(l) => l.partial_cmp(&number.to_u32()?),
+        Value::U64(l) => l.partial_cmp(&number.to_u64()?),
+        Value::U128(l) => l.partial_cmp(&number.to_u128()?),
+        Value::F32(l) => l.partial_cmp(&number.to_f32()?),
+        Value::F64(l) => l.partial_cmp(&number.to_f64()?),
+        Value::Decimal(l) => {
+            BigDecimal::new(l.mantissa().into(), i64::from(l.scale())).partial_cmp(number)
         }
-        (Value::Str(l), Evaluated::Text(r)) => Some(l.as_str().cmp(r)),
-        (Value::Date(l), Evaluated::Text(r)) => l.partial_cmp(&parse_date(r)?),
-        (Value::Timestamp(l), Evaluated::Text(r)) => l.partial_cmp(&parse_timestamp(r)?),
-        (Value::Time(l), Evaluated::Text(r)) => l.partial_cmp(&parse_time(r)?),
-        (Value::Uuid(l), Evaluated::Text(r)) => l.partial_cmp(&parse_uuid(r).ok()?),
-        (Value::Inet(l), Evaluated::Text(r)) => l.partial_cmp(&IpAddr::from_str(r).ok()?),
-        (Value::Inet(l), Evaluated::Number(r)) => {
-            if let Some(x) = r.to_u32() {
+        Value::Inet(l) => {
+            if let Some(x) = number.to_u32() {
                 l.partial_cmp(&IpAddr::V4(x.into()))
-            } else if let Some(x) = r.to_u128() {
+            } else if let Some(x) = number.to_u128() {
                 l.partial_cmp(&IpAddr::V6(x.into()))
             } else {
                 None
@@ -88,10 +80,22 @@ fn value_cmp_with_literal(value: &Value, literal: &Evaluated<'_>) -> Option<Orde
     }
 }
 
+fn value_cmp_with_text(value: &Value, text: &str) -> Option<Ordering> {
+    match value {
+        Value::Str(l) => Some(l.as_str().cmp(text)),
+        Value::Date(l) => l.partial_cmp(&parse_date(text)?),
+        Value::Timestamp(l) => l.partial_cmp(&parse_timestamp(text)?),
+        Value::Time(l) => l.partial_cmp(&parse_time(text)?),
+        Value::Uuid(l) => l.partial_cmp(&parse_uuid(text).ok()?),
+        Value::Inet(l) => l.partial_cmp(&IpAddr::from_str(text).ok()?),
+        _ => None,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use {
-        super::{Evaluated, value_cmp_with_literal},
+        super::{Evaluated, value_cmp_with_number, value_cmp_with_text},
         crate::data::{
             Value,
             value::{parse_date, parse_time, parse_timestamp, parse_uuid},
@@ -102,78 +106,124 @@ mod tests {
     };
 
     #[test]
-    fn value_cmp_with_literal_regression() {
-        let num = |n| Evaluated::Number(Cow::Owned(BigDecimal::from(n)));
-        let text = |v: &str| Evaluated::Text(Cow::Owned(v.to_owned()));
+    fn test_value_cmp_with_number() {
+        let num = |n: i32| BigDecimal::from(n);
 
-        #[allow(clippy::similar_names)]
-        let assert_cmp = |value: Value, literal: Evaluated<'_>, expected| {
-            assert_eq!(value_cmp_with_literal(&value, &literal), expected);
-        };
-
-        assert_cmp(Value::I8(1), num(1), Some(Ordering::Equal));
-        assert_cmp(Value::I16(1), num(2), Some(Ordering::Less));
-        assert_cmp(Value::I32(10), num(3), Some(Ordering::Greater));
-        assert_cmp(Value::I64(10), num(10), Some(Ordering::Equal));
-        assert_cmp(Value::I128(10), num(10), Some(Ordering::Equal));
-        assert_cmp(Value::U8(1), num(1), Some(Ordering::Equal));
-        assert_cmp(Value::U16(1), num(2), Some(Ordering::Less));
-        assert_cmp(Value::U32(10), num(3), Some(Ordering::Greater));
-        assert_cmp(Value::U64(10), num(10), Some(Ordering::Equal));
-        assert_cmp(Value::U128(10), num(10), Some(Ordering::Equal));
-        assert_cmp(Value::F32(10.0), num(10), Some(Ordering::Equal));
-        assert_cmp(Value::F64(10.0), num(10), Some(Ordering::Equal));
-        assert_cmp(
-            Value::Decimal(Decimal::new(215, 2)),
-            num(3),
+        assert_eq!(
+            value_cmp_with_number(&Value::I8(1), &num(1)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::I16(1), &num(2)),
+            Some(Ordering::Less)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::I32(10), &num(3)),
+            Some(Ordering::Greater)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::I64(10), &num(10)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::I128(10), &num(10)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::U8(1), &num(1)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::U16(1), &num(2)),
+            Some(Ordering::Less)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::U32(10), &num(3)),
+            Some(Ordering::Greater)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::U64(10), &num(10)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::U128(10), &num(10)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::F32(10.0), &num(10)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::F64(10.0), &num(10)),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            value_cmp_with_number(&Value::Decimal(Decimal::new(215, 2)), &num(3)),
             Some(Ordering::Less),
         );
-        assert_cmp(
-            Value::Str("Hello".to_owned()),
-            text("Hello"),
+        assert_eq!(
+            value_cmp_with_number(
+                &Value::Inet(IpAddr::from_str("255.255.255.255").unwrap()),
+                &BigDecimal::new(4_294_967_295_u32.into(), 0),
+            ),
             Some(Ordering::Equal),
         );
-        assert_cmp(
-            Value::Date(parse_date("2021-11-21").unwrap()),
-            text("2021-11-21"),
+        assert_eq!(
+            value_cmp_with_number(
+                &Value::Inet(IpAddr::from_str("::2:4cb0:16ea").unwrap()),
+                &BigDecimal::new(9_876_543_210_u128.into(), 0),
+            ),
             Some(Ordering::Equal),
         );
-        assert_cmp(
-            Value::Timestamp(parse_timestamp("2021-11-21T10:00:00Z").unwrap()),
-            text("2021-11-21T10:00:00Z"),
-            Some(Ordering::Equal),
-        );
-        assert_cmp(
-            Value::Time(parse_time("10:00:00").unwrap()),
-            text("10:00:00"),
-            Some(Ordering::Equal),
-        );
-        assert_cmp(
-            Value::Uuid(parse_uuid("936DA01F9ABD4d9d80C702AF85C822A8").unwrap()),
-            text("936DA01F9ABD4d9d80C702AF85C822A8"),
-            Some(Ordering::Equal),
-        );
-        assert_cmp(
-            Value::Inet(IpAddr::from_str("215.87.1.1").unwrap()),
-            text("215.87.1.1"),
-            Some(Ordering::Equal),
-        );
-        assert_cmp(
-            Value::Inet(IpAddr::from_str("255.255.255.255").unwrap()),
-            Evaluated::Number(Cow::Owned(BigDecimal::new(4_294_967_295_u32.into(), 0))),
-            Some(Ordering::Equal),
-        );
-        assert_cmp(
-            Value::Inet(IpAddr::from_str("::2:4cb0:16ea").unwrap()),
-            Evaluated::Number(Cow::Owned(BigDecimal::new(9_876_543_210_u128.into(), 0))),
-            Some(Ordering::Equal),
-        );
-        assert_cmp(
-            Value::Inet(IpAddr::from_str("127.0.0.1").unwrap()),
-            num(-1),
+        assert_eq!(
+            value_cmp_with_number(
+                &Value::Inet(IpAddr::from_str("127.0.0.1").unwrap()),
+                &num(-1)
+            ),
             None,
         );
-        assert_cmp(Value::Null, num(1), None);
+        assert_eq!(value_cmp_with_number(&Value::Null, &num(1)), None);
+    }
+
+    #[test]
+    fn test_value_cmp_with_text() {
+        assert_eq!(
+            value_cmp_with_text(&Value::Str("Hello".to_owned()), "Hello"),
+            Some(Ordering::Equal),
+        );
+        assert_eq!(
+            value_cmp_with_text(
+                &Value::Date(parse_date("2021-11-21").unwrap()),
+                "2021-11-21"
+            ),
+            Some(Ordering::Equal),
+        );
+        assert_eq!(
+            value_cmp_with_text(
+                &Value::Timestamp(parse_timestamp("2021-11-21T10:00:00Z").unwrap()),
+                "2021-11-21T10:00:00Z",
+            ),
+            Some(Ordering::Equal),
+        );
+        assert_eq!(
+            value_cmp_with_text(&Value::Time(parse_time("10:00:00").unwrap()), "10:00:00"),
+            Some(Ordering::Equal),
+        );
+        assert_eq!(
+            value_cmp_with_text(
+                &Value::Uuid(parse_uuid("936DA01F9ABD4d9d80C702AF85C822A8").unwrap()),
+                "936DA01F9ABD4d9d80C702AF85C822A8",
+            ),
+            Some(Ordering::Equal),
+        );
+        assert_eq!(
+            value_cmp_with_text(
+                &Value::Inet(IpAddr::from_str("215.87.1.1").unwrap()),
+                "215.87.1.1"
+            ),
+            Some(Ordering::Equal),
+        );
+        assert_eq!(value_cmp_with_text(&Value::Null, "foo"), None);
     }
 
     #[test]
