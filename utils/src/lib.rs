78e80ad2f4f28a23bb9010972fd0069ba6846ee3
Taehoon Moon <taehoon.moon@outlook.com>
2026-06-02 17:55:53 +0900

    Remove unused OrStream utility

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -2,11 +2,7 @@
 
 mod hashmap;
 mod indexmap;
-mod or_stream;
 mod tribool;
 mod vector;
 
-pub use {
-    self::indexmap::IndexMap, hashmap::HashMapExt, or_stream::OrStream, tribool::Tribool,
-    vector::Vector,
-};
+pub use {self::indexmap::IndexMap, hashmap::HashMapExt, tribool::Tribool, vector::Vector};
