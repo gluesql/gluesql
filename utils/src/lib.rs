f9e5e1bf196044118b82b42fa9a4411776ee2d5b
Taehoon Moon <taehoon.moon@outlook.com>
2026-06-21 16:00:16 +0900

    Remove unused gluesql-utils exports (#1929)
    Drop the unused IndexMap and HashMapExt utilities, remove the indexmap dependency from gluesql-utils, and clean up unused utils dependencies from csv/json storage crates.

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,8 +1,6 @@
 #![deny(clippy::str_to_string)]
 
-mod hashmap;
-mod indexmap;
 mod tribool;
 mod vector;
 
-pub use {self::indexmap::IndexMap, hashmap::HashMapExt, tribool::Tribool, vector::Vector};
+pub use {tribool::Tribool, vector::Vector};
