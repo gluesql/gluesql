79cf6db6e6f08c59c7989587857e4e60805f1233
Taehoon Moon <taehoon.moon@outlook.com>
2026-08-02 17:42:53 +0900

    Use query project lookup for select payloads

--- core/src/executor/select.rs
+++ core/src/executor/select.rs
@@ -5,10 +5,7 @@ use {
     },
     crate::{
         data::Value,
-        plan::{
-            DistinctInputPlan, DistinctPlan, LimitInputPlan, LimitPlan, OffsetInputPlan,
-            OffsetPlan, ProjectPlan, ProjectionPlan, QueryPlan,
-        },
+        plan::{ProjectionPlan, QueryPlan},
         result::Result,
         store::GStore,
     },
@@ -35,38 +32,7 @@ pub(super) fn execute<T: GStore>(storage: &T, query: &QueryPlan) -> Result<Paylo
 }
 
 fn is_schemaless_map(query: &QueryPlan) -> bool {
-    match query {
-        QueryPlan::Project(project) => project_is_schemaless(project),
-        QueryPlan::Values(_) | QueryPlan::ValuesOrderBy(_) => false,
-        QueryPlan::SelectOrderBy(order_by) => project_is_schemaless(&order_by.input),
-        QueryPlan::Distinct(distinct) => distinct_is_schemaless(distinct),
-        QueryPlan::Offset(offset) => offset_is_schemaless(offset),
-        QueryPlan::Limit(LimitPlan { input, .. }) => match input {
-            LimitInputPlan::Project(project) => project_is_schemaless(project),
-            LimitInputPlan::Values(_) | LimitInputPlan::ValuesOrderBy(_) => false,
-            LimitInputPlan::SelectOrderBy(order_by) => project_is_schemaless(&order_by.input),
-            LimitInputPlan::Distinct(distinct) => distinct_is_schemaless(distinct),
-            LimitInputPlan::Offset(offset) => offset_is_schemaless(offset),
-        },
-    }
-}
-
-fn offset_is_schemaless(offset: &OffsetPlan) -> bool {
-    match &offset.input {
-        OffsetInputPlan::Project(project) => project_is_schemaless(project),
-        OffsetInputPlan::Values(_) | OffsetInputPlan::ValuesOrderBy(_) => false,
-        OffsetInputPlan::SelectOrderBy(order_by) => project_is_schemaless(&order_by.input),
-        OffsetInputPlan::Distinct(distinct) => distinct_is_schemaless(distinct),
-    }
-}
-
-fn distinct_is_schemaless(distinct: &DistinctPlan) -> bool {
-    match &distinct.input {
-        DistinctInputPlan::Project(project) => project_is_schemaless(project),
-        DistinctInputPlan::SelectOrderBy(order_by) => project_is_schemaless(&order_by.input),
-    }
-}
-
-fn project_is_schemaless(project: &ProjectPlan) -> bool {
-    matches!(project.projection, ProjectionPlan::SchemalessMap)
+    query
+        .project()
+        .is_some_and(|project| matches!(project.projection, ProjectionPlan::SchemalessMap))
 }
