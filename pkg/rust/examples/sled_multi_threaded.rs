e47b113c9e7dcce9b60dcbbfffc6d57e91fdb846
Taehoon Moon <taehoon.moon@outlook.com>
2026-08-24 17:28:44 +0900

    Stabilize Sled multithreaded example
    - Wait for the writer to commit before the reader queries the cloned storage
    - Return and validate the reader payload instead of querying through a stale clone

--- pkg/rust/examples/sled_multi_threaded.rs
+++ pkg/rust/examples/sled_multi_threaded.rs
@@ -8,7 +8,7 @@ mod sled_multi_threaded {
             gluesql_sled_storage::SledStorage,
             prelude::{Glue, Payload, Value},
         },
-        std::{fs, thread},
+        std::{fs, sync::mpsc, thread},
     };
 
     pub fn run() {
@@ -27,33 +27,36 @@ mod sled_multi_threaded {
             SledStorage supports cloning, using this we can create copies of the storage for new threads;
             all we need to do is wrap it in glue again.
         */
+        let (inserted_tx, inserted_rx) = mpsc::channel();
         let insert_storage = storage.clone();
         let insert_thread = thread::spawn(move || {
             let mut glue = Glue::new(insert_storage);
             let query = "INSERT INTO greet (name) VALUES ('Foo')";
 
             glue.execute(query).unwrap();
+            inserted_tx.send(()).unwrap();
         });
 
         let select_storage = storage;
         let select_thread = thread::spawn(move || {
+            inserted_rx.recv().unwrap();
+
             let mut glue = Glue::new(select_storage);
             let query = "SELECT * FROM greet;";
 
             let payloads = glue.execute(query).unwrap();
             println!("{payloads:?}");
-        });
 
-        select_thread
-            .join()
-            .expect("Something went wrong in the world thread");
+            payloads
+        });
 
         insert_thread
             .join()
             .expect("Something went wrong in the foo thread");
 
-        let query = "SELECT name FROM greet";
-        let payloads = glue.execute(query).unwrap();
+        let payloads = select_thread
+            .join()
+            .expect("Something went wrong in the world thread");
         assert_eq!(payloads.len(), 1);
 
         let Payload::Select { rows, .. } = &payloads[0] else {
@@ -67,7 +70,7 @@ mod sled_multi_threaded {
             value => panic!("Unexpected type: {value:?}"),
         };
 
-        // Will typically output "Hello Foo!" but will sometimes output "Hello World!"; depends on which thread finished first.
+        // Outputs "Hello Foo!" after the reader observes the writer's committed row.
         println!("Hello {to_greet}!");
     }
 }
